6bbf656c1da9196f88a715ea9938f638667bcb7b
whuwh27604 <5991277@qq.com>
2020-08-29 15:08:17 +0800

    add

---
+++ 中等/600~799/729. 我的日程安排表 I/我的日程安排表 I.cpp
@@ -0,0 +1,82 @@
+﻿/* 我的日程安排表 I.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+实现一个 MyCalendar 类来存放你的日程安排。如果要添加的时间内没有其他安排，则可以存储这个新的日程安排。
+
+MyCalendar 有一个 book(int start, int end)方法。它意味着在 start 到 end 时间内增加一个日程安排，注意，这里的时间是半开区间，即 [start, end), 实数 x 的范围为，  start <= x < end。
+
+当两个日程安排有一些时间上的交叉时（例如两个日程安排都在同一时间内），就会产生重复预订。
+
+每次调用 MyCalendar.book方法时，如果可以将日程安排成功添加到日历中而不会导致重复预订，返回 true。否则，返回 false 并且不要将该日程安排添加到日历中。
+
+请按照以下步骤调用 MyCalendar 类: MyCalendar cal = new MyCalendar(); MyCalendar.book(start, end)
+
+示例 1:
+
+MyCalendar();
+MyCalendar.book(10, 20); // returns true
+MyCalendar.book(15, 25); // returns false
+MyCalendar.book(20, 30); // returns true
+解释:
+第一个日程安排可以添加到日历中.  第二个日程安排不能添加到日历中，因为时间 15 已经被第一个日程安排预定了。
+第三个日程安排可以添加到日历中，因为第一个日程安排并不包含时间 20 。
+说明:
+
+每个测试用例，调用 MyCalendar.book 函数最多不超过 100次。
+调用函数 MyCalendar.book(start, end)时， start 和 end 的取值范围为 [0, 10^9]。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/my-calendar-i
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <set>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+struct eventCompare {
+    bool operator() (const pair<int, int>& e1, const pair<int, int>& e2) const {
+        return e1.second < e2.second;  // 按结束时间排序
+    }
+};
+
+class MyCalendar {
+public:
+    MyCalendar() {
+
+    }
+
+    bool book(int start, int end) {
+        auto iter = events.upper_bound({ 0, start });
+        if ((iter == events.end()) || (iter->first >= end)) {
+            events.insert({ start, end });
+            return true;
+        }
+
+        return false;
+    }
+
+private:
+    multiset<pair<int, int>, eventCompare> events;
+};
+
+int main()
+{
+    CheckResult check;
+
+    MyCalendar o1;
+    check.checkBool(true, o1.book(10, 20));
+    check.checkBool(false, o1.book(15, 25));
+    check.checkBool(true, o1.book(20, 30));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
