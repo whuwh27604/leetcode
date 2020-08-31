2e243eb4f337c71a646ac5987ba23505cbddc88f
whuwh27604 <5991277@qq.com>
2020-08-31 08:55:25 +0800

    add

---
+++ 中等/800~999/841. 钥匙和房间/钥匙和房间.cpp
@@ -0,0 +1,100 @@
+﻿/* 钥匙和房间.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+有 N 个房间，开始时你位于 0 号房间。每个房间有不同的号码：0，1，2，...，N-1，并且房间里可能有一些钥匙能使你进入下一个房间。
+
+在形式上，对于每个房间 i 都有一个钥匙列表 rooms[i]，每个钥匙 rooms[i][j] 由 [0,1，...，N-1] 中的一个整数表示，其中 N = rooms.length。 钥匙 rooms[i][j] = v 可以打开编号为 v 的房间。
+
+最初，除 0 号房间外的其余所有房间都被锁住。
+
+你可以自由地在房间之间来回走动。
+
+如果能进入每个房间返回 true，否则返回 false。
+
+示例 1：
+
+输入: [[1],[2],[3],[]]
+输出: true
+解释:
+我们从 0 号房间开始，拿到钥匙 1。
+之后我们去 1 号房间，拿到钥匙 2。
+然后我们去 2 号房间，拿到钥匙 3。
+最后我们去了 3 号房间。
+由于我们能够进入每个房间，我们返回 true。
+示例 2：
+
+输入：[[1,3],[3,0,1],[2],[0]]
+输出：false
+解释：我们不能进入 2 号房间。
+提示：
+
+1 <= rooms.length <= 1000
+0 <= rooms[i].length <= 1000
+所有房间中的钥匙数量总计不超过 3000。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/keys-and-rooms
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <unordered_set>
+#include <queue>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        unordered_set<int> visited;
+        queue<int> bfs;
+        bfs.push(0);
+        visited.insert(0);
+
+        while (!bfs.empty()) {
+            int room = bfs.front();
+            bfs.pop();
+
+            for (int i = 0; i < (int)rooms[room].size(); i++) {
+                if (visited.find(rooms[room][i]) == visited.end()) {
+                    bfs.push(rooms[room][i]);
+                    visited.insert(rooms[room][i]);
+                }
+            }
+
+            if (visited.size() == rooms.size()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<vector<int>> rooms = { {1},{2},{3},{} };
+    check.checkBool(true, o.canVisitAllRooms(rooms));
+
+    rooms = { {1,3},{3,0,1},{2},{0} };
+    check.checkBool(false, o.canVisitAllRooms(rooms));
+
+    rooms = { {} };
+    check.checkBool(true, o.canVisitAllRooms(rooms));
+
+    rooms = { {1,2,3},{},{},{} };
+    check.checkBool(true, o.canVisitAllRooms(rooms));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
