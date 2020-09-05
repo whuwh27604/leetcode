78eba1fa1af29280138742e63b031b1667999f73
whuwh27604 <5991277@qq.com>
2020-09-05 16:03:00 +0800

    add

---
+++ 中等/1~199/134. 加油站/加油站.cpp
@@ -0,0 +1,107 @@
+﻿/* 加油站.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+在一条环路上有 N 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
+
+你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。你从其中的一个加油站出发，开始时油箱为空。
+
+如果你可以绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1。
+
+说明: 
+
+如果题目有解，该答案即为唯一答案。
+输入数组均为非空数组，且长度相同。
+输入数组中的元素均为非负数。
+示例 1:
+
+输入:
+gas  = [1,2,3,4,5]
+cost = [3,4,5,1,2]
+
+输出: 3
+
+解释:
+从 3 号加油站(索引为 3 处)出发，可获得 4 升汽油。此时油箱有 = 0 + 4 = 4 升汽油
+开往 4 号加油站，此时油箱有 4 - 1 + 5 = 8 升汽油
+开往 0 号加油站，此时油箱有 8 - 2 + 1 = 7 升汽油
+开往 1 号加油站，此时油箱有 7 - 3 + 2 = 6 升汽油
+开往 2 号加油站，此时油箱有 6 - 4 + 3 = 5 升汽油
+开往 3 号加油站，你需要消耗 5 升汽油，正好足够你返回到 3 号加油站。
+因此，3 可为起始索引。
+示例 2:
+
+输入:
+gas  = [2,3,4]
+cost = [3,4,3]
+
+输出: -1
+
+解释:
+你不能从 0 号或 1 号加油站出发，因为没有足够的汽油可以让你行驶到下一个加油站。
+我们从 2 号加油站出发，可以获得 4 升汽油。 此时油箱有 = 0 + 4 = 4 升汽油
+开往 0 号加油站，此时油箱有 4 - 3 + 2 = 3 升汽油
+开往 1 号加油站，此时油箱有 3 - 3 + 3 = 3 升汽油
+你无法返回 2 号加油站，因为返程需要消耗 4 升汽油，但是你的油箱只有 3 升汽油。
+因此，无论怎样，你都不可能绕环路行驶一周。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/gas-station
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        // 通过合并加油站的方法归纳总结，可以证明总gas大于等于总cost就一定有解
+        // 所以一次遍历，当profit为负的时候，就认为从下一个加油站出发能够跑完一圈
+        int totalProfit = 0, currentProfit = 0;
+        int i, size = gas.size(), startIndex = 0;
+
+        for (i = 0; i < size; i++) {
+            totalProfit += (gas[i] - cost[i]);
+            currentProfit += (gas[i] - cost[i]);
+            if (currentProfit < 0) {
+                startIndex = i + 1;
+                currentProfit = 0;
+            }
+        }
+
+        return (totalProfit >= 0) ? startIndex : -1;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<int> gas = { 1 };
+    vector<int> cost = { 2 };
+    check.checkInt(-1, o.canCompleteCircuit(gas, cost));
+
+    gas = { 2 };
+    cost = { 2 };
+    check.checkInt(0, o.canCompleteCircuit(gas, cost));
+
+    gas = { 1,2,3,4,5 };
+    cost = { 3,4,5,1,2 };
+    check.checkInt(3, o.canCompleteCircuit(gas, cost));
+
+    gas = { 2,3,4 };
+    cost = { 3,4,3 };
+    check.checkInt(-1, o.canCompleteCircuit(gas, cost));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
