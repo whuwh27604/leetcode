e10c4ec68fd74175665ad8e741ff6fc75abc5c89
whuwh27604 <5991277@qq.com>
2020-09-07 22:46:35 +0800

    add

---
+++ 中等/600~799/792. 匹配子序列的单词数/匹配子序列的单词数（桶）.cpp
@@ -0,0 +1,113 @@
+﻿/* 匹配子序列的单词数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给定字符串 S 和单词字典 words, 求 words[i] 中是 S 的子序列的单词个数。
+
+示例:
+输入:
+S = "abcde"
+words = ["a", "bb", "acd", "ace"]
+输出: 3
+解释: 有三个是 S 的子序列的单词: "a", "acd", "ace"。
+注意:
+
+所有在words和 S 里的单词都只由小写字母组成。
+S 的长度在 [1, 50000]。
+words 的长度在 [1, 5000]。
+words[i]的长度在[1, 50]。
+通过次数5,311提交次数12,408
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-matching-subsequences
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <list>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int numMatchingSubseq(string S, vector<string>& words) {
+        int i, size = words.size(), count = 0;
+        vector<list<pair<int, int>>> buckets(26, list<pair<int, int>>{});
+
+        for (i = 0; i < size; i++) {
+            buckets[words[i][0] - 'a'].push_back({i, 0});  // pair<int, int>记录words中的第first个word，匹配到了第second个字符
+        }
+
+        for (char c : S) {
+            int j, sameFirstWordsSize = buckets[c - 'a'].size();
+
+            for (j = 0; j < sameFirstWordsSize; j++) {
+                pair<int, int>& word = buckets[c - 'a'].front();
+                if (word.second + 1 == words[word.first].size()) {
+                    count++;
+                }
+                else {  // 匹配上的index加一，放入新的bucket
+                    buckets[words[word.first][word.second + 1] - 'a'].push_back({ word.first, word.second + 1 });
+                }
+
+                buckets[c - 'a'].pop_front();  // 从老的bucket删除
+            }
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<string> words = { "a","bb","acd","ace" };
+    check.checkInt(3, o.numMatchingSubseq("abcde", words));
+
+    words = { "fxxxbzg","cfxxxbz" };
+    check.checkInt(2, o.numMatchingSubseq("cfxxxbzg", words));
+
+    words = { "a" };
+    check.checkInt(1, o.numMatchingSubseq("abcde", words));
+
+    words = { "x" };
+    check.checkInt(0, o.numMatchingSubseq("abcde", words));
+
+    words = { "ykbkihjdfxxxwragzcbhngbzgasxysxdtwn" };
+    check.checkInt(1, o.numMatchingSubseq("kguhsugfxvwxakdcovjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhngbzgasxysxdtwntvbpdihtvkffacmxhbxxqniyqm", words));
+
+    words = { "wxakdcovjeczhqvbevkhlixsrhumxykbkihj" };
+    check.checkInt(1, o.numMatchingSubseq("kguhsugfxvwxakdcovjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhngbzgasxysxdtwntvbpdihtvkffacmxhbxxqniyqm", words));
+
+    words = { "diht" };
+    check.checkInt(1, o.numMatchingSubseq("kguhsugfxvwxakdcovjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhngbzgasxysxdtwntvbpdihtvkffacmxhbxxqniyqm", words));
+
+    words = { "covjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhngbz" };
+    check.checkInt(1, o.numMatchingSubseq("kguhsugfxvwxakdcovjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhngbzgasxysxdtwntvbpdihtvkffacmxhbxxqniyqm", words));
+
+    words = { "ovjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhng" };
+    check.checkInt(1, o.numMatchingSubseq("kguhsugfxvwxakdcovjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhngbzgasxysxdtwntvbpdihtvkffacmxhbxxqniyqm", words));
+
+    words = { "ykbkihjdfxxxwragzcbhngbzgasxysxdtwn","wxakdcovjeczhqvbevkhlixsrhumxykbkihj","diht","covjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhngbz","ovjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhng" };
+    check.checkInt(5, o.numMatchingSubseq("kguhsugfxvwxakdcovjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhngbzgasxysxdtwntvbpdihtvkffacmxhbxxqniyqm", words));
+
+    words = { "ykbkihjdfxxxwragzcbhngbzgasxysxdtwn","wxakdcovjeczhqvbevkhlixsrhumxykbkihj","diht","covjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhngbz","ovjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhng","qhzucvqxalfrtlrdjverseuldzymzbunhugekoyyghmkpkfqmd","eydmbsorvjnnifqxsyuypsrijzrosukrhtbneprpyyoawbvoki","uanfzlfmmtvhzzebrazyuslbapsfzwtlvqbhqxsfmqagwxetro","fffaawedchlcyqvzzxbzczbwyfjkllpsjhoozyresqemmawban","astrknwzefcmuswdxalooatmiduspjuofthtomoqilgdojwhon" };
+    check.checkInt(5, o.numMatchingSubseq("kguhsugfxvwxakdcovjeczhqvbevkhlixsrhumxykbkihjdfxxxwragzcbhngbzgasxysxdtwntvbpdihtvkffacmxhbxxqniyqm", words));
+
+    words = { "ddeoincygycdpeh","yznmojdhbeomyjqptltpugzceyzenflfnhrptuug","ejuisks","teiqyligsnbqglqblhpdzzeurtdohdcbjvzgjwylmmoi","mrwnxhaqfezeeabuacyswollycgio","qfskkpfakjretogrokmxemjjbvgmmqrfdxlkfvycwalbdeumav","wjgjhlrpvhqozvvkifhftnfqcfjmmzhtxsoqbeduqmnpvimagq","ibxhtobuolmllbasaxlanjgalgmbjuxmqpadllryaobcucdeqc","ydlddogzvzttizzzjohfsenatvbpngarutztgdqczkzoenbxzv","rmsakibpprdrttycxglfgtjlifznnnlkgjqseguijfctrcahbb","pqquuarnoybphojyoyizhuyjfgwdlzcmkdbdqzatgmabhnpuyh" };
+    check.checkInt(5, o.numMatchingSubseq("ricogwqznwxxcpueelcobbbkuvxxrvgyehsudccpsnuxpcqobtvwkuvsubiidjtccoqvuahijyefbpqhbejuisksutsowhufsygtwteiqyligsnbqglqblhpdzzeurtdohdcbjvzgjwylmmoiundjscnlhbrhookmioxqighkxfugpeekgtdofwzemelpyjsdeeppapjoliqlhbrbghqjezzaxuwyrbczodtrhsvnaxhcjiyiphbglyolnswlvtlbmkrsurrcsgdzutwgjofowhryrubnxkahocqjzwwagqidjhwbunvlchojtbvnzdzqpvrazfcxtvhkruvuturdicnucvndigovkzrqiyastqpmfmuouycodvsyjajekhvyjyrydhxkdhffyytldcdlxqbaszbuxsacqwqnhrewhagldzhryzdmmrwnxhaqfezeeabuacyswollycgiowuuudrgzmwnxaezuqlsfvchjfloczlwbefksxsbanrektvibbwxnokzkhndmdhweyeycamjeplecewpnpbshhidnzwopdjuwbecarkgapyjfgmanuavzrxricbgagblomyseyvoeurekqjyljosvbneofjzxtaizjypbcxnbfeibrfjwyjqrisuybfxpvqywqjdlyznmojdhbeomyjqptltpugzceyzenflfnhrptuugyfsghluythksqhmxlmggtcbdddeoincygycdpehteiugqbptyqbvokpwovbnplshnzafunqglnpjvwddvdlmjjyzmwwxzjckmaptilrbfpjxiarmwalhbdjiwbaknvcqovwcqiekzfskpbhgxpyomekqvzpqyirelpadooxjhsyxjkfqavbaoqqvvknqryhotjritrkvdveyapjfsfzenfpuazdrfdofhudqbfnzxnvpluwicurrtshyvevkriudayyysepzqfgqwhgobwyhxltligahroyshfndydvffd", words));
+
+    words = { "wpddkvbnn","lnagtva","kvbnnuglnagtvamxkqtwhqgwbqgfbvgkwyuqkdwhzudsxvju","rwpddkvbnnugln","gloeofnpjqlkdsqvruvabjrikfwronbrdyyj","vbgeinupkvgmgxeaaiuiyojmoqkahwvbpwugdainxciedbdkos","mspuhbykmmumtveoighlcgpcapzczomshiblnvhjzqjlfkpina","rgmliajkiknongrofpugfgajedxicdhxinzjakwnifvxwlokip","fhepktaipapyrbylskxddypwmuuxyoivcewzrdwwlrlhqwzikq","qatithxifaaiwyszlkgoljzkkweqkjjzvymedvclfxwcezqebx" };
+    check.checkInt(5, o.numMatchingSubseq("rwpddkvbnnuglnagtvamxkqtwhqgwbqgfbvgkwyuqkdwhzudsxvjubjgloeofnpjqlkdsqvruvabjrikfwronbrdyyjnakstqjac", words));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ 中等/600~799/792. 匹配子序列的单词数/匹配子序列的单词数（贪心）.cpp
@@ -0,0 +1,110 @@
+﻿/* 匹配子序列的单词数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给定字符串 S 和单词字典 words, 求 words[i] 中是 S 的子序列的单词个数。
+
+示例:
+输入:
+S = "abcde"
+words = ["a", "bb", "acd", "ace"]
+输出: 3
+解释: 有三个是 S 的子序列的单词: "a", "acd", "ace"。
+注意:
+
+所有在words和 S 里的单词都只由小写字母组成。
+S 的长度在 [1, 50000]。
+words 的长度在 [1, 5000]。
+words[i]的长度在[1, 50]。
+通过次数5,311提交次数12,408
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-matching-subsequences
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <set>
+#include <unordered_map>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int numMatchingSubseq(string S, vector<string>& words) {
+        unordered_map<char, set<int>> letterIndices;
+        countLetters(S, letterIndices);
+
+        int count = 0;
+        for (string& word : words) {
+            if (isSubseq(word, letterIndices)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    void countLetters(string& S, unordered_map<char, set<int>>& letterIndices) {
+        for (int i = 0; i < (int)S.size(); i++) {
+            auto iter = letterIndices.find(S[i]);
+            if (iter == letterIndices.end()) {
+                letterIndices[S[i]] = { i };
+            }
+            else {
+                iter->second.insert(i);
+            }
+        }
+    }
+
+    // 贪心算法，每次匹配上一个字符后第一次匹配上的位置。如果第一次匹配的位置都不能匹配完，后面的位置更没有可能。
+    bool isSubseq(string& word, unordered_map<char, set<int>>& letterIndices) {
+        int last = -1;
+
+        for (char c : word) {
+            auto iter = letterIndices.find(c);
+            if (iter == letterIndices.end()) {
+                return false;
+            }
+
+            auto it = iter->second.upper_bound(last);
+            if (it == iter->second.end()) {
+                return false;
+            }
+
+            last = *it;
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<string> words = { "a","bb","acd","ace" };
+    check.checkInt(3, o.numMatchingSubseq("abcde", words));
+
+    words = { "a" };
+    check.checkInt(1, o.numMatchingSubseq("abcde", words));
+
+    words = { "x" };
+    check.checkInt(0, o.numMatchingSubseq("abcde", words));
+
+    words = { "ddeoincygycdpeh","yznmojdhbeomyjqptltpugzceyzenflfnhrptuug","ejuisks","teiqyligsnbqglqblhpdzzeurtdohdcbjvzgjwylmmoi","mrwnxhaqfezeeabuacyswollycgio","qfskkpfakjretogrokmxemjjbvgmmqrfdxlkfvycwalbdeumav","wjgjhlrpvhqozvvkifhftnfqcfjmmzhtxsoqbeduqmnpvimagq","ibxhtobuolmllbasaxlanjgalgmbjuxmqpadllryaobcucdeqc","ydlddogzvzttizzzjohfsenatvbpngarutztgdqczkzoenbxzv","rmsakibpprdrttycxglfgtjlifznnnlkgjqseguijfctrcahbb","pqquuarnoybphojyoyizhuyjfgwdlzcmkdbdqzatgmabhnpuyh" };
+    check.checkInt(5, o.numMatchingSubseq("ricogwqznwxxcpueelcobbbkuvxxrvgyehsudccpsnuxpcqobtvwkuvsubiidjtccoqvuahijyefbpqhbejuisksutsowhufsygtwteiqyligsnbqglqblhpdzzeurtdohdcbjvzgjwylmmoiundjscnlhbrhookmioxqighkxfugpeekgtdofwzemelpyjsdeeppapjoliqlhbrbghqjezzaxuwyrbczodtrhsvnaxhcjiyiphbglyolnswlvtlbmkrsurrcsgdzutwgjofowhryrubnxkahocqjzwwagqidjhwbunvlchojtbvnzdzqpvrazfcxtvhkruvuturdicnucvndigovkzrqiyastqpmfmuouycodvsyjajekhvyjyrydhxkdhffyytldcdlxqbaszbuxsacqwqnhrewhagldzhryzdmmrwnxhaqfezeeabuacyswollycgiowuuudrgzmwnxaezuqlsfvchjfloczlwbefksxsbanrektvibbwxnokzkhndmdhweyeycamjeplecewpnpbshhidnzwopdjuwbecarkgapyjfgmanuavzrxricbgagblomyseyvoeurekqjyljosvbneofjzxtaizjypbcxnbfeibrfjwyjqrisuybfxpvqywqjdlyznmojdhbeomyjqptltpugzceyzenflfnhrptuugyfsghluythksqhmxlmggtcbdddeoincygycdpehteiugqbptyqbvokpwovbnplshnzafunqglnpjvwddvdlmjjyzmwwxzjckmaptilrbfpjxiarmwalhbdjiwbaknvcqovwcqiekzfskpbhgxpyomekqvzpqyirelpadooxjhsyxjkfqavbaoqqvvknqryhotjritrkvdveyapjfsfzenfpuazdrfdofhudqbfnzxnvpluwicurrtshyvevkriudayyysepzqfgqwhgobwyhxltligahroyshfndydvffd", words));
+
+    words = { "wpddkvbnn","lnagtva","kvbnnuglnagtvamxkqtwhqgwbqgfbvgkwyuqkdwhzudsxvju","rwpddkvbnnugln","gloeofnpjqlkdsqvruvabjrikfwronbrdyyj","vbgeinupkvgmgxeaaiuiyojmoqkahwvbpwugdainxciedbdkos","mspuhbykmmumtveoighlcgpcapzczomshiblnvhjzqjlfkpina","rgmliajkiknongrofpugfgajedxicdhxinzjakwnifvxwlokip","fhepktaipapyrbylskxddypwmuuxyoivcewzrdwwlrlhqwzikq","qatithxifaaiwyszlkgoljzkkweqkjjzvymedvclfxwcezqebx" };
+    check.checkInt(5, o.numMatchingSubseq("rwpddkvbnnuglnagtvamxkqtwhqgwbqgfbvgkwyuqkdwhzudsxvjubjgloeofnpjqlkdsqvruvabjrikfwronbrdyyjnakstqjac", words));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
