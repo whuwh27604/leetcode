07143e22ff2ab6fe64def4c0cf0a534f444c5a15
whuwh27604 <5991277@qq.com>
2020-09-08 17:54:50 +0800

    add

---
+++ 中等/1~199/150. 逆波兰表达式求值/逆波兰表达式求值.cpp
@@ -0,0 +1,147 @@
+﻿/* 逆波兰表达式求值.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+根据 逆波兰表示法，求表达式的值。
+
+有效的运算符包括 +, -, *, / 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+
+ 
+
+说明：
+
+整数除法只保留整数部分。
+给定逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。
+ 
+
+示例 1：
+
+输入: ["2", "1", "+", "3", "*"]
+输出: 9
+解释: 该算式转化为常见的中缀算术表达式为：((2 + 1) * 3) = 9
+示例 2：
+
+输入: ["4", "13", "5", "/", "+"]
+输出: 6
+解释: 该算式转化为常见的中缀算术表达式为：(4 + (13 / 5)) = 6
+示例 3：
+
+输入: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+输出: 22
+解释:
+该算式转化为常见的中缀算术表达式为：
+  ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+ 
+
+逆波兰表达式：
+
+逆波兰表达式是一种后缀表达式，所谓后缀就是指算符写在后面。
+
+平常使用的算式则是一种中缀表达式，如 ( 1 + 2 ) * ( 3 + 4 ) 。
+该算式的逆波兰表达式写法为 ( ( 1 2 + ) ( 3 4 + ) * ) 。
+逆波兰表达式主要有以下两个优点：
+
+去掉括号后表达式无歧义，上式即便写成 1 2 + 3 4 + * 也可以依据次序计算出正确结果。
+适合用栈操作运算：遇到数字则入栈；遇到算符则取出栈顶两个数字进行计算，并将结果压入栈中。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/evaluate-reverse-polish-notation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <sstream>
+#include <stack>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        if (tokens.empty()) {
+            return 0;
+        }
+
+        stack<int> numbers;
+        int a, b, number;
+
+        for (string s : tokens) {
+            if (s == "+") {
+                get2Numbers(numbers, a, b);
+                numbers.push(a + b);
+            }
+            else if (s == "-") {
+                get2Numbers(numbers, a, b);
+                numbers.push(a - b);
+            }
+            else if (s == "*") {
+                get2Numbers(numbers, a, b);
+                numbers.push(a * b);
+            }
+            else if (s == "/") {
+                get2Numbers(numbers, a, b);
+                numbers.push(a / b);
+            }
+            else {
+                stringstream ss;
+                ss << s;
+                ss >> number;
+                numbers.push(number);
+            }
+        }
+
+        return numbers.top();
+    }
+
+    void get2Numbers(stack<int>& numbers, int& a, int& b) {
+        b = numbers.top();
+        numbers.pop();
+        a = numbers.top();
+        numbers.pop();
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<string> tokens = {};
+    check.checkInt(0, o.evalRPN(tokens));
+
+    tokens = { "1","2","+" };
+    check.checkInt(3, o.evalRPN(tokens));
+
+    tokens = { "5","3","-" };
+    check.checkInt(2, o.evalRPN(tokens));
+
+    tokens = { "2","3","*" };
+    check.checkInt(6, o.evalRPN(tokens));
+
+    tokens = { "12","3","/" };
+    check.checkInt(4, o.evalRPN(tokens));
+
+    tokens = { "2", "1", "+", "3", "*" };
+    check.checkInt(9, o.evalRPN(tokens));
+
+    tokens = { "4", "13", "5", "/", "+" };
+    check.checkInt(6, o.evalRPN(tokens));
+
+    tokens = { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
+    check.checkInt(22, o.evalRPN(tokens));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ 中等/1~199/151. 翻转字符串里的单词/翻转字符串里的单词.cpp
@@ -0,0 +1,95 @@
+﻿/* 翻转字符串里的单词.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给定一个字符串，逐个翻转字符串中的每个单词。
+
+ 
+
+示例 1：
+
+输入: "the sky is blue"
+输出: "blue is sky the"
+示例 2：
+
+输入: "  hello world!  "
+输出: "world! hello"
+解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+示例 3：
+
+输入: "a good   example"
+输出: "example good a"
+解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ 
+
+说明：
+
+无空格字符构成一个单词。
+输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ 
+
+进阶：
+
+请选用 C 语言的用户尝试使用 O(1) 额外空间复杂度的原地解法。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-words-in-a-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans;
+        int position = 0, start = 0;
+        s.push_back(' ');
+
+        while ((position = s.find(' ', position)) != s.npos) {
+            if ((position - start) == 0) {
+                start = ++position;
+                continue;
+            }
+
+            string word(s, start, (position - start));
+            ans = word + " " + ans;
+            start = ++position;
+        }
+
+        if (!ans.empty()) {
+            ans.pop_back();
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    check.checkString("", o.reverseWords(""));
+    check.checkString("", o.reverseWords(" "));
+    check.checkString("", o.reverseWords("  "));
+    check.checkString("a", o.reverseWords("a"));
+    check.checkString("a", o.reverseWords("  a   "));
+    check.checkString("abc", o.reverseWords("abc"));
+    check.checkString("abc", o.reverseWords(" abc "));
+    check.checkString("example good a", o.reverseWords("a good   example"));
+    check.checkString("world! hello", o.reverseWords("  hello world!  "));
+    check.checkString("blue is sky the", o.reverseWords("the sky is blue"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
