7bc670e660f04656ad57699e4cd074201543607c
whuwh27604 <5991277@qq.com>
2020-09-24 22:20:14 +0800

    add

---
+++ 中等/800~999/885. 螺旋矩阵 III/螺旋矩阵 III.cpp
@@ -0,0 +1,102 @@
+﻿/* 螺旋矩阵 III.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+在 R 行 C 列的矩阵上，我们从 (r0, c0) 面朝东面开始
+
+这里，网格的西北角位于第一行第一列，网格的东南角位于最后一行最后一列。
+
+现在，我们以顺时针按螺旋状行走，访问此网格中的每个位置。
+
+每当我们移动到网格的边界之外时，我们会继续在网格之外行走（但稍后可能会返回到网格边界）。
+
+最终，我们到过网格的所有 R * C 个空间。
+
+按照访问顺序返回表示网格位置的坐标列表。
+
+ 
+
+示例 1：
+
+输入：R = 1, C = 4, r0 = 0, c0 = 0
+输出：[[0,0],[0,1],[0,2],[0,3]]
+
+
+ 
+
+示例 2：
+
+输入：R = 5, C = 6, r0 = 1, c0 = 4
+输出：[[1,4],[1,5],[2,5],[2,4],[2,3],[1,3],[0,3],[0,4],[0,5],[3,5],[3,4],[3,3],[3,2],[2,2],[1,2],[0,2],[4,5],[4,4],[4,3],[4,2],[4,1],[3,1],[2,1],[1,1],[0,1],[4,0],[3,0],[2,0],[1,0],[0,0]]
+
+
+ 
+
+提示：
+
+1 <= R <= 100
+1 <= C <= 100
+0 <= r0 < R
+0 <= c0 < C
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/spiral-matrix-iii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> spiralMatrixIII(int R, int C, int r0, int c0) {
+        int dr[4] = { 0,1,0,-1 }, dc[4] = { 1,0,-1,0 };
+        int i, j, k = 1, row = r0, column = c0, direction = 0, step = 1, size = R * C;
+        vector<vector<int>> ans(size, { r0, c0 });
+
+        while (k < size) {
+            for (i = 0; i < 2; ++i) {
+                for (j = 0; j < step; ++j) {
+                    row += dr[direction];
+                    column += dc[direction];
+
+                    if (row >= 0 && row < R && column >= 0 && column < C) {
+                        ans[k][0] = row;
+                        ans[k++][1] = column;
+                    }
+                }
+
+                direction = (direction + 1) % 4;  // 每走step步，方向加1
+            }
+
+            ++step;  // 每走两个方向，步长加1
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<vector<int>> actual = o.spiralMatrixIII(1, 4, 0, 0);
+    vector<vector<int>> expected = { {0,0},{0,1},{0,2},{0,3} };
+    check.checkIntVectorVector(expected, actual);
+
+    actual = o.spiralMatrixIII(5, 6, 1, 4);
+    expected = { {1,4},{1,5},{2,5},{2,4},{2,3},{1,3},{0,3},{0,4},{0,5},{3,5},{3,4},{3,3},{3,2},{2,2},{1,2},{0,2},
+        {4,5},{4,4},{4,3},{4,2},{4,1},{3,1},{2,1},{1,1},{0,1},{4,0},{3,0},{2,0},{1,0},{0,0} };
+    check.checkIntVectorVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
