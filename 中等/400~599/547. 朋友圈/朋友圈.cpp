1b162950b032a27135f158aad7ee66e302fcc793
whuwh27604 <5991277@qq.com>
2020-10-06 23:01:01 +0800

    add

---
+++ 中等/400~599/547. 朋友圈/朋友圈.cpp
@@ -0,0 +1,105 @@
+﻿/* 朋友圈.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
+
+给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，否则为不知道。你必须输出所有学生中的已知的朋友圈总数。
+
+示例 1:
+
+输入:
+[[1,1,0],
+ [1,1,0],
+ [0,0,1]]
+输出: 2
+说明：已知学生0和学生1互为朋友，他们在一个朋友圈。
+第2个学生自己在一个朋友圈。所以返回2。
+示例 2:
+
+输入:
+[[1,1,0],
+ [1,1,1],
+ [0,1,1]]
+输出: 1
+说明：已知学生0和学生1互为朋友，学生1和学生2互为朋友，所以学生0和学生2也是朋友，所以他们三个在一个朋友圈，返回1。
+注意：
+
+N 在[1,200]的范围内。
+对于所有学生，有M[i][i] = 1。
+如果有M[i][j] = 1，则有M[j][i] = 1。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/friend-circles
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <unordered_set>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& M) {
+        int N = M.size(), circleCount = 0;
+        unordered_set<int> sdutentsLeft;
+        for (int i = 0; i < N; i++) {
+            sdutentsLeft.insert(i);
+        }
+
+        while (!sdutentsLeft.empty()) {
+            circleCount++;
+            unordered_set<int> sdutentsInCircle;
+            int sdutent = *sdutentsLeft.begin();
+            DFS(M, sdutent, sdutentsInCircle, sdutentsLeft);
+        }
+
+        return circleCount;
+    }
+
+    void DFS(vector<vector<int>>& M, int sdutent, unordered_set<int>& sdutentsInCircle, unordered_set<int>& sdutentsLeft) {
+        sdutentsLeft.erase(sdutent);
+
+        if (sdutentsInCircle.find(sdutent) != sdutentsInCircle.end()) {
+            return;
+        }
+        sdutentsInCircle.insert(sdutent);
+
+        for (int i = 0; i < (int)M.size(); i++) {
+            if (M[sdutent][i] == 1) {
+                DFS(M, i, sdutentsInCircle, sdutentsLeft);
+            }
+        }
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<vector<int>> M = { {1} };
+    check.checkInt(1, o.findCircleNum(M));
+
+    M = { {1,1,0},{1,1,0},{0,0,1} };
+    check.checkInt(2, o.findCircleNum(M));
+
+    M = { {1,1,0},{1,1,1},{0,1,1} };
+    check.checkInt(1, o.findCircleNum(M));
+
+    M = { {1,1,0,0,0,0,0,1,0,0,0,0,0,0,0},{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,1,0,1,1,0,0,0,0,0,0,0,0},
+        {0,0,0,0,1,0,0,0,0,1,1,0,0,0,0},{0,0,0,1,0,1,0,0,0,0,1,0,0,0,0},{0,0,0,1,0,0,1,0,1,0,0,0,0,1,0},{1,0,0,0,0,0,0,1,1,0,0,0,0,0,0},
+        {0,0,0,0,0,0,1,1,1,0,0,0,0,1,0},{0,0,0,0,1,0,0,0,0,1,0,1,0,0,1},{0,0,0,0,1,1,0,0,0,0,1,1,0,0,0},{0,0,0,0,0,0,0,0,0,1,1,1,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0},{0,0,0,0,0,0,1,0,1,0,0,0,0,1,0},{0,0,0,0,0,0,0,0,0,1,0,0,0,0,1} };
+    check.checkInt(3, o.findCircleNum(M));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
