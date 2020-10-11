7c4e6987cfc6b5151c84b582fdd2e0247d4beb46
whuwh27604 <5991277@qq.com>
2020-10-11 19:49:58 +0800

    add

---
+++ 中等/400~599/470. 用 Rand7() 实现 Rand10()/用 Rand7() 实现 Rand10().cpp
@@ -0,0 +1,86 @@
+﻿/* 用 Rand7() 实现 Rand10().cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+已有方法 rand7 可生成 1 到 7 范围内的均匀随机整数，试写一个方法 rand10 生成 1 到 10 范围内的均匀随机整数。
+
+不要使用系统的 Math.random() 方法。
+
+ 
+
+示例 1:
+
+输入: 1
+输出: [7]
+示例 2:
+
+输入: 2
+输出: [8,4]
+示例 3:
+
+输入: 3
+输出: [8,1,10]
+ 
+
+提示:
+
+rand7 已定义。
+传入参数: n 表示 rand10 的调用次数。
+ 
+
+进阶:
+
+rand7()调用次数的 期望值 是多少 ?
+你能否尽量少调用 rand7() ?
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/implement-rand10-using-rand7
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <random>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+int rand7() {
+    random_device rd;
+    return 1 + (rd() - 1) % 7;;  // just simulate
+}
+
+class Solution {
+public:
+    int rand10() {
+        int row, column, index;
+
+        do {
+            row = rand7();
+            column = rand7();
+            index = column + (row - 1) * 7;
+        } while (index > 40);
+
+        return 1 + (index - 1) % 10;
+    }
+};
+
+int main()
+{
+    Solution o;
+
+    for (int i = 0; i < 100; i++) {
+        cout << o.rand10() << " ";
+    }
+    cout << endl;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
