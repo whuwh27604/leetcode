bb0e07b50e4498a8ee99f24a2b835daf8cdad719
whuwh27604 <5991277@qq.com>
2020-10-17 14:04:48 +0800

    add

--- tools/TreeNode.cpp
+++ tools/TreeNode.cpp
@@ -1,9 +1,18 @@
 
 #include <iostream>
 #include <queue>
+#include <sstream>
 #include "../header/TreeNode.h"
 
 using namespace std;
+
+int getNumber(string& value) {
+	stringstream ss;
+	ss << value;
+	int number;
+	ss >> number;
+	return number;
+}
 
 TreeNode* createTree(vector<int>& values) {
 	int i, size = values.size();
@@ -20,6 +29,37 @@ TreeNode* createTree(vector<int>& values) {
 		}
 		else {
 			node = new TreeNode(values[i]);
+			nodes.push(node);
+		}
+
+		if (i % 2 == 1) {
+			current = nodes.front();
+			nodes.pop();
+			current->left = node;
+		}
+		else {
+			current->right = node;
+		}
+	}
+
+	return root;
+}
+
+TreeNode* createTree(vector<string>& values) {
+	int i, size = values.size();
+	TreeNode* root = new TreeNode(getNumber(values[0]));
+	queue<TreeNode*> nodes;
+	nodes.push(root);
+
+	TreeNode* current = root;
+	TreeNode* node;
+
+	for (i = 1; i < size; ++i) {
+		if (values[i] == "null") {
+			node = NULL;
+		}
+		else {
+			node = new TreeNode(getNumber(values[i]));
 			nodes.push(node);
 		}
 
