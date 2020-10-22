972fcbd2023721eaafa4852a55f0b9dc4f944ac3
whuwh27604 <5991277@qq.com>
2020-10-22 08:23:13 +0800

    add

--- header/ListNode.h
+++ header/ListNode.h
@@ -1,10 +1,16 @@
 #ifndef __LISTNODE_H__
 #define __LISTNODE_H__
 
+#include <vector>
+
+using namespace std;
+
 struct ListNode {
 	int val;
 	ListNode* next;
 	ListNode(int x) : val(x), next(NULL) {}
 };
+
+extern ListNode* createList(vector<int>& values);
 
 #endif
