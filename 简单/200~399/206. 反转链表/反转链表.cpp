8b38df5b63f7dc626afc50f1a477a86c83ebd06a
whuwh27604 <5991277@qq.com>
2020-10-23 08:24:59 +0800

    add

---
+++ 简单/200~399/206. 反转链表/反转链表.cpp
@@ -0,0 +1,68 @@
+﻿/* 反转链表.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+反转一个单链表。
+
+示例 :
+
+输入: 1->2->3->4->5->NULL
+输出 : 5->4->3->2->1->NULL
+进阶 :
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+
+来源：力扣（LeetCode）
+链接：https ://leetcode-cn.com/problems/reverse-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* current = head;
+        while (current != NULL) {
+            ListNode* next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    ListNode node1(1);
+    ListNode node2(2);
+    ListNode node3(3);
+    ListNode node4(4);
+    ListNode node5(5);
+    node1.next = &node2;
+    node2.next = &node3;
+    node3.next = &node4;
+    node4.next = &node5;
+    check.checkSingleList(&node5, o.reverseList(&node1));
+
+    node1.next = NULL;
+    check.checkSingleList(&node1, o.reverseList(&node1));
+
+    check.checkSingleList(NULL, o.reverseList(NULL));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
