0eb5e83456d1727469cbb8f5924d40c9718402a5
whuwh27604 <5991277@qq.com>
2020-11-05 09:48:50 +0800

    add

---
+++ 中等/1000~1199/1140. 石子游戏 II/石子游戏 II（记忆化搜索）.cpp
@@ -0,0 +1,112 @@
+﻿/* 石子游戏 II.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+亚历克斯和李继续他们的石子游戏。许多堆石子 排成一行，每堆都有正整数颗石子 piles[i]。游戏以谁手中的石子最多来决出胜负。
+
+亚历克斯和李轮流进行，亚历克斯先开始。最初，M = 1。
+
+在每个玩家的回合中，该玩家可以拿走剩下的 前 X 堆的所有石子，其中 1 <= X <= 2M。然后，令 M = max(M, X)。
+
+游戏一直持续到所有石子都被拿走。
+
+假设亚历克斯和李都发挥出最佳水平，返回亚历克斯可以得到的最大数量的石头。
+
+ 
+
+示例：
+
+输入：piles = [2,7,9,4,4]
+输出：10
+解释：
+如果亚历克斯在开始时拿走一堆石子，李拿走两堆，接着亚历克斯也拿走两堆。在这种情况下，亚历克斯可以拿到 2 + 4 + 4 = 10 颗石子。
+如果亚历克斯在开始时拿走两堆石子，那么李就可以拿走剩下全部三堆石子。在这种情况下，亚历克斯可以拿到 2 + 7 = 9 颗石子。
+所以我们返回更大的 10。
+ 
+
+提示：
+
+1 <= piles.length <= 100
+1 <= piles[i] <= 10 ^ 4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/stone-game-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        vector<int> preSum(piles.size() + 1, 0);
+        getPreSum(piles, preSum);
+
+        unordered_map<int, int> memo;
+        return DFS(piles, preSum, memo, 0, 1);
+    }
+
+    void getPreSum(vector<int>& piles, vector<int>& preSum) {
+        for (unsigned int i = 0; i < piles.size(); ++i) {
+            preSum[i + 1] = preSum[i] + piles[i];
+        }
+    }
+
+    int DFS(vector<int>& piles, vector<int>& preSum, unordered_map<int, int>& memo, int start, int M) {
+        int i, size = piles.size(), maxStones = 0;
+
+        if (start == size) {
+            return 0;
+        }
+
+        int key = ((start << 8) | M);
+        if (memo.count(key) != 0) {
+            return memo[key];
+        }
+
+        for (i = start; i < (start + 2 * M) && i < size; ++i) {
+            int stones = preSum[i + 1] - preSum[start];  // 取[start,i]的stone
+            int newM = max(M, i - start + 1);
+            // 自己取[start,i]以后，还剩preSum[size] - preSum[i+1]，对手最多取DFS()，剩下的是自己的
+            stones += (preSum[size] - preSum[i + 1] - DFS(piles, preSum, memo, i + 1, newM));
+            maxStones = max(maxStones, stones);
+        }
+
+        memo[key] = maxStones;
+        return maxStones;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> piles = { 2,7,9,4,4 };
+    check.checkInt(10, o.stoneGameII(piles));
+
+    piles = { 1,5,7,9,9 };
+    check.checkInt(17, o.stoneGameII(piles));
+
+    piles = { 1,2,3,4,5,6,7,8,9,10 };
+    check.checkInt(26, o.stoneGameII(piles));
+
+    piles = { 1,2,3,4,5,6,7,8,9,10,10,9,8,7,6,5,4,3,2,1 };
+    check.checkInt(55, o.stoneGameII(piles));
+
+    piles = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50 };
+    check.checkInt(638, o.stoneGameII(piles));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
