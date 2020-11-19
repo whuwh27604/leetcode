f6cb3648d593e75d515365cc153f5ef7fa61add6
whuwh27604 <5991277@qq.com>
2020-11-20 00:39:08 +0800

    add

---
+++ questions/1~99/55. 跳跃游戏/跳跃游戏.cpp
@@ -0,0 +1,77 @@
+﻿/* 跳跃游戏.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给定一个非负整数数组，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+判断你是否能够到达最后一个位置。
+
+示例 1:
+
+输入: [2,3,1,1,4]
+输出: true
+解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+示例 2:
+
+输入: [3,2,1,0,4]
+输出: false
+解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/jump-game
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int i, size = nums.size(), maxDistance = 0;
+
+        for (i = 0; i < size; ++i) {
+            if (i > maxDistance) {
+                return false;
+            }
+
+            maxDistance = max(maxDistance, i + nums[i]);
+            if (maxDistance >= size - 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 2,3,1,1,4 };
+    check.checkBool(true, o.canJump(nums));
+
+    nums = { 3,2,1,0,4 };
+    check.checkBool(false, o.canJump(nums));
+
+    nums = { 0 };
+    check.checkBool(true, o.canJump(nums));
+
+    nums = { 0,1 };
+    check.checkBool(false, o.canJump(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
