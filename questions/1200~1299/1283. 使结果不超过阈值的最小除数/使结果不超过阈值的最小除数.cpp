b1c6ae3ecc2345881506042f42ee9530471ae488
whuwh27604 <5991277@qq.com>
2020-11-30 00:13:40 +0800

    add

---
+++ questions/1200~1299/1283. 使结果不超过阈值的最小除数/使结果不超过阈值的最小除数.cpp
@@ -0,0 +1,100 @@
+﻿/* 使结果不超过阈值的最小除数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数数组 nums 和一个正整数 threshold  ，你需要选择一个正整数作为除数，然后将数组里每个数都除以它，并对除法结果求和。
+
+请你找出能够使上述结果小于等于阈值 threshold 的除数中 最小 的那个。
+
+每个数除以除数后都向上取整，比方说 7/3 = 3 ， 10/2 = 5 。
+
+题目保证一定有解。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,5,9], threshold = 6
+输出：5
+解释：如果除数为 1 ，我们可以得到和为 17 （1+2+5+9）。
+如果除数为 4 ，我们可以得到和为 7 (1+1+2+3) 。如果除数为 5 ，和为 5 (1+1+1+2)。
+示例 2：
+
+输入：nums = [2,3,5,7,11], threshold = 11
+输出：3
+示例 3：
+
+输入：nums = [19], threshold = 5
+输出：4
+ 
+
+提示：
+
+1 <= nums.length <= 5 * 10^4
+1 <= nums[i] <= 10^6
+nums.length <= threshold <= 10^6
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-the-smallest-divisor-given-a-threshold
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int left = 1, right = *max_element(nums.begin(), nums.end());
+
+        while (left < right) {
+            int middle = (left + right) / 2;
+            long long sum = getSum(nums, middle);
+
+            if (sum > threshold) {
+                left = middle + 1;
+            }
+            else {
+                right = middle;
+            }
+        }
+
+        return left;
+    }
+
+    long long getSum(vector<int>& nums, int divisor) {
+        long long sum = 0;
+
+        for (int num : nums) {
+            sum += (((long long)num - 1) / divisor + 1);
+        }
+
+        return sum;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,2,5,9 };
+    check.checkInt(5, o.smallestDivisor(nums, 6));
+
+    nums = { 2,3,5,7,11 };
+    check.checkInt(3, o.smallestDivisor(nums, 11));
+
+    nums = { 19 };
+    check.checkInt(4, o.smallestDivisor(nums, 5));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
