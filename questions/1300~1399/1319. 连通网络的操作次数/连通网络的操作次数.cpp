5ae307c449f8bec3ce5e803c9b78e0ae34416f5b
whuwh27604 <5991277@qq.com>
2020-12-06 00:03:50 +0800

    add

---
+++ questions/1300~1399/1319. 连通网络的操作次数/连通网络的操作次数.cpp
@@ -0,0 +1,145 @@
+﻿/* 连通网络的操作次数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+用以太网线缆将 n 台计算机连接成一个网络，计算机的编号从 0 到 n-1。线缆用 connections 表示，其中 connections[i] = [a, b] 连接了计算机 a 和 b。
+
+网络中的任何一台计算机都可以通过网络直接或者间接访问同一个网络中其他任意一台计算机。
+
+给你这个计算机网络的初始布线 connections，你可以拔开任意两台直连计算机之间的线缆，并用它连接一对未直连的计算机。请你计算并返回使所有计算机都连通所需的最少操作次数。如果不可能，则返回 -1 。 
+
+ 
+
+示例 1：
+
+
+
+输入：n = 4, connections = [[0,1],[0,2],[1,2]]
+输出：1
+解释：拔下计算机 1 和 2 之间的线缆，并将它插到计算机 1 和 3 上。
+示例 2：
+
+
+
+输入：n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+输出：2
+示例 3：
+
+输入：n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+输出：-1
+解释：线缆数量不足。
+示例 4：
+
+输入：n = 5, connections = [[0,1],[0,2],[3,4],[2,3]]
+输出：0
+ 
+
+提示：
+
+1 <= n <= 10^5
+1 <= connections.length <= min(n*(n-1)/2, 10^5)
+connections[i].length == 2
+0 <= connections[i][0], connections[i][1] < n
+connections[i][0] != connections[i][1]
+没有重复的连接。
+两台计算机不会通过多条线缆连接。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-operations-to-make-network-connected
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class DSU {
+public:
+    DSU(int _size) {
+        size = _size;
+        parents = new int[size];
+
+        for (int i = 0; i < size; ++i) {
+            parents[i] = i;
+        }
+    }
+
+    ~DSU() {
+        delete[] parents;
+    }
+
+    int find(int x) {
+        if (parents[x] != x) {
+            parents[x] = find(parents[x]);
+        }
+
+        return parents[x];
+    }
+
+    void merge(int x, int y) {
+        parents[find(x)] = find(y);
+    }
+
+    int count() {
+        int sets = 0;
+
+        for (int i = 0; i < size; ++i) {
+            if (find(i) == i) {
+                ++sets;
+            }
+        }
+
+        return sets;
+    }
+
+private:
+    int* parents;
+    int size;
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if ((int)connections.size() + 1 < n) {
+            return -1;
+        }
+
+        DSU dsu(n);
+
+        for (vector<int>& connection : connections) {
+            dsu.merge(connection[0], connection[1]);
+        }
+
+        return dsu.count() - 1;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> connections = { {0,1},{0,2},{1,2} };
+    check.checkInt(1, o.makeConnected(4, connections));
+
+    connections = { {0,1},{0,2},{0,3},{1,2},{1,3} };
+    check.checkInt(2, o.makeConnected(6, connections));
+
+    connections = { {0,1},{0,2},{0,3},{1,2} };
+    check.checkInt(-1, o.makeConnected(6, connections));
+
+    connections = { {0,1},{0,2},{3,4},{2,3} };
+    check.checkInt(0, o.makeConnected(5, connections));
+
+    connections = { {0,1} };
+    check.checkInt(0, o.makeConnected(2, connections));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
