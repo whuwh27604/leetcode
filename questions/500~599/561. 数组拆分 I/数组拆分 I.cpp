047feed5e79a8eb88f2a97089be3eecd27520352
whuwh27604 <5991277@qq.com>
2020-12-15 08:34:59 +0800

    add

---
+++ questions/500~599/561. 数组拆分 I/数组拆分 I.cpp
@@ -0,0 +1,64 @@
+﻿/* 数组拆分 I.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
+
+示例 1:
+
+输入: [1,4,3,2]
+
+输出: 4
+解释: n 等于 2, 最大总和为 4 = min(1, 2) + min(3, 4).
+提示:
+
+n 是正整数,范围在 [1, 10000].
+数组中的元素范围在 [-10000, 10000].
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/array-partition-i
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int minSum = 0;
+        for (unsigned int i = 0; i < nums.size(); (i += 2)) {
+            minSum += nums[i];
+        }
+
+        return minSum;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<int> nums = { 1,4,3,2 };
+    check.checkInt(4, o.arrayPairSum(nums));
+
+    nums = { 1,4 };
+    check.checkInt(1, o.arrayPairSum(nums));
+
+    nums = { 1,1,2,2,3,3,4,4 };
+    check.checkInt(10, o.arrayPairSum(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
