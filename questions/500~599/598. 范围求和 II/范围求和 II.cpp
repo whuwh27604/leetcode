27cf93ac52864b52024c34b508f8ae41354f4360
whuwh27604 <5991277@qq.com>
2020-12-17 10:22:34 +0800

    add

---
+++ questions/500~599/598. 范围求和 II/范围求和 II.cpp
@@ -0,0 +1,85 @@
+﻿/* 范围求和 II.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给定一个初始元素全部为 0，大小为 m*n 的矩阵 M 以及在 M 上的一系列更新操作。
+
+操作用二维数组表示，其中的每个操作用一个含有两个正整数 a 和 b 的数组表示，含义是将所有符合 0 <= i < a 以及 0 <= j < b 的元素 M[i][j] 的值都增加 1。
+
+在执行给定的一系列操作后，你需要返回矩阵中含有最大整数的元素个数。
+
+示例 1:
+
+输入:
+m = 3, n = 3
+operations = [[2,2],[3,3]]
+输出: 4
+解释:
+初始状态, M =
+[[0, 0, 0],
+ [0, 0, 0],
+ [0, 0, 0]]
+
+执行完操作 [2,2] 后, M =
+[[1, 1, 0],
+ [1, 1, 0],
+ [0, 0, 0]]
+
+执行完操作 [3,3] 后, M =
+[[2, 2, 1],
+ [2, 2, 1],
+ [1, 1, 1]]
+
+M 中最大的整数是 2, 而且 M 中有4个值为2的元素。因此返回 4。
+注意:
+
+m 和 n 的范围是 [1,40000]。
+a 的范围是 [1,m]，b 的范围是 [1,n]。
+操作数目不超过 10000。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/range-addition-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        int minA = m, minB = n;
+        for (unsigned int i = 0; i < ops.size(); i++) {
+            minA = min(minA, ops[i][0]);
+            minB = min(minB, ops[i][1]);
+        }
+
+        return (minA * minB);
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<vector<int>> ops = { {2,2},{3,3} };
+    check.checkInt(4, o.maxCount(3, 3, ops));
+    
+    ops = { {1,1},{1,1} };
+    check.checkInt(1, o.maxCount(1, 1, ops));
+
+    ops = {  };
+    check.checkInt(30, o.maxCount(5, 6, ops));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
