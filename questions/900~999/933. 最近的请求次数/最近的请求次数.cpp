53c661784ca507b0f2f9414ffaadf9f089e376a5
whuwh27604 <5991277@qq.com>
2021-01-21 11:53:49 +0800

    add

---
+++ questions/900~999/933. 最近的请求次数/最近的请求次数.cpp
@@ -0,0 +1,79 @@
+﻿/* 最近的请求次数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+写一个 RecentCounter 类来计算最近的请求。
+
+它只有一个方法：ping(int t)，其中 t 代表以毫秒为单位的某个时间。
+
+返回从 3000 毫秒前到现在的 ping 数。
+
+任何处于 [t - 3000, t] 时间范围之内的 ping 都将会被计算在内，包括当前（指 t 时刻）的 ping。
+
+保证每次对 ping 的调用都使用比之前更大的 t 值。
+
+ 
+
+示例：
+
+输入：inputs = ["RecentCounter","ping","ping","ping","ping"], inputs = [[],[1],[100],[3001],[3002]]
+输出：[null,1,2,3,3]
+ 
+
+提示：
+
+每个测试用例最多调用 10000 次 ping。
+每个测试用例会使用严格递增的 t 值来调用 ping。
+每次调用 ping 都有 1 <= t <= 10^9。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-recent-calls
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <queue>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class RecentCounter {
+public:
+    RecentCounter() {
+
+    }
+
+    int ping(int t) {
+        times.push(t);
+
+        while (t - times.front() > 3000) {
+            times.pop();
+        }
+
+        return times.size();
+    }
+
+    queue<int> times;
+};
+
+int main()
+{
+    CheckResult check;
+    RecentCounter* obj = new RecentCounter();
+    int param_1 = obj->ping(1);
+    check.checkInt(1, param_1);
+    param_1 = obj->ping(100);
+    check.checkInt(2, param_1);
+    param_1 = obj->ping(3001);
+    check.checkInt(3, param_1);
+    param_1 = obj->ping(3002);
+    check.checkInt(3, param_1);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
