05ad8d1f7a3596f3d4af2fc053018a73f8419b4e
whuwh27604 <5991277@qq.com>
2021-01-23 00:18:44 +0800

    add

---
+++ questions/900~999/944. 删列造序/删列造序.cpp
@@ -0,0 +1,101 @@
+﻿/* 删列造序.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给定由 N 个小写字母字符串组成的数组 A，其中每个字符串长度相等。
+
+你需要选出一组要删掉的列 D，对 A 执行删除操作，使 A 中剩余的每一列都是 非降序 排列的，然后请你返回 D.length 的最小可能值。
+
+删除 操作的定义是：选出一组要删掉的列，删去 A 中对应列中的所有字符，形式上，第 n 列为 [A[0][n], A[1][n], ..., A[A.length-1][n]]）。
+
+比如，有 A = ["abcdef", "uvwxyz"]，
+
+
+
+要删掉的列为 {0, 2, 3}，删除后 A 为["bef", "vyz"]， A 的列分别为["b","v"], ["e","y"], ["f","z"]。
+
+
+
+ 
+
+示例 1：
+
+输入：["cba", "daf", "ghi"]
+输出：1
+解释：
+当选择 D = {1}，删除后 A 的列为：["c","d","g"] 和 ["a","f","i"]，均为非降序排列。
+若选择 D = {}，那么 A 的列 ["b","a","h"] 就不是非降序排列了。
+示例 2：
+
+输入：["a", "b"]
+输出：0
+解释：D = {}
+示例 3：
+
+输入：["zyx", "wvu", "tsr"]
+输出：3
+解释：D = {0, 1, 2}
+ 
+
+提示：
+
+1 <= A.length <= 100
+1 <= A[i].length <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/delete-columns-to-make-sorted
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& A) {
+        char previous;
+        int deleteCount = 0;
+
+        for (unsigned int i = 0; i < A[0].size(); i++) {
+            previous = 0;
+            for (unsigned int j = 0; j < A.size(); j++) {
+                if (A[j][i] < previous) {
+                    deleteCount++;
+                    break;
+                }
+
+                previous = A[j][i];
+            }
+        }
+
+        return deleteCount;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<string> A = { "cba", "daf", "ghi" };
+    check.checkInt(1, o.minDeletionSize(A));
+
+    A = { "a", "b" };
+    check.checkInt(0, o.minDeletionSize(A));
+
+    A = { "a" };
+    check.checkInt(0, o.minDeletionSize(A));
+
+    A = { "zyx", "wvu", "tsr" };
+    check.checkInt(3, o.minDeletionSize(A));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
