0b0c3e007975ac7c65c4ba7db4a4db9e9b940c0b
whuwh27604 <5991277@qq.com>
2021-01-28 09:56:04 +0800

    add

---
+++ questions/1000~1099/1025. 除数博弈/除数博弈.cpp
@@ -0,0 +1,71 @@
+﻿/* 除数博弈.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+爱丽丝和鲍勃一起玩游戏，他们轮流行动。爱丽丝先手开局。
+
+最初，黑板上有一个数字 N 。在每个玩家的回合，玩家需要执行以下操作：
+
+选出任一 x，满足 0 < x < N 且 N % x == 0 。
+用 N - x 替换黑板上的数字 N 。
+如果玩家无法执行这些操作，就会输掉游戏。
+
+只有在爱丽丝在游戏中取得胜利时才返回 True，否则返回 false。假设两个玩家都以最佳状态参与游戏。
+
+ 
+
+示例 1：
+
+输入：2
+输出：true
+解释：爱丽丝选择 1，鲍勃无法进行操作。
+示例 2：
+
+输入：3
+输出：false
+解释：爱丽丝选择 1，鲍勃也选择 1，然后爱丽丝无法进行操作。
+ 
+
+提示：
+
+1 <= N <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/divisor-game
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool divisorGame(int N) {
+        /* 结论：N是偶数一定赢，N是奇数一定输
+           1、如果N是奇数，奇数的因子一定是奇数，所以nextN一定是偶数
+           2、当N是偶数时，Alice选1变为奇数，根据1，Bob操作后一定再变为偶数，Alice再选1，最后得到2，Win
+           3、当N是奇数时，Alice操作一次后变为偶数，Bob Win */
+        return ((N % 2) == 0);
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    check.checkBool(true, o.divisorGame(2));
+    check.checkBool(false, o.divisorGame(3));
+    check.checkBool(false, o.divisorGame(1));
+    check.checkBool(true, o.divisorGame(1000));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
