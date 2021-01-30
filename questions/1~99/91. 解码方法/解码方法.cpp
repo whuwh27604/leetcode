6226611c206cdf5adc104dbb6c312949570af4b6
whuwh27604 <5991277@qq.com>
2021-01-30 17:16:17 +0800

    add

--- questions/1~99/91. 解码方法/解码方法.cpp
+++ questions/1~99/91. 解码方法/解码方法.cpp
@@ -53,49 +53,35 @@ using namespace std;
 class Solution {
 public:
     int numDecodings(string s) {
-        string newS;
-        if (!delZero(s, newS)) {
+        if (s[0] == '0') {
             return 0;
         }
 
-        int i, size = newS.size(), current = 1, post = 1;
-
-        for (i = size - 2; i >= 0; --i) {
-            if ((newS[i] == '1' && (newS[i + 1] >= '1' && newS[i + 1] <= '9')) || (newS[i] == '2' && (newS[i + 1] >= '1' && newS[i + 1] <= '6'))) {
-                current += post;
-                post = current - post;
-            }
-            else {
-                post = current;
-            }
-        }
-
-        return current;
-    }
-
-    bool delZero(string& s, string& newS) {
         int i, size = s.size();
+        vector<int> dp(size + 1);  // dp[i+1]表示s[0,i]的解码方法数
+        dp[0] = dp[1] = 1;
 
-        for (i = 0; i < size; ++i) {
+        for (i = 1; i < size; ++i) {
             if (s[i] == '0') {
-                return false;
-            }
-
-            if (s[i + 1] == '0') {
-                if (s[i] == '1' || s[i] == '2') {
-                    newS += 'a';
-                    ++i;
+                if (s[i - 1] == '1' || s[i - 1] == '2') {
+                    dp[i + 1] = dp[i - 1];  // 如果是0，前面只能是1或者2，解码数和去掉这个10或20相等
                 }
                 else {
-                    return false;
+                    return 0;  // 否则解码失败
                 }
             }
             else {
-                newS += s[i];
+                if ((s[i] >= '1' && s[i] <= '6' && (s[i - 1] == '1' || s[i - 1] == '2'))
+                    || (s[i] >= '7' && s[i] <= '9' && s[i - 1] == '1')) {
+                    dp[i + 1] = dp[i] + dp[i - 1];  // 19或26之类的有两种解码方法，s[i]单独解码，或者s[i-1,i]解码
+                }
+                else {
+                    dp[i + 1] = dp[i];  // 其它情况只能s[i]单独解码
+                }
             }
         }
 
-        return true;
+        return dp[size];
     }
 };
 
@@ -121,6 +107,9 @@ int main()
     check.checkInt(0, o.numDecodings("000"));
     check.checkInt(1, o.numDecodings("101"));
     check.checkInt(1, o.numDecodings("2101"));
+    check.checkInt(1597, o.numDecodings("1111111111111111"));
+    check.checkInt(1536, o.numDecodings("11437877141634163196491614691641649146"));
+    check.checkInt(768, o.numDecodings("12437877242634263296492624692642649246"));
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
