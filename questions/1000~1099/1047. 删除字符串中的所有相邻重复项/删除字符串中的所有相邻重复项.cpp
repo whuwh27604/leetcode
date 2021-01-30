7f3c0e712d3ffc22f4014fd3b4ee056c206fb259
whuwh27604 <5991277@qq.com>
2021-01-30 11:40:37 +0800

    add

---
+++ questions/1000~1099/1047. 删除字符串中的所有相邻重复项/删除字符串中的所有相邻重复项.cpp
@@ -0,0 +1,77 @@
+﻿/* 删除字符串中的所有相邻重复项.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
+
+在 S 上反复执行重复项删除操作，直到无法继续删除。
+
+在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+
+ 
+
+示例：
+
+输入："abbaca"
+输出："ca"
+解释：
+例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+ 
+
+提示：
+
+1 <= S.length <= 20000
+S 仅由小写英文字母组成。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <stack>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string S) {
+        string ans;
+
+        for (unsigned int i = 0; i < S.size(); i++) {
+            if (ans.empty()) {
+                ans.push_back(S[i]);
+                continue;
+            }
+
+            if (ans.back() == S[i]) {
+                ans.pop_back();
+            }
+            else {
+                ans.push_back(S[i]);
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    check.checkString("ca", o.removeDuplicates("abbaca"));
+    check.checkString("a", o.removeDuplicates("a"));
+    check.checkString("", o.removeDuplicates("abccba"));
+    check.checkString("a", o.removeDuplicates("abccbaabbaaaa"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
