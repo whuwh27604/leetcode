3ae8a7eaa72fb49537516f90be886d695cfcb485
whuwh27604 <5991277@qq.com>
2021-02-01 19:26:17 +0800

    add

---
+++ questions/1100~1199/1108. IP 地址无效化/IP 地址无效化.cpp
@@ -0,0 +1,64 @@
+﻿/* IP 地址无效化.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个有效的 IPv4 地址 address，返回这个 IP 地址的无效化版本。
+
+所谓无效化 IP 地址，其实就是用 "[.]" 代替了每个 "."。
+
+ 
+
+示例 1：
+
+输入：address = "1.1.1.1"
+输出："1[.]1[.]1[.]1"
+示例 2：
+
+输入：address = "255.100.50.0"
+输出："255[.]100[.]50[.]0"
+ 
+
+提示：
+
+给出的 address 是一个有效的 IPv4 地址
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/defanging-an-ip-address
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        for (unsigned int i = 0; i < address.size(); i++) {
+            if (address[i] == '.') {
+                address.replace(i, 1, "[.]");
+                i += 2;
+            }
+        }
+
+        return address;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    check.checkString("1[.]1[.]1[.]1", o.defangIPaddr("1.1.1.1"));
+    check.checkString("222[.]222[.]222[.]222", o.defangIPaddr("222.222.222.222"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
