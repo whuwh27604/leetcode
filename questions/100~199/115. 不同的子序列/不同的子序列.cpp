c422dfc7d8998ebbe454582c4197e6a9eb100904
whuwh27604 <5991277@qq.com>
2021-02-06 11:13:18 +0800

    add

---
+++ questions/100~199/115. 不同的子序列/不同的子序列.cpp
@@ -0,0 +1,104 @@
+﻿/* 不同的子序列.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给定一个字符串 s 和一个字符串 t ，计算在 s 的子序列中 t 出现的个数。
+
+字符串的一个 子序列 是指，通过删除一些（也可以不删除）字符且不干扰剩余字符相对位置所组成的新字符串。（例如，"ACE" 是 "ABCDE" 的一个子序列，而 "AEC" 不是）
+
+题目数据保证答案符合 32 位带符号整数范围。
+
+ 
+
+示例 1：
+
+输入：s = "rabbbit", t = "rabbit"
+输出：3
+解释：
+如下图所示, 有 3 种可以从 s 中得到 "rabbit" 的方案。
+(上箭头符号 ^ 表示选取的字母)
+rabbbit
+^^^^ ^^
+rabbbit
+^^ ^^^^
+rabbbit
+^^^ ^^^
+示例 2：
+
+输入：s = "babgbag", t = "bag"
+输出：5
+解释：
+如下图所示, 有 5 种可以从 s 中得到 "bag" 的方案。
+(上箭头符号 ^ 表示选取的字母)
+babgbag
+^^ ^
+babgbag
+^^    ^
+babgbag
+^    ^^
+babgbag
+  ^  ^^
+babgbag
+    ^^^
+ 
+
+提示：
+
+0 <= s.length, t.length <= 1000
+s 和 t 由英文字母组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/distinct-subsequences
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int i, j, sizeS = s.size(), sizeT = t.size();
+        vector<vector<long long>> dp(sizeS + 1, vector<long long>(sizeT + 1, 0));  // dp[i+1][j+1]表示s[0,i]构成t[0,j]的方法数
+        
+        for (i = 0; i <= sizeS; ++i) {
+            dp[i][0] = 1;  // t为空的方法数总是为1
+        }
+
+        for (i = 0; i < sizeS; ++i) {
+            for (j = 0; j < sizeT; ++j) {
+                if (t[j] == s[i]) {  // 最后一个字符匹配，两种方法构成t[0,j]：1、s[0,i-1]构成t[0,j-1]；2、s[0,i-1]构成t[0,j]
+                    dp[i + 1][j + 1] = dp[i][j] + dp[i][j + 1];
+                }
+                else {  // 不匹配就只能s[0,i-1]构成t[0,j]
+                    dp[i + 1][j + 1] = dp[i][j + 1];
+                }
+            }
+        }
+
+        return (int)dp[sizeS][sizeT];
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkInt(3, o.numDistinct("rabbbit", "rabbit"));
+    check.checkInt(5, o.numDistinct("babgbag", "bag"));
+    check.checkInt(1, o.numDistinct("", ""));
+    check.checkInt(0, o.numDistinct("", "a"));
+    check.checkInt(1, o.numDistinct("abc", ""));
+    check.checkInt(543744000, o.numDistinct("xslledayhxhadmctrliaxqpokyezcfhzaskeykchkmhpyjipxtsuljkwkovmvelvwxzwieeuqnjozrfwmzsylcwvsthnxujvrkszqwtglewkycikdaiocglwzukwovsghkhyidevhbgffoqkpabthmqihcfxxzdejletqjoxmwftlxfcxgxgvpperwbqvhxgsbbkmphyomtbjzdjhcrcsggleiczpbfjcgtpycpmrjnckslrwduqlccqmgrdhxolfjafmsrfdghnatexyanldrdpxvvgujsztuffoymrfteholgonuaqndinadtumnuhkboyzaqguwqijwxxszngextfcozpetyownmyneehdwqmtpjloztswmzzdzqhuoxrblppqvyvsqhnhryvqsqogpnlqfulurexdtovqpqkfxxnqykgscxaskmksivoazlducanrqxynxlgvwonalpsyddqmaemcrrwvrjmjjnygyebwtqxehrclwsxzylbqexnxjcgspeynlbmetlkacnnbhmaizbadynajpibepbuacggxrqavfnwpcwxbzxfymhjcslghmajrirqzjqxpgtgisfjreqrqabssobbadmtmdknmakdigjqyqcruujlwmfoagrckdwyiglviyyrekjealvvigiesnvuumxgsveadrxlpwetioxibtdjblowblqvzpbrmhupyrdophjxvhgzclidzybajuxllacyhyphssvhcffxonysahvzhzbttyeeyiefhunbokiqrpqfcoxdxvefugapeevdoakxwzykmhbdytjbhigffkmbqmqxsoaiomgmmgwapzdosorcxxhejvgajyzdmzlcntqbapbpofdjtulstuzdrffafedufqwsknumcxbschdybosxkrabyfdejgyozwillcxpcaiehlelczioskqtptzaczobvyojdlyflilvwqgyrqmjaeepydrcchfyftjighntqzoo", "rwmimatmhydhbujebqehjprrwfkoebcxxqfktayaaeheys"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
