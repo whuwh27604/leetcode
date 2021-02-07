8e4520b2f2e7952aa5fd09dc82c9136024ec4e3e
whuwh27604 <5991277@qq.com>
2021-02-07 10:33:09 +0800

    add

---
+++ questions/1200~1299/1266. 访问所有点的最小时间/访问所有点的最小时间.cpp
@@ -0,0 +1,81 @@
+﻿/* 访问所有点的最小时间.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+平面上有 n 个点，点的位置用整数坐标表示 points[i] = [xi, yi]。请你计算访问所有这些点需要的最小时间（以秒为单位）。
+
+你可以按照下面的规则在平面上移动：
+
+每一秒沿水平或者竖直方向移动一个单位长度，或者跨过对角线（可以看作在一秒内向水平和竖直方向各移动一个单位长度）。
+必须按照数组中出现的顺序来访问这些点。
+ 
+
+示例 1：
+
+
+
+输入：points = [[1,1],[3,4],[-1,0]]
+输出：7
+解释：一条最佳的访问路径是： [1,1] -> [2,2] -> [3,3] -> [3,4] -> [2,3] -> [1,2] -> [0,1] -> [-1,0]
+从 [1,1] 到 [3,4] 需要 3 秒
+从 [3,4] 到 [-1,0] 需要 4 秒
+一共需要 7 秒
+示例 2：
+
+输入：points = [[3,2],[-2,2]]
+输出：5
+ 
+
+提示：
+
+points.length == n
+1 <= n <= 100
+points[i].length == 2
+-1000 <= points[i][0], points[i][1] <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-time-visiting-all-points
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int i, len = points.size(), sumTime = 0;
+
+        for (i = 1; i < len; i++) {
+            sumTime += max(abs(points[i][0] - points[i - 1][0]), abs(points[i][1] - points[i - 1][1]));
+        }
+
+        return sumTime;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<vector<int>> points = { {1,1},{3,4},{-1,0} };
+    check.checkInt(7, o.minTimeToVisitAllPoints(points));
+
+    points = { {3,2},{-2,2} };
+    check.checkInt(5, o.minTimeToVisitAllPoints(points));
+
+    points = { {3,2} };
+    check.checkInt(0, o.minTimeToVisitAllPoints(points));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
