878eeb90676c8ac68a8f91d63cbc26cc7219b258
whuwh27604 <5991277@qq.com>
2021-02-16 12:01:14 +0800

    add

---
+++ questions/LCP/LCP 17. 速算机器人/速算机器人.cpp
@@ -0,0 +1,60 @@
+﻿/* 速算机器人.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+小扣在秋日市集发现了一款速算机器人。店家对机器人说出两个数字（记作 x 和 y），请小扣说出计算指令：
+
+"A" 运算：使 x = 2 * x + y；
+"B" 运算：使 y = 2 * y + x。
+在本次游戏中，店家说出的数字为 x = 1 和 y = 0，小扣说出的计算指令记作仅由大写字母 A、B 组成的字符串 s，字符串中字符的顺序表示计算顺序，请返回最终 x 与 y 的和为多少。
+
+示例 1：
+
+输入：s = "AB"
+
+输出：4
+
+解释：
+经过一次 A 运算后，x = 2, y = 0。
+再经过一次 B 运算，x = 2, y = 2。
+最终 x 与 y 之和为 4。
+
+提示：
+
+0 <= s.length <= 10
+s 由 'A' 和 'B' 组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/nGK0Fy
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int calculate(string s) {
+        return 1 << s.size();  // A或B操作都会使x+y的值翻倍
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkInt(4, o.calculate("AB"));
+    check.checkInt(256, o.calculate("ABBAAABB"));
+    check.checkInt(1, o.calculate(""));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
