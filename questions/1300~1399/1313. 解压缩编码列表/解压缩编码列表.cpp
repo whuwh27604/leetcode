31df09ae18ab7498d6255931fa2b0ceba60a5eb2
whuwh27604 <5991277@qq.com>
2021-02-18 23:23:26 +0800

    add

---
+++ questions/1300~1399/1313. 解压缩编码列表/解压缩编码列表.cpp
@@ -0,0 +1,82 @@
+﻿/* 解压缩编码列表.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个以行程长度编码压缩的整数列表 nums 。
+
+考虑每对相邻的两个元素 [freq, val] = [nums[2*i], nums[2*i+1]] （其中 i >= 0 ），每一对都表示解压后子列表中有 freq 个值为 val 的元素，你需要从左到右连接所有子列表以生成解压后的列表。
+
+请你返回解压后的列表。
+
+ 
+
+示例：
+
+输入：nums = [1,2,3,4]
+输出：[2,4,4,4]
+解释：第一对 [1,2] 代表着 2 的出现频次为 1，所以生成数组 [2]。
+第二对 [3,4] 代表着 4 的出现频次为 3，所以生成数组 [4,4,4]。
+最后将它们串联到一起 [2] + [4,4,4] = [2,4,4,4]。
+示例 2：
+
+输入：nums = [1,1,2,3]
+输出：[1,3,3]
+ 
+
+提示：
+
+2 <= nums.length <= 100
+nums.length % 2 == 0
+1 <= nums[i] <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/decompress-run-length-encoded-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> ans;
+
+        for (unsigned int i = 0; i < nums.size(); i += 2) {
+            ans.insert(ans.end(), nums[i], nums[i + 1]);
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<int> nums = { 1,2,3,4 };
+    vector<int> actual = o.decompressRLElist(nums);
+    vector<int> expected = { 2,4,4,4 };
+    check.checkIntVector(expected, actual);
+
+    nums = { 1,1,2,3 };
+    actual = o.decompressRLElist(nums);
+    expected = { 1,3,3 };
+    check.checkIntVector(expected, actual);
+
+    nums = { 10,5 };
+    actual = o.decompressRLElist(nums);
+    expected = { 5,5,5,5,5,5,5,5,5,5 };
+    check.checkIntVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
