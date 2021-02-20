149d371aaf7bb3003714e567d97aa7460a48ac69
whuwh27604 <5991277@qq.com>
2021-02-20 20:24:16 +0800

    add

---
+++ questions/1300~1399/1342. 将数字变成 0 的操作次数/将数字变成 0 的操作次数.cpp
@@ -0,0 +1,85 @@
+﻿/* 将数字变成 0 的操作次数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个非负整数 num ，请你返回将它变成 0 所需要的步数。 如果当前数字是偶数，你需要把它除以 2 ；否则，减去 1 。
+
+ 
+
+示例 1：
+
+输入：num = 14
+输出：6
+解释：
+步骤 1) 14 是偶数，除以 2 得到 7 。
+步骤 2） 7 是奇数，减 1 得到 6 。
+步骤 3） 6 是偶数，除以 2 得到 3 。
+步骤 4） 3 是奇数，减 1 得到 2 。
+步骤 5） 2 是偶数，除以 2 得到 1 。
+步骤 6） 1 是奇数，减 1 得到 0 。
+示例 2：
+
+输入：num = 8
+输出：4
+解释：
+步骤 1） 8 是偶数，除以 2 得到 4 。
+步骤 2） 4 是偶数，除以 2 得到 2 。
+步骤 3） 2 是偶数，除以 2 得到 1 。
+步骤 4） 1 是奇数，减 1 得到 0 。
+示例 3：
+
+输入：num = 123
+输出：12
+ 
+
+提示：
+
+0 <= num <= 10^6
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-steps-to-reduce-a-number-to-zero
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int steps = 0;
+
+        while (num != 0) {
+            if (num % 2 == 1) {
+                num -= 1;
+            }
+            else {
+                num /= 2;
+            }
+            steps++;
+        }
+
+        return steps;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    check.checkInt(6, o.numberOfSteps(14));
+    check.checkInt(4, o.numberOfSteps(8));
+    check.checkInt(0, o.numberOfSteps(0));
+    check.checkInt(61, o.numberOfSteps(0x7fffffff));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
