620d3590a59fb77e2d961230cac9a6e0efb5f3a3
whuwh27604 <5991277@qq.com>
2021-02-22 22:21:29 +0800

    add

---
+++ questions/1700~1799/1768. 交替合并字符串/交替合并字符串.cpp
@@ -0,0 +1,92 @@
+﻿/* 交替合并字符串.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你两个字符串 word1 和 word2 。请你从 word1 开始，通过交替添加字母来合并字符串。如果一个字符串比另一个字符串长，就将多出来的字母追加到合并后字符串的末尾。
+
+返回 合并后的字符串 。
+
+ 
+
+示例 1：
+
+输入：word1 = "abc", word2 = "pqr"
+输出："apbqcr"
+解释：字符串合并情况如下所示：
+word1：  a   b   c
+word2：    p   q   r
+合并后：  a p b q c r
+示例 2：
+
+输入：word1 = "ab", word2 = "pqrs"
+输出："apbqrs"
+解释：注意，word2 比 word1 长，"rs" 需要追加到合并后字符串的末尾。
+word1：  a   b
+word2：    p   q   r   s
+合并后：  a p b q   r   s
+示例 3：
+
+输入：word1 = "abcd", word2 = "pq"
+输出："apbqcd"
+解释：注意，word1 比 word2 长，"cd" 需要追加到合并后字符串的末尾。
+word1：  a   b   c   d
+word2：    p   q
+合并后：  a p b q c   d
+ 
+
+提示：
+
+1 <= word1.length, word2.length <= 100
+word1 和 word2 由小写英文字母组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-strings-alternately
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i, size1 = word1.size(), size2 = word2.size();
+        string merged;
+
+        for (i = 0; i < size1 && i < size2; ++i) {
+            merged += word1[i];
+            merged += word2[i];
+        }
+
+        for (; i < size1; ++i) {
+            merged += word1[i];
+        }
+
+        for (; i < size2; ++i) {
+            merged += word2[i];
+        }
+
+        return merged;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkString("apbqcr", o.mergeAlternately("abc", "pqr"));
+    check.checkString("apbqrs", o.mergeAlternately("ab", "pqrs"));
+    check.checkString("apbqcd", o.mergeAlternately("abcd", "pq"));
+    check.checkString("ab", o.mergeAlternately("a", "b"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
