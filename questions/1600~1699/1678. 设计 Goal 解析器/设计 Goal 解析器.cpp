1b9234759af5956a77bbf3aecae6855d60be7e5d
whuwh27604 <5991277@qq.com>
2021-03-05 10:08:53 +0800

    add

---
+++ questions/1600~1699/1678. 设计 Goal 解析器/设计 Goal 解析器.cpp
@@ -0,0 +1,88 @@
+﻿/* 设计 Goal 解析器.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+请你设计一个可以解释字符串 command 的 Goal 解析器 。command 由 "G"、"()" 和/或 "(al)" 按某种顺序组成。Goal 解析器会将 "G" 解释为字符串 "G"、"()" 解释为字符串 "o" ，"(al)" 解释为字符串 "al" 。然后，按原顺序将经解释得到的字符串连接成一个字符串。
+
+给你字符串 command ，返回 Goal 解析器 对 command 的解释结果。
+
+ 
+
+示例 1：
+
+输入：command = "G()(al)"
+输出："Goal"
+解释：Goal 解析器解释命令的步骤如下所示：
+G -> G
+() -> o
+(al) -> al
+最后连接得到的结果是 "Goal"
+示例 2：
+
+输入：command = "G()()()()(al)"
+输出："Gooooal"
+示例 3：
+
+输入：command = "(al)G(al)()()G"
+输出："alGalooG"
+ 
+
+提示：
+
+1 <= command.length <= 100
+command 由 "G"、"()" 和/或 "(al)" 按某种顺序组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/goal-parser-interpretation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string interpret(string command) {
+        int i, size = command.size();
+        string ans;
+
+        for (i = 0; i < size; ++i) {
+            if (command[i] == 'G') {
+                ans += 'G';
+            }
+            else {
+                if (command[i + 1] == ')') {
+                    ans += 'o';
+                    i += 1;
+                }
+                else {
+                    ans += "al";
+                    i += 3;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkString("Goal", o.interpret("G()(al)"));
+    check.checkString("Gooooal", o.interpret("G()()()()(al)"));
+    check.checkString("alGalooG", o.interpret("(al)G(al)()()G"));
+    check.checkString("G", o.interpret("G"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
