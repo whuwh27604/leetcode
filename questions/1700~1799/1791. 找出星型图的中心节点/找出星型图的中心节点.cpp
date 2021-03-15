60cca513d880850ba67f98e4ef6b6fefaa475089
whuwh27604 <5991277@qq.com>
2021-03-15 12:34:55 +0800

    add

---
+++ questions/1700~1799/1791. 找出星型图的中心节点/找出星型图的中心节点.cpp
@@ -0,0 +1,71 @@
+﻿/* 找出星型图的中心节点.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+有一个无向的 星型 图，由 n 个编号从 1 到 n 的节点组成。星型图有一个 中心 节点，并且恰有 n - 1 条边将中心节点与其他每个节点连接起来。
+
+给你一个二维整数数组 edges ，其中 edges[i] = [ui, vi] 表示在节点 ui 和 vi 之间存在一条边。请你找出并返回 edges 所表示星型图的中心节点。
+
+ 
+
+示例 1：
+
+
+输入：edges = [[1,2],[2,3],[4,2]]
+输出：2
+解释：如上图所示，节点 2 与其他每个节点都相连，所以节点 2 是中心节点。
+示例 2：
+
+输入：edges = [[1,2],[5,1],[1,3],[1,4]]
+输出：1
+ 
+
+提示：
+
+3 <= n <= 105
+edges.length == n - 1
+edges[i].length == 2
+1 <= ui, vi <= n
+ui != vi
+题目数据给出的 edges 表示一个有效的星型图
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-center-of-star-graph
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
+            return edges[0][0];
+        }
+
+        return edges[0][1];
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> edges = { {1,2},{2,3},{4,2} };
+    check.checkInt(2, o.findCenter(edges));
+
+    edges = { {1,2},{5,1},{1,3},{1,4} };
+    check.checkInt(1, o.findCenter(edges));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
