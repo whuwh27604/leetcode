a9198aeaa40cce845e2887976d91aee6d516c5c3
whuwh27604 <5991277@qq.com>
2021-03-23 14:01:47 +0800

    add

---
+++ questions/1800~1899/1801. 积压订单中的订单总数/积压订单中的订单总数.cpp
@@ -0,0 +1,145 @@
+﻿/* 积压订单中的订单总数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个二维整数数组 orders ，其中每个 orders[i] = [pricei, amounti, orderTypei] 表示有 amounti 笔类型为 orderTypei 、价格为 pricei 的订单。
+
+订单类型 orderTypei 可以分为两种：
+
+0 表示这是一批采购订单 buy
+1 表示这是一批销售订单 sell
+注意，orders[i] 表示一批共计 amounti 笔的独立订单，这些订单的价格和类型相同。对于所有有效的 i ，由 orders[i] 表示的所有订单提交时间均早于 orders[i+1] 表示的所有订单。
+
+存在由未执行订单组成的 积压订单 。积压订单最初是空的。提交订单时，会发生以下情况：
+
+如果该订单是一笔采购订单 buy ，则可以查看积压订单中价格 最低 的销售订单 sell 。如果该销售订单 sell 的价格 低于或等于 当前采购订单 buy 的价格，则匹配并执行这两笔订单，并将销售订单 sell 从积压订单中删除。否则，采购订单 buy 将会添加到积压订单中。
+反之亦然，如果该订单是一笔销售订单 sell ，则可以查看积压订单中价格 最高 的采购订单 buy 。如果该采购订单 buy 的价格 高于或等于 当前销售订单 sell 的价格，则匹配并执行这两笔订单，并将采购订单 buy 从积压订单中删除。否则，销售订单 sell 将会添加到积压订单中。
+输入所有订单后，返回积压订单中的 订单总数 。由于数字可能很大，所以需要返回对 109 + 7 取余的结果。
+
+ 
+
+示例 1：
+
+
+输入：orders = [[10,5,0],[15,2,1],[25,1,1],[30,4,0]]
+输出：6
+解释：输入订单后会发生下述情况：
+- 提交 5 笔采购订单，价格为 10 。没有销售订单，所以这 5 笔订单添加到积压订单中。
+- 提交 2 笔销售订单，价格为 15 。没有采购订单的价格大于或等于 15 ，所以这 2 笔订单添加到积压订单中。
+- 提交 1 笔销售订单，价格为 25 。没有采购订单的价格大于或等于 25 ，所以这 1 笔订单添加到积压订单中。
+- 提交 4 笔采购订单，价格为 30 。前 2 笔采购订单与价格最低（价格为 15）的 2 笔销售订单匹配，从积压订单中删除这 2 笔销售订单。第 3 笔采购订单与价格最低的 1 笔销售订单匹配，销售订单价格为 25 ，从积压订单中删除这 1 笔销售订单。积压订单中不存在更多销售订单，所以第 4 笔采购订单需要添加到积压订单中。
+最终，积压订单中有 5 笔价格为 10 的采购订单，和 1 笔价格为 30 的采购订单。所以积压订单中的订单总数为 6 。
+示例 2：
+
+
+输入：orders = [[7,1000000000,1],[15,3,0],[5,999999995,0],[5,1,1]]
+输出：999999984
+解释：输入订单后会发生下述情况：
+- 提交 109 笔销售订单，价格为 7 。没有采购订单，所以这 109 笔订单添加到积压订单中。
+- 提交 3 笔采购订单，价格为 15 。这些采购订单与价格最低（价格为 7 ）的 3 笔销售订单匹配，从积压订单中删除这 3 笔销售订单。
+- 提交 999999995 笔采购订单，价格为 5 。销售订单的最低价为 7 ，所以这 999999995 笔订单添加到积压订单中。
+- 提交 1 笔销售订单，价格为 5 。这笔销售订单与价格最高（价格为 5 ）的 1 笔采购订单匹配，从积压订单中删除这 1 笔采购订单。
+最终，积压订单中有 (1000000000-3) 笔价格为 7 的销售订单，和 (999999995-1) 笔价格为 5 的采购订单。所以积压订单中的订单总数为 1999999991 ，等于 999999984 % (109 + 7) 。
+ 
+
+提示：
+
+1 <= orders.length <= 105
+orders[i].length == 3
+1 <= pricei, amounti <= 109
+orderTypei 为 0 或 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-orders-in-the-backlog
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <queue>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int getNumberOfBacklogOrders(vector<vector<int>>& orders) {
+        priority_queue<pair<int, int>> buyOrders;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> sellOrders;
+        long long backlogOrders = 0;
+
+        for (auto& order : orders) {
+            if (order[2] == 0) {  // buy
+                while (!sellOrders.empty() && sellOrders.top().first <= order[0]) {
+                    int price = sellOrders.top().first, amount = sellOrders.top().second;
+                    sellOrders.pop();
+
+                    if (order[1] >= amount) {
+                        order[1] -= amount;
+                    }
+                    else {
+                        amount -= order[1];
+                        order[1] = 0;
+                        sellOrders.push({ price, amount });
+                        break;
+                    }
+                }
+
+                if (order[1] != 0) {
+                    buyOrders.push({ order[0], order[1] });
+                }
+            }
+            else {
+                while (!buyOrders.empty() && buyOrders.top().first >= order[0]) {
+                    int price = buyOrders.top().first, amount = buyOrders.top().second;
+                    buyOrders.pop();
+
+                    if (order[1] >= amount) {
+                        order[1] -= amount;
+                    }
+                    else {
+                        amount -= order[1];
+                        order[1] = 0;
+                        buyOrders.push({ price, amount });
+                        break;
+                    }
+                }
+
+                if (order[1] != 0) {
+                    sellOrders.push({ order[0], order[1] });
+                }
+            }
+        }
+
+        while (!buyOrders.empty()) {
+            backlogOrders += buyOrders.top().second;
+            buyOrders.pop();
+        }
+
+        while (!sellOrders.empty()) {
+            backlogOrders += sellOrders.top().second;
+            sellOrders.pop();
+        }
+
+        return (int)(backlogOrders % 1000000007);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> orders = { {10,5,0},{15,2,1},{25,1,1},{30,4,0} };
+    check.checkInt(6, o.getNumberOfBacklogOrders(orders));
+
+    orders = { {7,1000000000,1},{15,3,0},{5,999999995,0},{5,1,1} };
+    check.checkInt(999999984, o.getNumberOfBacklogOrders(orders));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
