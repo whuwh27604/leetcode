01a56bedcde9d13b65c0c9d0ee1c970d0528c44a
whuwh27604 <5991277@qq.com>
2021-03-30 22:33:17 +0800

    add

---
+++ questions/1700~1799/1720. 解码异或后的数组/解码异或后的数组.cpp
@@ -0,0 +1,79 @@
+﻿/* 解码异或后的数组.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+未知 整数数组 arr 由 n 个非负整数组成。
+
+经编码后变为长度为 n - 1 的另一个整数数组 encoded ，其中 encoded[i] = arr[i] XOR arr[i + 1] 。例如，arr = [1,0,2,1] 经编码后得到 encoded = [1,2,3] 。
+
+给你编码后的数组 encoded 和原数组 arr 的第一个元素 first（arr[0]）。
+
+请解码返回原数组 arr 。可以证明答案存在并且是唯一的。
+
+ 
+
+示例 1：
+
+输入：encoded = [1,2,3], first = 1
+输出：[1,0,2,1]
+解释：若 arr = [1,0,2,1] ，那么 first = 1 且 encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+示例 2：
+
+输入：encoded = [6,2,7,3], first = 4
+输出：[4,2,0,7,4]
+ 
+
+提示：
+
+2 <= n <= 104
+encoded.length == n - 1
+0 <= encoded[i] <= 105
+0 <= first <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/decode-xored-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> arr = { first };
+
+        for (int num : encoded) {
+            first ^= num;
+            arr.push_back(first);
+        }
+
+        return arr;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> encoded = { 1,2,3 };
+    vector<int> actual = o.decode(encoded, 1);
+    vector<int> expected = { 1,0,2,1 };
+    check.checkIntVector(expected, actual);
+
+    encoded = { 6,2,7,3 };
+    actual = o.decode(encoded, 4);
+    expected = { 4,2,0,7,4 };
+    check.checkIntVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
