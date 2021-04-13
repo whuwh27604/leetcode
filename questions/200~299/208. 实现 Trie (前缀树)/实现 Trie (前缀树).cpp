9be053532ce6bc1c0b7942b6d19a6fc09810d0e4
whuwh27604 <5991277@qq.com>
2021-04-14 00:35:29 +0800

    add

--- questions/200~299/208. 实现 Trie (前缀树)/实现 Trie (前缀树).cpp
+++ questions/200~299/208. 实现 Trie (前缀树)/实现 Trie (前缀树).cpp
@@ -1,20 +1,37 @@
 ﻿/* 实现 Trie (前缀树).cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
 
-示例:
+请你实现 Trie 类：
 
+Trie() 初始化前缀树对象。
+void insert(String word) 向前缀树中插入字符串 word 。
+boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+ 
+
+示例：
+
+输入
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+输出
+[null, null, true, false, true, null, true]
+
+解释
 Trie trie = new Trie();
-
 trie.insert("apple");
-trie.search("apple");   // 返回 true
-trie.search("app");     // 返回 false
-trie.startsWith("app"); // 返回 true
+trie.search("apple");   // 返回 True
+trie.search("app");     // 返回 False
+trie.startsWith("app"); // 返回 True
 trie.insert("app");
-trie.search("app");     // 返回 true
-说明:
+trie.search("app");     // 返回 True
+ 
 
-你可以假设所有的输入都是由小写字母 a-z 构成的。
-保证所有输入均为非空字符串。
+提示：
+
+1 <= word.length, prefix.length <= 2000
+word 和 prefix 仅由小写英文字母组成
+insert、search 和 startsWith 调用次数 总计 不超过 3 * 104 次
 
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/implement-trie-prefix-tree
@@ -45,69 +62,49 @@ public:
         root = new TrieNode;
     }
 
-    ~Trie() {
-        list<TrieNode*> nodes;
-        nodes.push_back(root);
-
-        while (!nodes.empty()) {
-            int i, j, size = nodes.size();
-            for (i = 0; i < size; i++) {
-                TrieNode* node = nodes.front();
-                nodes.pop_front();
-                for (j = 0; j < 26; j++) {
-                    if (node->next[j] != NULL) {
-                        nodes.push_back(node->next[j]);
-                    }
-                }
-
-                delete node;
-            }
-        }
-    }
-
     /** Inserts a word into the trie. */
     void insert(string word) {
-        TrieNode* nextNode = root;
+        TrieNode* current = root;
 
         for (char c : word) {
             int index = c - 'a';
-            if (nextNode->next[index] == NULL) {
-                nextNode->next[index] = new TrieNode;
+            if (current->next[index] == NULL) {
+                current->next[index] = new TrieNode;
             }
 
-            nextNode = nextNode->next[index];
+            current = current->next[index];
         }
 
-        nextNode->isEnd = true;
+        current->isEnd = true;
     }
 
     /** Returns if the word is in the trie. */
     bool search(string word) {
-        TrieNode* nextNode = root;
+        TrieNode* current = root;
 
         for (char c : word) {
             int index = c - 'a';
-            if (nextNode->next[index] == NULL) {
+            if (current->next[index] == NULL) {
                 return false;
             }
 
-            nextNode = nextNode->next[index];
+            current = current->next[index];
         }
 
-        return nextNode->isEnd;
+        return current->isEnd;
     }
 
     /** Returns if there is any word in the trie that starts with the given prefix. */
     bool startsWith(string prefix) {
-        TrieNode* nextNode = root;
+        TrieNode* current = root;
 
         for (char c : prefix) {
             int index = c - 'a';
-            if (nextNode->next[index] == NULL) {
+            if (current->next[index] == NULL) {
                 return false;
             }
 
-            nextNode = nextNode->next[index];
+            current = current->next[index];
         }
 
         return true;
