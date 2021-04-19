38e95801a189e2aae042807294f3edb8f6c1be8f
whuwh27604 <5991277@qq.com>
2021-04-19 16:49:55 +0800

    add

---
+++ questions/500~599/519. 随机翻转矩阵/随机翻转矩阵.cpp
@@ -0,0 +1,141 @@
+﻿/* 随机翻转矩阵.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+题中给出一个 n_rows 行 n_cols 列的二维矩阵，且所有值被初始化为 0。要求编写一个 flip 函数，均匀随机的将矩阵中的 0 变为 1，并返回该值的位置下标 [row_id,col_id]；同样编写一个 reset 函数，将所有的值都重新置为 0。尽量最少调用随机函数 Math.random()，并且优化时间和空间复杂度。
+
+注意:
+
+1 <= n_rows, n_cols <= 10000
+0 <= row.id < n_rows 并且 0 <= col.id < n_cols
+当矩阵中没有值为 0 时，不可以调用 flip 函数
+调用 flip 和 reset 函数的次数加起来不会超过 1000 次
+示例 1：
+
+输入:
+["Solution","flip","flip","flip","flip"]
+[[2,3],[],[],[],[]]
+输出: [null,[0,1],[1,2],[1,0],[1,1]]
+示例 2：
+
+输入:
+["Solution","flip","flip","reset","flip"]
+[[1,2],[],[],[],[]]
+输出: [null,[0,0],[0,1],null,[0,0]]
+输入语法解释：
+
+输入包含两个列表：被调用的子程序和他们的参数。Solution 的构造函数有两个参数，分别为 n_rows 和 n_cols。flip 和 reset 没有参数，参数总会以列表形式给出，哪怕该列表为空
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/random-flip-matrix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <random>
+#include <unordered_map>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    Solution(int n_rows, int n_cols) {
+        row = n_rows;
+        column = n_cols;
+        size = n_rows * n_cols;
+    }
+
+    vector<int> flip() {
+        int rnum = rd() % size--, pos;  // 随机选择一个位置
+
+        // 如果这个位置还没有被翻转，那么翻转它；如果已经被翻转了，那么翻转它映射的位置
+        pos = flipped.count(rnum) == 0 ? rnum : flipped[rnum];
+
+        // 将选中的这个位置映射到最后一个位置，如果最后一个位置已经翻转了，那么映射到它翻转时映射的位置
+        flipped[rnum] = flipped.count(size) == 0 ? size : flipped[size];
+        
+        return { pos / column, pos % column };
+    }
+
+    void reset() {
+        flipped.clear();
+        size = row * column;
+    }
+
+private:
+    random_device rd;
+    unordered_map<int, int> flipped;
+    int row;
+    int column;
+    int size;
+};
+
+int main()
+{
+    CheckResult check;
+
+    Solution o1(2, 3);
+    vector<vector<int>> expected = { {0,0},{0,1},{0,2},{1,0},{1,1},{1,2} };
+    vector<vector<int>> actual;
+    for (int i = 0; i < 6; ++i) {
+        actual.push_back(o1.flip());
+    }
+    check.checkIntVectorRandomVectorRandomOrder(expected, actual);
+    o1.reset();
+    actual.clear();
+    for (int i = 0; i < 6; ++i) {
+        actual.push_back(o1.flip());
+    }
+    check.checkIntVectorRandomVectorRandomOrder(expected, actual);
+
+    Solution o2(1, 2);
+    expected = { {0,0},{0,1} };
+    actual.clear();
+    for (int i = 0; i < 2; ++i) {
+        actual.push_back(o2.flip());
+    }
+    check.checkIntVectorRandomVectorRandomOrder(expected, actual);
+    o2.reset();
+    actual.clear();
+    for (int i = 0; i < 2; ++i) {
+        actual.push_back(o2.flip());
+    }
+    check.checkIntVectorRandomVectorRandomOrder(expected, actual);
+
+    Solution o3(1, 1);
+    expected = { {0,0} };
+    actual.clear();
+    for (int i = 0; i < 1; ++i) {
+        actual.push_back(o3.flip());
+    }
+    check.checkIntVectorRandomVectorRandomOrder(expected, actual);
+    o3.reset();
+    actual.clear();
+    for (int i = 0; i < 1; ++i) {
+        actual.push_back(o3.flip());
+    }
+    check.checkIntVectorRandomVectorRandomOrder(expected, actual);
+
+    Solution o4(3, 4);
+    expected = { {0,0},{0,1},{0,2},{0,3},{1,0},{1,1},{1,2},{1,3},{2,0},{2,1},{2,2},{2,3} };
+    actual.clear();
+    for (int i = 0; i < 12; ++i) {
+        actual.push_back(o4.flip());
+    }
+    check.checkIntVectorRandomVectorRandomOrder(expected, actual);
+    o4.reset();
+    actual.clear();
+    for (int i = 0; i < 12; ++i) {
+        actual.push_back(o4.flip());
+    }
+    check.checkIntVectorRandomVectorRandomOrder(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ questions/700~799/710. 黑名单中的随机数/黑名单中的随机数.cpp
@@ -0,0 +1,154 @@
+﻿/* 黑名单中的随机数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给定一个包含 [0，n ) 中独特的整数的黑名单 B，写一个函数从 [ 0，n ) 中返回一个不在 B 中的随机整数。
+
+对它进行优化使其尽量少调用系统方法 Math.random() 。
+
+提示:
+
+1 <= N <= 1000000000
+0 <= B.length < min(100000, N)
+[0, N) 不包含 N，详细参见 interval notation 。
+示例 1:
+
+输入:
+["Solution","pick","pick","pick"]
+[[1,[]],[],[],[]]
+输出: [null,0,0,0]
+示例 2:
+
+输入:
+["Solution","pick","pick","pick"]
+[[2,[]],[],[],[]]
+输出: [null,1,1,1]
+示例 3:
+
+输入:
+["Solution","pick","pick","pick"]
+[[3,[1]],[],[],[]]
+Output: [null,0,0,2]
+示例 4:
+
+输入:
+["Solution","pick","pick","pick"]
+[[4,[2]],[],[],[]]
+输出: [null,1,3,1]
+输入语法说明：
+
+输入是两个列表：调用成员函数名和调用的参数。Solution的构造函数有两个参数，N 和黑名单 B。pick 没有参数，输入参数是一个列表，即使参数为空，也会输入一个 [] 空列表。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/random-pick-with-blacklist
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <random>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    Solution(int N, vector<int>& blacklist) {
+        int bSize = blacklist.size();
+        n = N;
+        k = N - bSize;
+        unordered_set<int> whiteNolessK;
+
+        initWhiteNolessK(whiteNolessK);
+        eraseBlackNolessK(whiteNolessK, blacklist);
+        mappingBlack2White(whiteNolessK, blacklist, b2w);
+    }
+
+    void initWhiteNolessK(unordered_set<int>& whiteNolessK) {
+        for (int i = k; i < n; ++i) {
+            whiteNolessK.insert(i);
+        }
+    }
+
+    void eraseBlackNolessK(unordered_set<int>& whiteNolessK, vector<int>& blacklist) {
+        for (int num : blacklist) {
+            if (num >= k) {
+                whiteNolessK.erase(num);
+            }
+        }
+    }
+
+    void mappingBlack2White(unordered_set<int>& whiteNolessK, vector<int>& blacklist, unordered_map<int, int>& b2w) {
+        auto iter = whiteNolessK.begin();
+
+        for (int num : blacklist) {
+            if (num < k) {
+                b2w[num] = *iter;
+                ++iter;
+            }
+        }
+    }
+
+    int pick() {
+        int rnum = rd() % k;
+        return b2w.count(rnum) == 0 ? rnum : b2w[rnum];
+    }
+
+private:
+    int n;
+    int k;
+    unordered_map<int, int> b2w;
+    random_device rd;
+};
+
+int main()
+{
+    vector<int> blacklist;
+    Solution o1(1, blacklist);
+    cout << "{ ";
+    for (int i = 0; i < 20; ++i) {
+        cout << o1.pick() << " ";
+    }
+    cout << "}" << endl;
+
+    blacklist;
+    Solution o2(2, blacklist);
+    cout << "{ ";
+    for (int i = 0; i < 20; ++i) {
+        cout << o2.pick() << " ";
+    }
+    cout << "}" << endl;
+
+    blacklist = { 1 };
+    Solution o3(3, blacklist);
+    cout << "{ ";
+    for (int i = 0; i < 20; ++i) {
+        cout << o3.pick() << " ";
+    }
+    cout << "}" << endl;
+
+    blacklist = { 2 };
+    Solution o4(4, blacklist);
+    cout << "{ ";
+    for (int i = 0; i < 20; ++i) {
+        cout << o4.pick() << " ";
+    }
+    cout << "}" << endl;
+
+    blacklist = { 2,4 };
+    Solution o5(6, blacklist);
+    cout << "{ ";
+    for (int i = 0; i < 20; ++i) {
+        cout << o5.pick() << " ";
+    }
+    cout << "}" << endl;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
