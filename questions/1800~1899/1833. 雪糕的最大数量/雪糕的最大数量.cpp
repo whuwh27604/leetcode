a1c6dd8875416f381f6b36695a3cfedc6f9f1449
whuwh27604 <5991277@qq.com>
2021-04-20 17:30:09 +0800

    add

---
+++ questions/1800~1899/1833. 雪糕的最大数量/雪糕的最大数量.cpp
@@ -0,0 +1,92 @@
+﻿/* 雪糕的最大数量.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+夏日炎炎，小男孩 Tony 想买一些雪糕消消暑。
+
+商店中新到 n 支雪糕，用长度为 n 的数组 costs 表示雪糕的定价，其中 costs[i] 表示第 i 支雪糕的现金价格。Tony 一共有 coins 现金可以用于消费，他想要买尽可能多的雪糕。
+
+给你价格数组 costs 和现金量 coins ，请你计算并返回 Tony 用 coins 现金能够买到的雪糕的 最大数量 。
+
+注意：Tony 可以按任意顺序购买雪糕。
+
+ 
+
+示例 1：
+
+输入：costs = [1,3,2,4,1], coins = 7
+输出：4
+解释：Tony 可以买下标为 0、1、2、4 的雪糕，总价为 1 + 3 + 2 + 1 = 7
+示例 2：
+
+输入：costs = [10,6,8,7,7,8], coins = 5
+输出：0
+解释：Tony 没有足够的钱买任何一支雪糕。
+示例 3：
+
+输入：costs = [1,6,3,1,2,5], coins = 20
+输出：6
+解释：Tony 可以买下所有的雪糕，总价为 1 + 6 + 3 + 1 + 2 + 5 = 18 。
+ 
+
+提示：
+
+costs.length == n
+1 <= n <= 105
+1 <= costs[i] <= 105
+1 <= coins <= 108
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-ice-cream-bars
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int count = 0, sum = 0;
+
+        sort(costs.begin(), costs.end());
+
+        for (int cost : costs) {
+            if (sum + cost <= coins) {
+                sum += cost;
+                ++count;
+            }
+            else {
+                break;
+            }
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> costs = { 1,3,2,4,1 };
+    check.checkInt(4, o.maxIceCream(costs, 7));
+
+    costs = { 10,6,8,7,7,8 };
+    check.checkInt(0, o.maxIceCream(costs, 5));
+
+    costs = { 1,6,3,1,2,5 };
+    check.checkInt(6, o.maxIceCream(costs, 20));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
