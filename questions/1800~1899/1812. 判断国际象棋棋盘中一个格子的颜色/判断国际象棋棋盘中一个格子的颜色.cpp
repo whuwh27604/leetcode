ac9e31ab50ff6251d30024cf340a21cb5404bb32
whuwh27604 <5991277@qq.com>
2021-05-22 21:10:07 +0800

    add

---
+++ questions/1800~1899/1812. 判断国际象棋棋盘中一个格子的颜色/判断国际象棋棋盘中一个格子的颜色.cpp
@@ -0,0 +1,70 @@
+﻿/* 判断国际象棋棋盘中一个格子的颜色.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个坐标 coordinates ，它是一个字符串，表示国际象棋棋盘中一个格子的坐标。下图是国际象棋棋盘示意图。
+
+
+
+如果所给格子的颜色是白色，请你返回 true，如果是黑色，请返回 false 。
+
+给定坐标一定代表国际象棋棋盘上一个存在的格子。坐标第一个字符是字母，第二个字符是数字。
+
+ 
+
+示例 1：
+
+输入：coordinates = "a1"
+输出：false
+解释：如上图棋盘所示，"a1" 坐标的格子是黑色的，所以返回 false 。
+示例 2：
+
+输入：coordinates = "h3"
+输出：true
+解释：如上图棋盘所示，"h3" 坐标的格子是白色的，所以返回 true 。
+示例 3：
+
+输入：coordinates = "c7"
+输出：false
+ 
+
+提示：
+
+coordinates.length == 2
+'a' <= coordinates[0] <= 'h'
+'1' <= coordinates[1] <= '8'
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/determine-color-of-a-chessboard-square
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        return ((coordinates[0] - 'a' + coordinates[1] - '1') & 1) == 1;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkBool(false, o.squareIsWhite("a1"));
+    check.checkBool(true, o.squareIsWhite("h3"));
+    check.checkBool(false, o.squareIsWhite("c7"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
