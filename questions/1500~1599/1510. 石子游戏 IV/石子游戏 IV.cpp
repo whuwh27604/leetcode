aaafb5a024e2576e633508327bd7a865b80ef5b3
whuwh27604 <5991277@qq.com>
2021-06-05 13:10:05 +0800

    add

---
+++ questions/1500~1599/1510. 石子游戏 IV/石子游戏 IV.cpp
@@ -0,0 +1,96 @@
+﻿/* 石子游戏 IV.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+Alice 和 Bob 两个人轮流玩一个游戏，Alice 先手。
+
+一开始，有 n 个石子堆在一起。每个人轮流操作，正在操作的玩家可以从石子堆里拿走 任意 非零 平方数 个石子。
+
+如果石子堆里没有石子了，则无法操作的玩家输掉游戏。
+
+给你正整数 n ，且已知两个人都采取最优策略。如果 Alice 会赢得比赛，那么返回 True ，否则返回 False 。
+
+ 
+
+示例 1：
+
+输入：n = 1
+输出：true
+解释：Alice 拿走 1 个石子并赢得胜利，因为 Bob 无法进行任何操作。
+示例 2：
+
+输入：n = 2
+输出：false
+解释：Alice 只能拿走 1 个石子，然后 Bob 拿走最后一个石子并赢得胜利（2 -> 1 -> 0）。
+示例 3：
+
+输入：n = 4
+输出：true
+解释：n 已经是一个平方数，Alice 可以一次全拿掉 4 个石子并赢得胜利（4 -> 0）。
+示例 4：
+
+输入：n = 7
+输出：false
+解释：当 Bob 采取最优策略时，Alice 无法赢得比赛。
+如果 Alice 一开始拿走 4 个石子， Bob 会拿走 1 个石子，然后 Alice 只能拿走 1 个石子，Bob 拿走最后一个石子并赢得胜利（7 -> 3 -> 2 -> 1 -> 0）。
+如果 Alice 一开始拿走 1 个石子， Bob 会拿走 4 个石子，然后 Alice 只能拿走 1 个石子，Bob 拿走最后一个石子并赢得胜利（7 -> 6 -> 2 -> 1 -> 0）。
+示例 5：
+
+输入：n = 17
+输出：false
+解释：如果 Bob 采取最优策略，Alice 无法赢得胜利。
+ 
+
+提示：
+
+1 <= n <= 10^5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/stone-game-iv
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n + 1, false);
+        dp[1] = true;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j * j <= i; ++j) {
+                if (dp[i - j * j] == false) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkBool(true, o.winnerSquareGame(1));
+    check.checkBool(false, o.winnerSquareGame(2));
+    check.checkBool(true, o.winnerSquareGame(4));
+    check.checkBool(false, o.winnerSquareGame(7));
+    check.checkBool(false, o.winnerSquareGame(17));
+    check.checkBool(true, o.winnerSquareGame(100000));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
