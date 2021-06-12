3e1b6f5de4d4f5b742a540dc667a3eace25ddcd6
whuwh27604 <5991277@qq.com>
2021-06-12 12:27:44 +0800

    add

---
+++ questions/1800~1899/1844. 将所有数字用字符替换/将所有数字用字符替换.cpp
@@ -0,0 +1,79 @@
+﻿/* 将所有数字用字符替换.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的字符串 s ，它的 偶数 下标处为小写英文字母，奇数 下标处为数字。
+
+定义一个函数 shift(c, x) ，其中 c 是一个字符且 x 是一个数字，函数返回字母表中 c 后面第 x 个字符。
+
+比方说，shift('a', 5) = 'f' 和 shift('x', 0) = 'x' 。
+对于每个 奇数 下标 i ，你需要将数字 s[i] 用 shift(s[i-1], s[i]) 替换。
+
+请你替换所有数字以后，将字符串 s 返回。题目 保证 shift(s[i-1], s[i]) 不会超过 'z' 。
+
+ 
+
+示例 1：
+
+输入：s = "a1c1e1"
+输出："abcdef"
+解释：数字被替换结果如下：
+- s[1] -> shift('a',1) = 'b'
+- s[3] -> shift('c',1) = 'd'
+- s[5] -> shift('e',1) = 'f'
+示例 2：
+
+输入：s = "a1b2c3d4e"
+输出："abbdcfdhe"
+解释：数字被替换结果如下：
+- s[1] -> shift('a',1) = 'b'
+- s[3] -> shift('b',2) = 'd'
+- s[5] -> shift('c',3) = 'f'
+- s[7] -> shift('d',4) = 'h'
+ 
+
+提示：
+
+1 <= s.length <= 100
+s 只包含小写英文字母和数字。
+对所有 奇数 下标处的 i ，满足 shift(s[i-1], s[i]) <= 'z' 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/replace-all-digits-with-characters
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string replaceDigits(string s) {
+        for (int i = 1; i < (int)s.size(); i += 2) {
+            s[i] = s[i] - '0' + s[i - 1];
+        }
+
+        return s;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkString("abcdef", o.replaceDigits("a1c1e1"));
+    check.checkString("abbdcfdhe", o.replaceDigits("a1b2c3d4e"));
+    check.checkString("z", o.replaceDigits("z"));
+    check.checkString("zz", o.replaceDigits("z0"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
