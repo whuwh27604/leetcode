557b4cb6d8d79909dcd612eda8686cfa17d3f602
whuwh27604 <5991277@qq.com>
2021-06-24 16:56:49 +0800

    add

---
+++ questions/1800~1899/1828. 统计一个圆中点的数目/统计一个圆中点的数目.cpp
@@ -0,0 +1,99 @@
+﻿/* 统计一个圆中点的数目.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个数组 points ，其中 points[i] = [xi, yi] ，表示第 i 个点在二维平面上的坐标。多个点可能会有 相同 的坐标。
+
+同时给你一个数组 queries ，其中 queries[j] = [xj, yj, rj] ，表示一个圆心在 (xj, yj) 且半径为 rj 的圆。
+
+对于每一个查询 queries[j] ，计算在第 j 个圆 内 点的数目。如果一个点在圆的 边界上 ，我们同样认为它在圆 内 。
+
+请你返回一个数组 answer ，其中 answer[j]是第 j 个查询的答案。
+
+ 
+
+示例 1：
+
+
+输入：points = [[1,3],[3,3],[5,3],[2,2]], queries = [[2,3,1],[4,3,1],[1,1,2]]
+输出：[3,2,2]
+解释：所有的点和圆如上图所示。
+queries[0] 是绿色的圆，queries[1] 是红色的圆，queries[2] 是蓝色的圆。
+示例 2：
+
+
+输入：points = [[1,1],[2,2],[3,3],[4,4],[5,5]], queries = [[1,2,2],[2,2,2],[4,3,2],[4,3,3]]
+输出：[2,3,2,4]
+解释：所有的点和圆如上图所示。
+queries[0] 是绿色的圆，queries[1] 是红色的圆，queries[2] 是蓝色的圆，queries[3] 是紫色的圆。
+ 
+
+提示：
+
+1 <= points.length <= 500
+points[i].length == 2
+0 <= x​​​​​​i, y​​​​​​i <= 500
+1 <= queries.length <= 500
+queries[j].length == 3
+0 <= xj, yj <= 500
+1 <= rj <= 500
+所有的坐标都是整数。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/queries-on-number-of-points-inside-a-circle
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        vector<int> ans;
+
+        for (auto& query : queries) {
+            int count = 0;
+
+            for (auto& point : points) {
+                int x = query[0] - point[0], y = query[1] - point[1], sqrR = query[2] * query[2];
+
+                if (x * x + y * y <= sqrR) {
+                    ++count;
+                }
+            }
+
+            ans.push_back(count);
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> points = { {1,3},{3,3},{5,3},{2,2} };
+    vector<vector<int>> queries = { {2,3,1},{4,3,1},{1,1,2} };
+    vector<int> actual = o.countPoints(points, queries);
+    vector<int> expected = { 3,2,2 };
+    check.checkIntVector(expected, actual);
+
+    points = { {1,1},{2,2},{3,3},{4,4},{5,5} };
+    queries = { {1,2,2},{2,2,2},{4,3,2},{4,3,3} };
+    actual = o.countPoints(points, queries);
+    expected = { 2,3,2,4 };
+    check.checkIntVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
