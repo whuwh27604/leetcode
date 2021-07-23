353957b7b17799b2af32323bc9c02d753c4e2122
whuwh27604 <5991277@qq.com>
2021-07-23 18:41:52 +0800

    add

---
+++ questions/1800~1899/1893. 检查是否区域内所有整数都被覆盖/检查是否区域内所有整数都被覆盖.cpp
@@ -0,0 +1,92 @@
+﻿/* 检查是否区域内所有整数都被覆盖.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个二维整数数组 ranges 和两个整数 left 和 right 。每个 ranges[i] = [starti, endi] 表示一个从 starti 到 endi 的 闭区间 。
+
+如果闭区间 [left, right] 内每个整数都被 ranges 中 至少一个 区间覆盖，那么请你返回 true ，否则返回 false 。
+
+已知区间 ranges[i] = [starti, endi] ，如果整数 x 满足 starti <= x <= endi ，那么我们称整数x 被覆盖了。
+
+ 
+
+示例 1：
+
+输入：ranges = [[1,2],[3,4],[5,6]], left = 2, right = 5
+输出：true
+解释：2 到 5 的每个整数都被覆盖了：
+- 2 被第一个区间覆盖。
+- 3 和 4 被第二个区间覆盖。
+- 5 被第三个区间覆盖。
+示例 2：
+
+输入：ranges = [[1,10],[10,20]], left = 21, right = 21
+输出：false
+解释：21 没有被任何一个区间覆盖。
+ 
+
+提示：
+
+1 <= ranges.length <= 50
+1 <= starti <= endi <= 50
+1 <= left <= right <= 50
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/check-if-all-the-integers-in-a-range-are-covered
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+        vector<int> nums(52, 0);
+
+        for (auto& range : ranges) {
+            ++nums[range[0]];
+            --nums[range[1] + 1];
+        }
+
+        for (int i = 1; i < 51; ++i) {
+            nums[i] += nums[i - 1];
+        }
+
+        for (int i = left; i <= right; ++i) {
+            if (nums[i] == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> ranges = { {1,2},{3,4},{5,6} };
+    check.checkBool(true, o.isCovered(ranges, 2, 5));
+
+    ranges = { {1,10},{10,20} };
+    check.checkBool(false, o.isCovered(ranges, 21, 21));
+
+    ranges = { {1,1} };
+    check.checkBool(false, o.isCovered(ranges, 1, 50));
+
+    ranges = { {1,50} };
+    check.checkBool(true, o.isCovered(ranges, 1, 50));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
