6c583c4325d6f3f9aa945c24b3820ef73cae7411
whuwh27604 <5991277@qq.com>
2021-07-24 10:37:34 +0800

    add

---
+++ questions/1900~1999/1929. 数组串联/数组串联.cpp
@@ -0,0 +1,79 @@
+﻿/* 数组串联.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个长度为 n 的整数数组 nums 。请你构建一个长度为 2n 的答案数组 ans ，数组下标 从 0 开始计数 ，对于所有 0 <= i < n 的 i ，满足下述所有要求：
+
+ans[i] == nums[i]
+ans[i + n] == nums[i]
+具体而言，ans 由两个 nums 数组 串联 形成。
+
+返回数组 ans 。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,1]
+输出：[1,2,1,1,2,1]
+解释：数组 ans 按下述方式形成：
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+示例 2：
+
+输入：nums = [1,3,2,1]
+输出：[1,3,2,1,1,3,2,1]
+解释：数组 ans 按下述方式形成：
+- ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+- ans = [1,3,2,1,1,3,2,1]
+ 
+
+提示：
+
+n == nums.length
+1 <= n <= 1000
+1 <= nums[i] <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/concatenation-of-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans = nums;
+        ans.insert(ans.end(), nums.begin(), nums.end());
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,2,1 };
+    vector<int> actual = o.getConcatenation(nums);
+    vector<int> expected = { 1,2,1,1,2,1 };
+    check.checkIntVector(expected, actual);
+
+    nums = { 1,3,2,1 };
+    actual = o.getConcatenation(nums);
+    expected = { 1,3,2,1,1,3,2,1 };
+    check.checkIntVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
