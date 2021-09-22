e2aaaab12916ee83d17fe76fd5a28df746f71984
whuwh27604 <5991277@qq.com>
2021-09-22 14:06:26 +0800

    add

---
+++ questions/2000~2099/2012. 数组美丽值求和/数组美丽值求和.cpp
@@ -0,0 +1,107 @@
+﻿/* 数组美丽值求和.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums 。对于每个下标 i（1 <= i <= nums.length - 2），nums[i] 的 美丽值 等于：
+
+2，对于所有 0 <= j < i 且 i < k <= nums.length - 1 ，满足 nums[j] < nums[i] < nums[k]
+1，如果满足 nums[i - 1] < nums[i] < nums[i + 1] ，且不满足前面的条件
+0，如果上述条件全部不满足
+返回符合 1 <= i <= nums.length - 2 的所有 nums[i] 的 美丽值的总和 。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,3]
+输出：2
+解释：对于每个符合范围 1 <= i <= 1 的下标 i :
+- nums[1] 的美丽值等于 2
+示例 2：
+
+输入：nums = [2,4,6,4]
+输出：1
+解释：对于每个符合范围 1 <= i <= 2 的下标 i :
+- nums[1] 的美丽值等于 1
+- nums[2] 的美丽值等于 0
+示例 3：
+
+输入：nums = [3,2,1]
+输出：0
+解释：对于每个符合范围 1 <= i <= 1 的下标 i :
+- nums[1] 的美丽值等于 0
+ 
+
+提示：
+
+3 <= nums.length <= 105
+1 <= nums[i] <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sum-of-beauty-in-the-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int sumOfBeauties(vector<int>& nums) {
+        int i, size = nums.size(), maximum = nums[0], minimum = nums[size - 1], sum = 0;
+        vector<bool> isMax(size, false), isMin(size, false);
+
+        for (i = 1; i < size - 1; ++i) {
+            if (nums[i] > maximum) {
+                maximum = nums[i];
+                isMax[i] = true;
+            }
+        }
+
+        for (i = size - 2; i > 0; --i) {
+            if (nums[i] < minimum) {
+                minimum = nums[i];
+                isMin[i] = true;
+            }
+        }
+
+        for (i = 1; i < size - 1; ++i) {
+            if (isMax[i] && isMin[i]) {
+                sum += 2;
+            }
+            else if (nums[i] > nums[i - 1] && nums[i] < nums[i + 1]) {
+                sum += 1;
+            }
+        }
+
+        return sum;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,2,3 };
+    check.checkInt(2, o.sumOfBeauties(nums));
+
+    nums = { 2,4,6,4 };
+    check.checkInt(1, o.sumOfBeauties(nums));
+
+    nums = { 3,2,1 };
+    check.checkInt(0, o.sumOfBeauties(nums));
+
+    nums = { 1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1 };
+    check.checkInt(21, o.sumOfBeauties(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
