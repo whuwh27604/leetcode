d890bc7ff9a1c987415a7a437997d5cb749ae4b0
whuwh27604 <5991277@qq.com>
2021-10-09 19:16:57 +0800

    add

---
+++ questions/2000~2099/2023. 连接后等于目标字符串的字符串对/连接后等于目标字符串的字符串对.cpp
@@ -0,0 +1,115 @@
+﻿/* 连接后等于目标字符串的字符串对.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个 数字 字符串数组 nums 和一个 数字 字符串 target ，请你返回 nums[i] + nums[j] （两个字符串连接）结果等于 target 的下标 (i, j) （需满足 i != j）的数目。
+
+ 
+
+示例 1：
+
+输入：nums = ["777","7","77","77"], target = "7777"
+输出：4
+解释：符合要求的下标对包括：
+- (0, 1)："777" + "7"
+- (1, 0)："7" + "777"
+- (2, 3)："77" + "77"
+- (3, 2)："77" + "77"
+示例 2：
+
+输入：nums = ["123","4","12","34"], target = "1234"
+输出：2
+解释：符合要求的下标对包括
+- (0, 1)："123" + "4"
+- (2, 3)："12" + "34"
+示例 3：
+
+输入：nums = ["1","1","1"], target = "11"
+输出：6
+解释：符合要求的下标对包括
+- (0, 1)："1" + "1"
+- (1, 0)："1" + "1"
+- (0, 2)："1" + "1"
+- (2, 0)："1" + "1"
+- (1, 2)："1" + "1"
+- (2, 1)："1" + "1"
+ 
+
+提示：
+
+2 <= nums.length <= 100
+1 <= nums[i].length <= 100
+2 <= target.length <= 100
+nums[i] 和 target 只包含数字。
+nums[i] 和 target 不含有任何前导 0 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-pairs-of-strings-with-concatenation-equal-to-target
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int numOfPairs(vector<string>& nums, string target) {
+        int i, size = target.size(), pairs = 0, halfTargetCount = 0;
+        string half1(target, 0, size / 2), half2(target, size / 2, size - size / 2), halfTarget = half1 == half2 ? half1 : "";
+        vector<vector<string>> classifiedNums(size);
+
+        for (string& num : nums) {
+            if ((int)num.size() < size) {
+                classifiedNums[num.size()].push_back(num);
+            }
+
+            if (num == halfTarget) {
+                ++halfTargetCount;
+            }
+        }
+
+        for (i = 1; i < size; ++i) {
+            for (string& num1 : classifiedNums[i]) {
+                for (string& num2 : classifiedNums[size - i]) {
+                    string num = num1 + num2;
+                    if (num == target) {
+                        ++pairs;
+                    }
+                }
+            }
+        }
+
+        return pairs - halfTargetCount;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<string> nums = { "777","7","77","77" };
+    check.checkInt(4, o.numOfPairs(nums, "7777"));
+
+    nums = { "123","4","12","34" };
+    check.checkInt(2, o.numOfPairs(nums, "1234"));
+
+    nums = { "1","1","1" };
+    check.checkInt(6, o.numOfPairs(nums, "11"));
+
+    nums = { "77777" };
+    check.checkInt(0, o.numOfPairs(nums, "7777"));
+
+    nums = { "7","7","7","7","7","7","7","7","7","7" };
+    check.checkInt(90, o.numOfPairs(nums, "77"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ questions/2000~2099/2024. 考试的最大困扰度/考试的最大困扰度.cpp
@@ -0,0 +1,94 @@
+﻿/* 考试的最大困扰度.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+一位老师正在出一场由 n 道判断题构成的考试，每道题的答案为 true （用 'T' 表示）或者 false （用 'F' 表示）。老师想增加学生对自己做出答案的不确定性，方法是 最大化 有 连续相同 结果的题数。（也就是连续出现 true 或者连续出现 false）。
+
+给你一个字符串 answerKey ，其中 answerKey[i] 是第 i 个问题的正确结果。除此以外，还给你一个整数 k ，表示你能进行以下操作的最多次数：
+
+每次操作中，将问题的正确答案改为 'T' 或者 'F' （也就是将 answerKey[i] 改为 'T' 或者 'F' ）。
+请你返回在不超过 k 次操作的情况下，最大 连续 'T' 或者 'F' 的数目。
+
+ 
+
+示例 1：
+
+输入：answerKey = "TTFF", k = 2
+输出：4
+解释：我们可以将两个 'F' 都变为 'T' ，得到 answerKey = "TTTT" 。
+总共有四个连续的 'T' 。
+示例 2：
+
+输入：answerKey = "TFFT", k = 1
+输出：3
+解释：我们可以将最前面的 'T' 换成 'F' ，得到 answerKey = "FFFT" 。
+或者，我们可以将第二个 'T' 换成 'F' ，得到 answerKey = "TFFF" 。
+两种情况下，都有三个连续的 'F' 。
+示例 3：
+
+输入：answerKey = "TTFTTFTT", k = 1
+输出：5
+解释：我们可以将第一个 'F' 换成 'T' ，得到 answerKey = "TTTTTFTT" 。
+或者我们可以将第二个 'F' 换成 'T' ，得到 answerKey = "TTFTTTTT" 。
+两种情况下，都有五个连续的 'T' 。
+ 
+
+提示：
+
+n == answerKey.length
+1 <= n <= 5 * 104
+answerKey[i] 要么是 'T' ，要么是 'F'
+1 <= k <= n
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximize-the-confusion-of-an-exam
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        return max(maxConsecutiveAnswers(answerKey, k + 1, 'T'), maxConsecutiveAnswers(answerKey, k + 1, 'F'));
+    }
+
+    int maxConsecutiveAnswers(string answerKey, int window, char change) {
+        int left = -1, right = 0, size = answerKey.size(), count = 0, maxConsecutive = 0;
+
+        for (right = 0; right < size; ++right) {
+            if (answerKey[right] == change) {
+                if (++count >= window) {
+                    maxConsecutive = max(maxConsecutive, right - left - 1);
+                    for (left += 1; answerKey[left] != change; ++left);
+                }
+            }
+        }
+
+        return max(maxConsecutive, right - left - 1);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkInt(4, o.maxConsecutiveAnswers("TTFF", 2));
+    check.checkInt(3, o.maxConsecutiveAnswers("TFFT", 1));
+    check.checkInt(5, o.maxConsecutiveAnswers("TTFTTFTT", 1));
+    check.checkInt(1, o.maxConsecutiveAnswers("T", 1));
+    check.checkInt(34, o.maxConsecutiveAnswers("TFFTFFTFTFTFTTTTTTTTTTTTFTFTFTFTFTFFFFFFFFFTFTFTTFFTFTFTFTTTTTTTTTTTTTTFTFFTFTFTFTFTTFTFTFTFTFTFTTF", 10));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
