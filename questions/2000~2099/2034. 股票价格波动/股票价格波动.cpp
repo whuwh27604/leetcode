cdf38545074ec51936c6c0f1a478983ea7756be9
whuwh27604 <5991277@qq.com>
2021-10-12 10:35:35 +0800

    add

---
+++ questions/2000~2099/2034. 股票价格波动/股票价格波动.cpp
@@ -0,0 +1,135 @@
+﻿/* 股票价格波动.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一支股票价格的数据流。数据流中每一条记录包含一个 时间戳 和该时间点股票对应的 价格 。
+
+不巧的是，由于股票市场内在的波动性，股票价格记录可能不是按时间顺序到来的。某些情况下，有的记录可能是错的。如果两个有相同时间戳的记录出现在数据流中，前一条记录视为错误记录，后出现的记录 更正 前一条错误的记录。
+
+请你设计一个算法，实现：
+
+更新 股票在某一时间戳的股票价格，如果有之前同一时间戳的价格，这一操作将 更正 之前的错误价格。
+找到当前记录里 最新股票价格 。最新股票价格 定义为时间戳最晚的股票价格。
+找到当前记录里股票的 最高价格 。
+找到当前记录里股票的 最低价格 。
+请你实现 StockPrice 类：
+
+StockPrice() 初始化对象，当前无股票价格记录。
+void update(int timestamp, int price) 在时间点 timestamp 更新股票价格为 price 。
+int current() 返回股票 最新价格 。
+int maximum() 返回股票 最高价格 。
+int minimum() 返回股票 最低价格 。
+ 
+
+示例 1：
+
+输入：
+["StockPrice", "update", "update", "current", "maximum", "update", "maximum", "update", "minimum"]
+[[], [1, 10], [2, 5], [], [], [1, 3], [], [4, 2], []]
+输出：
+[null, null, null, 5, 10, null, 5, null, 2]
+
+解释：
+StockPrice stockPrice = new StockPrice();
+stockPrice.update(1, 10); // 时间戳为 [1] ，对应的股票价格为 [10] 。
+stockPrice.update(2, 5);  // 时间戳为 [1,2] ，对应的股票价格为 [10,5] 。
+stockPrice.current();     // 返回 5 ，最新时间戳为 2 ，对应价格为 5 。
+stockPrice.maximum();     // 返回 10 ，最高价格的时间戳为 1 ，价格为 10 。
+stockPrice.update(1, 3);  // 之前时间戳为 1 的价格错误，价格更新为 3 。
+                          // 时间戳为 [1,2] ，对应股票价格为 [3,5] 。
+stockPrice.maximum();     // 返回 5 ，更正后最高价格为 5 。
+stockPrice.update(4, 2);  // 时间戳为 [1,2,4] ，对应价格为 [3,5,2] 。
+stockPrice.minimum();     // 返回 2 ，最低价格时间戳为 4 ，价格为 2 。
+ 
+
+提示：
+
+1 <= timestamp, price <= 109
+update，current，maximum 和 minimum 总 调用次数不超过 105 。
+current，maximum 和 minimum 被调用时，update 操作 至少 已经被调用过 一次 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/stock-price-fluctuation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include <set>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Stock {
+public:
+    int timestamp;
+    int price;
+
+    Stock() : timestamp(0), price(0) {}
+    Stock(int t, int p) : timestamp(t), price(p) {}
+
+    bool operator< (const Stock& s) const {
+        return price == s.price ? timestamp < s.timestamp : price < s.price;
+    }
+};
+
+class StockPrice {
+public:
+    StockPrice() {
+        maxTimestamp = 0;
+    }
+
+    void update(int timestamp, int price) {
+        if (timestampNodes.count(timestamp) != 0) {
+            Stock stock = timestampNodes[timestamp];
+            nodes.erase(stock);
+            timestampNodes.erase(timestamp);
+        }
+
+        Stock stock(timestamp, price);
+        timestampNodes[timestamp] = stock;
+        nodes.insert(stock);
+        maxTimestamp = max(maxTimestamp, timestamp);
+    }
+
+    int current() {
+        return timestampNodes[maxTimestamp].price;
+    }
+
+    int maximum() {
+        return (--nodes.end())->price;
+    }
+
+    int minimum() {
+        return nodes.begin()->price;
+    }
+
+private:
+    int maxTimestamp;
+    unordered_map<int, Stock> timestampNodes;
+    set<Stock> nodes;
+};
+
+int main()
+{
+    CheckResult check;
+
+    StockPrice o1;
+    o1.update(1, 10);
+    o1.update(2, 5);
+    check.checkInt(5, o1.current());
+    check.checkInt(10, o1.maximum());
+    o1.update(1, 3);
+    check.checkInt(5, o1.maximum());
+    o1.update(4, 2);
+    check.checkInt(2, o1.minimum());
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
