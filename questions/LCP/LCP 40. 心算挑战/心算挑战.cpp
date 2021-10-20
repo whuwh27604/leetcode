281e9eaa7e404f0d351bc00ced0346c9d0a02093
whuwh27604 <5991277@qq.com>
2021-10-20 12:30:22 +0800

    add

---
+++ questions/LCP/LCP 40. 心算挑战/心算挑战.cpp
@@ -0,0 +1,121 @@
+﻿/* 心算挑战.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+「力扣挑战赛」心算项目的挑战比赛中，要求选手从 N 张卡牌中选出 cnt 张卡牌，若这 cnt 张卡牌数字总和为偶数，则选手成绩「有效」且得分为 cnt 张卡牌数字总和。
+给定数组 cards 和 cnt，其中 cards[i] 表示第 i 张卡牌上的数字。 请帮参赛选手计算最大的有效得分。若不存在获取有效得分的卡牌方案，则返回 0。
+
+示例 1：
+
+输入：cards = [1,2,8,9], cnt = 3
+
+输出：18
+
+解释：选择数字为 1、8、9 的这三张卡牌，此时可获得最大的有效得分 1+8+9=18。
+
+示例 2：
+
+输入：cards = [3,3,1], cnt = 1
+
+输出：0
+
+解释：不存在获取有效得分的卡牌方案。
+
+提示：
+
+1 <= cnt <= cards.length <= 10^5
+1 <= cards[i] <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/uOAnQW
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxmiumScore(vector<int>& cards, int cnt) {
+        vector<int> odds, evens;
+
+        for (int card : cards) {
+            ((card & 1) == 1) ? odds.push_back(card) : evens.push_back(card);
+        }
+
+        sort(odds.begin(), odds.end());
+        sort(evens.begin(), evens.end());
+        int i = odds.size() - 1, j = evens.size() - 1, score = 0;
+
+        if ((cnt & 1) == 1) {
+            if (j >= 0) {
+                score += evens[j--];
+                --cnt;
+            }
+            else {
+                return 0;
+            }
+        }
+
+        while (cnt != 0) {
+            int twoOddSum = 0, twoEvenSum = 0;
+
+            if (i > 0) {
+                twoOddSum = odds[i] + odds[i - 1];
+            }
+
+            if (j > 0) {
+                twoEvenSum = evens[j] + evens[j - 1];
+            }
+
+            if (twoOddSum == 0 && twoEvenSum == 0) {
+                return 0;
+            }
+
+            if (twoOddSum > twoEvenSum) {
+                score += twoOddSum;
+                i -= 2;
+            }
+            else {
+                score += twoEvenSum;
+                j -= 2;
+            }
+
+            cnt -= 2;
+        }
+
+        return score;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> cards = { 1,2,8,9 };
+    check.checkInt(18, o.maxmiumScore(cards, 3));
+
+    cards = { 3,3,1 };
+    check.checkInt(0, o.maxmiumScore(cards, 1));
+
+    cards = { 1 };
+    check.checkInt(0, o.maxmiumScore(cards, 1));
+
+    cards = { 2 };
+    check.checkInt(2, o.maxmiumScore(cards, 1));
+
+    cards = { 14,6,8,4,4,3,12,12,14,13,7,12,13,12,15,9,19,14,10,2,16,1,19,18,18,2,17,3,15,1,3,19,19,15,8,11,1,12,6,20,18,8,16,19,3,18,5,12,9,8,2,7,1,13,20,11,6,13,13,3,7,1,13,1,18,18,15,12,18,11,7,18,9,19,17,4,20,9,7,5,14,19 };
+    check.checkInt(892, o.maxmiumScore(cards, 80));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ questions/LCS/LCS 01. 下载插件/下载插件.cpp
@@ -0,0 +1,86 @@
+﻿/* 下载插件.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+小扣打算给自己的 VS code 安装使用插件，初始状态下带宽每分钟可以完成 1 个插件的下载。假定每分钟选择以下两种策略之一:
+
+使用当前带宽下载插件
+将带宽加倍（下载插件数量随之加倍）
+请返回小扣完成下载 n 个插件最少需要多少分钟。
+
+注意：实际的下载的插件数量可以超过 n 个
+
+示例 1：
+
+输入：n = 2
+
+输出：2
+
+解释：
+以下两个方案，都能实现 2 分钟内下载 2 个插件
+
+方案一：第一分钟带宽加倍，带宽可每分钟下载 2 个插件；第二分钟下载 2 个插件
+方案二：第一分钟下载 1 个插件，第二分钟下载 1 个插件
+示例 2：
+
+输入：n = 4
+
+输出：3
+
+解释：
+最少需要 3 分钟可完成 4 个插件的下载，以下是其中一种方案:
+第一分钟带宽加倍，带宽可每分钟下载 2 个插件;
+第二分钟下载 2 个插件;
+第三分钟下载 2 个插件。
+
+提示：
+
+1 <= n <= 10^5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/Ju9Xwi
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int leastMinutes(int n) {
+        int minutes = 1, num = 1;
+
+        while (num < n) {
+            num <<= 1;
+            ++minutes;
+        }
+
+        return minutes;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkInt(1, o.leastMinutes(1));
+    check.checkInt(2, o.leastMinutes(2));
+    check.checkInt(3, o.leastMinutes(4));
+    check.checkInt(4, o.leastMinutes(8));
+    check.checkInt(5, o.leastMinutes(10));
+    check.checkInt(8, o.leastMinutes(100));
+    check.checkInt(11, o.leastMinutes(1000));
+    check.checkInt(15, o.leastMinutes(10000));
+    check.checkInt(18, o.leastMinutes(100000));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
