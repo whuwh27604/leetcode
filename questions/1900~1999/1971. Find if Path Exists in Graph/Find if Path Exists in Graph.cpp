5b5022c96680de87076612c9da168d0baf0158bc
whuwh27604 <5991277@qq.com>
2021-10-23 19:45:37 +0800

    add

---
+++ questions/1900~1999/1971. Find if Path Exists in Graph/Find if Path Exists in Graph.cpp
@@ -0,0 +1,110 @@
+﻿/* Find if Path Exists in Graph.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+There is a bi-directional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive). The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a bi-directional edge between vertex ui and vertex vi. Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
+
+You want to determine if there is a valid path that exists from vertex start to vertex end.
+
+Given edges and the integers n, start, and end, return true if there is a valid path from start to end, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: n = 3, edges = [[0,1],[1,2],[2,0]], start = 0, end = 2
+Output: true
+Explanation: There are two paths from vertex 0 to vertex 2:
+- 0 → 1 → 2
+- 0 → 2
+Example 2:
+
+
+Input: n = 6, edges = [[0,1],[0,2],[3,5],[5,4],[4,3]], start = 0, end = 5
+Output: false
+Explanation: There is no path from vertex 0 to vertex 5.
+ 
+
+Constraints:
+
+1 <= n <= 2 * 105
+0 <= edges.length <= 2 * 105
+edges[i].length == 2
+0 <= ui, vi <= n - 1
+ui != vi
+0 <= start, end <= n - 1
+There are no duplicate edges.
+There are no self edges.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-if-path-exists-in-graph
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class DSU {
+public:
+    DSU(int size) {
+        parents.resize(size);
+
+        for (int i = 0; i < size; ++i) {
+            parents[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (parents[x] != x) {
+            parents[x] = find(parents[x]);
+        }
+
+        return parents[x];
+    }
+
+    void merge(int x, int y) {
+        parents[find(x)] = find(y);
+    }
+
+private:
+    vector<int> parents;
+};
+
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
+        DSU dsu(n);
+
+        for (auto& edge : edges) {
+            dsu.merge(edge[0], edge[1]);
+        }
+
+        return dsu.find(start) == dsu.find(end);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> edges = { {0,1},{1,2},{2,0} };
+    check.checkBool(true, o.validPath(3, edges, 0, 2));
+
+    edges = { {0,1},{0,2},{3,5},{5,4},{4,3} };
+    check.checkBool(false, o.validPath(6, edges, 0, 5));
+
+    edges = {  };
+    check.checkBool(true, o.validPath(1, edges, 0, 0));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
