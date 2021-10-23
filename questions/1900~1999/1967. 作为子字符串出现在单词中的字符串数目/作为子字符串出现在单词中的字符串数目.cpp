14ca9f34b930c95f0a7cec0081ceea5d7849daf4
whuwh27604 <5991277@qq.com>
2021-10-23 19:43:32 +0800

    add

---
+++ questions/1900~1999/1967. 作为子字符串出现在单词中的字符串数目/作为子字符串出现在单词中的字符串数目.cpp
@@ -0,0 +1,96 @@
+﻿/* 作为子字符串出现在单词中的字符串数目.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个字符串数组 patterns 和一个字符串 word ，统计 patterns 中有多少个字符串是 word 的子字符串。返回字符串数目。
+
+子字符串 是字符串中的一个连续字符序列。
+
+ 
+
+示例 1：
+
+输入：patterns = ["a","abc","bc","d"], word = "abc"
+输出：3
+解释：
+- "a" 是 "abc" 的子字符串。
+- "abc" 是 "abc" 的子字符串。
+- "bc" 是 "abc" 的子字符串。
+- "d" 不是 "abc" 的子字符串。
+patterns 中有 3 个字符串作为子字符串出现在 word 中。
+示例 2：
+
+输入：patterns = ["a","b","c"], word = "aaaaabbbbb"
+输出：2
+解释：
+- "a" 是 "aaaaabbbbb" 的子字符串。
+- "b" 是 "aaaaabbbbb" 的子字符串。
+- "c" 不是 "aaaaabbbbb" 的字符串。
+patterns 中有 2 个字符串作为子字符串出现在 word 中。
+示例 3：
+
+输入：patterns = ["a","a","a"], word = "ab"
+输出：3
+解释：patterns 中的每个字符串都作为子字符串出现在 word "ab" 中。
+ 
+
+提示：
+
+1 <= patterns.length <= 100
+1 <= patterns[i].length <= 100
+1 <= word.length <= 100
+patterns[i] 和 word 由小写英文字母组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-strings-that-appear-as-substrings-in-word
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int count = 0;
+
+        for (string& pattern : patterns) {
+            if (word.find(pattern) != string::npos) {
+                ++count;
+            }
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<string> patterns = { "a","abc","bc","d" };
+    check.checkInt(3, o.numOfStrings(patterns, "abc"));
+
+    patterns = { "a","b","c" };
+    check.checkInt(2, o.numOfStrings(patterns, "aaaaabbbbb"));
+
+    patterns = { "a","a","a" };
+    check.checkInt(3, o.numOfStrings(patterns, "ab"));
+
+    patterns = { "z" };
+    check.checkInt(1, o.numOfStrings(patterns, "z"));
+
+    patterns = { "z" };
+    check.checkInt(0, o.numOfStrings(patterns, "a"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
