92a88504601d9ec1cc3f0b752a9a8352a201bc98
whuwh27604 <5991277@qq.com>
2021-11-10 21:49:28 +0800

    add

---
+++ questions/2000~2099/2064. 分配给商店的最多商品的最小值/分配给商店的最多商品的最小值.cpp
@@ -0,0 +1,108 @@
+﻿/* 分配给商店的最多商品的最小值.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数 n ，表示有 n 间零售商店。总共有 m 种产品，每种产品的数目用一个下标从 0 开始的整数数组 quantities 表示，其中 quantities[i] 表示第 i 种商品的数目。
+
+你需要将 所有商品 分配到零售商店，并遵守这些规则：
+
+一间商店 至多 只能有 一种商品 ，但一间商店拥有的商品数目可以为 任意 件。
+分配后，每间商店都会被分配一定数目的商品（可能为 0 件）。用 x 表示所有商店中分配商品数目的最大值，你希望 x 越小越好。也就是说，你想 最小化 分配给任意商店商品数目的 最大值 。
+请你返回最小的可能的 x 。
+
+ 
+
+示例 1：
+
+输入：n = 6, quantities = [11,6]
+输出：3
+解释： 一种最优方案为：
+- 11 件种类为 0 的商品被分配到前 4 间商店，分配数目分别为：2，3，3，3 。
+- 6 件种类为 1 的商品被分配到另外 2 间商店，分配数目分别为：3，3 。
+分配给所有商店的最大商品数目为 max(2, 3, 3, 3, 3, 3) = 3 。
+示例 2：
+
+输入：n = 7, quantities = [15,10,10]
+输出：5
+解释：一种最优方案为：
+- 15 件种类为 0 的商品被分配到前 3 间商店，分配数目为：5，5，5 。
+- 10 件种类为 1 的商品被分配到接下来 2 间商店，数目为：5，5 。
+- 10 件种类为 2 的商品被分配到最后 2 间商店，数目为：5，5 。
+分配给所有商店的最大商品数目为 max(5, 5, 5, 5, 5, 5, 5) = 5 。
+示例 3：
+
+输入：n = 1, quantities = [100000]
+输出：100000
+解释：唯一一种最优方案为：
+- 所有 100000 件商品 0 都分配到唯一的商店中。
+分配给所有商店的最大商品数目为 max(100000) = 100000 。
+ 
+
+提示：
+
+m == quantities.length
+1 <= m <= n <= 105
+1 <= quantities[i] <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimized-maximum-of-products-distributed-to-any-store
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int low = 1, high = *max_element(quantities.begin(), quantities.end()), x = high;
+
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            int store = 0;
+
+            for (int quantity : quantities) {
+                store += (quantity / middle + (quantity % middle == 0 ? 0 : 1));
+            }
+
+            if (store <= n) {
+                x = middle;
+                high = middle - 1;
+            }
+            else {
+                low = middle + 1;
+            }
+        }
+
+        return x;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> quantities = { 11,6 };
+    check.checkInt(3, o.minimizedMaximum(6, quantities));
+
+    quantities = { 15,10,10 };
+    check.checkInt(5, o.minimizedMaximum(7, quantities));
+
+    quantities = { 100000 };
+    check.checkInt(100000, o.minimizedMaximum(1, quantities));
+
+    quantities = { 1 };
+    check.checkInt(1, o.minimizedMaximum(1, quantities));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
