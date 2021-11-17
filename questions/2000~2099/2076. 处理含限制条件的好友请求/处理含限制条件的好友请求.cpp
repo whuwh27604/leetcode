58bce2ce11c2a858fa1badb91e3a694c745cbd63
whuwh27604 <5991277@qq.com>
2021-11-17 11:03:20 +0800

    add

---
+++ questions/2000~2099/2076. 处理含限制条件的好友请求/处理含限制条件的好友请求.cpp
@@ -0,0 +1,157 @@
+﻿/* 处理含限制条件的好友请求.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数 n ，表示网络上的用户数目。每个用户按从 0 到 n - 1 进行编号。
+
+给你一个下标从 0 开始的二维整数数组 restrictions ，其中 restrictions[i] = [xi, yi] 意味着用户 xi 和用户 yi 不能 成为 朋友 ，不管是 直接 还是通过其他用户 间接 。
+
+最初，用户里没有人是其他用户的朋友。给你一个下标从 0 开始的二维整数数组 requests 表示好友请求的列表，其中 requests[j] = [uj, vj] 是用户 uj 和用户 vj 之间的一条好友请求。
+
+如果 uj 和 vj 可以成为 朋友 ，那么好友请求将会 成功 。每个好友请求都会按列表中给出的顺序进行处理（即，requests[j] 会在 requests[j + 1] 前）。一旦请求成功，那么对所有未来的好友请求而言， uj 和 vj 将会 成为直接朋友 。
+
+返回一个 布尔数组 result ，其中元素遵循此规则：如果第 j 个好友请求 成功 ，那么 result[j] 就是 true ；否则，为 false 。
+
+注意：如果 uj 和 vj 已经是直接朋友，那么他们之间的请求将仍然 成功 。
+
+ 
+
+示例 1：
+
+输入：n = 3, restrictions = [[0,1]], requests = [[0,2],[2,1]]
+输出：[true,false]
+解释：
+请求 0 ：用户 0 和 用户 2 可以成为朋友，所以他们成为直接朋友。
+请求 1 ：用户 2 和 用户 1 不能成为朋友，因为这会使 用户 0 和 用户 1 成为间接朋友 (1--2--0) 。
+示例 2：
+
+输入：n = 3, restrictions = [[0,1]], requests = [[1,2],[0,2]]
+输出：[true,false]
+解释：
+请求 0 ：用户 1 和 用户 2 可以成为朋友，所以他们成为直接朋友。
+请求 1 ：用户 0 和 用户 2 不能成为朋友，因为这会使 用户 0 和 用户 1 成为间接朋友 (0--2--1) 。
+示例 3：
+
+输入：n = 5, restrictions = [[0,1],[1,2],[2,3]], requests = [[0,4],[1,2],[3,1],[3,4]]
+输出：[true,false,true,false]
+解释：
+请求 0 ：用户 0 和 用户 4 可以成为朋友，所以他们成为直接朋友。
+请求 1 ：用户 1 和 用户 2 不能成为朋友，因为他们之间存在限制。
+请求 2 ：用户 3 和 用户 1 可以成为朋友，所以他们成为直接朋友。
+请求 3 ：用户 3 和 用户 4 不能成为朋友，因为这会使 用户 0 和 用户 1 成为间接朋友 (0--4--3--1) 。
+ 
+
+提示：
+
+2 <= n <= 1000
+0 <= restrictions.length <= 1000
+restrictions[i].length == 2
+0 <= xi, yi <= n - 1
+xi != yi
+1 <= requests.length <= 1000
+requests[j].length == 2
+0 <= uj, vj <= n - 1
+uj != vj
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/process-restricted-friend-requests
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class DSU {
+public:
+    DSU(int size) {
+        parents.resize(size);
+
+        for (int i = 0; i < size; ++i) {
+            parents[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (parents[x] != x) {
+            parents[x] = find(parents[x]);
+        }
+
+        return parents[x];
+    }
+
+    void merge(int x, int y) {
+        parents[find(x)] = find(y);
+    }
+
+private:
+    vector<int> parents;
+};
+
+class Solution {
+public:
+    vector<bool> friendRequests(int n, vector<vector<int>>& restrictions, vector<vector<int>>& requests) {
+        DSU dsu(n);
+        int i, size = requests.size();
+        vector<bool> responses(size);
+
+        for (i = 0; i < size; ++i) {
+            if (isConflict(dsu, requests[i], restrictions)) {
+                responses[i] = false;
+            }
+            else {
+                responses[i] = true;
+                dsu.merge(requests[i][0], requests[i][1]);
+            }
+        }
+
+        return responses;
+    }
+
+    bool isConflict(DSU& dsu, vector<int>& request, vector<vector<int>>& restrictions) {
+        int x = dsu.find(request[0]), y = dsu.find(request[1]);
+
+        for (auto& restriction : restrictions) {
+            int u = dsu.find(restriction[0]), v = dsu.find(restriction[1]);
+
+            if ((x == u && y == v) || (x == v && y == u)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> restrictions = {{0,1}};
+    vector<vector<int>> requests = {{0,2},{2,1}};
+    vector<bool> actual = o.friendRequests(3,restrictions,requests);
+    vector<bool> expected = { true,false };
+    check.checkBoolVector(expected,actual);
+
+    restrictions = {{0,1}};
+    requests = {{1,2},{0,2}};
+    actual = o.friendRequests(3,restrictions,requests);
+    expected = { true,false };
+    check.checkBoolVector(expected,actual);
+
+    restrictions = {{0,1},{1,2},{2,3}};
+    requests = {{0,4},{1,2},{3,1},{3,4}};
+    actual = o.friendRequests(5,restrictions,requests);
+    expected = { true,false,true,false };
+    check.checkBoolVector(expected,actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
