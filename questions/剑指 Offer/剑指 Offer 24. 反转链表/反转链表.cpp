1e4422ce47829b85b644659e95fc3d29b4d71c3a
whuwh27604 <5991277@qq.com>
2021-11-19 19:53:01 +0800

    add

---
+++ questions/剑指 Offer/剑指 Offer 24. 反转链表/反转链表.cpp
@@ -0,0 +1,83 @@
+﻿/* 反转链表.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+
+ 
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+ 
+
+限制：
+
+0 <= 节点个数 <= 5000
+
+ 
+
+注意：本题与主站 206 题相同：https://leetcode-cn.com/problems/reverse-linked-list/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../tools/TestData.h"
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* current = head;
+
+        while (current != NULL) {
+            ListNode* next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> values = { 1,2,3,4,5 };
+    ListNode* actual = o.reverseList(createList(values));
+    values = { 5,4,3,2,1 };
+    check.checkSingleList(createList(values), actual);
+
+    values = {  };
+    actual = o.reverseList(createList(values));
+    values = {  };
+    check.checkSingleList(createList(values), actual);
+
+    values = { 1 };
+    actual = o.reverseList(createList(values));
+    values = { 1 };
+    check.checkSingleList(createList(values), actual);
+
+    values = { 1,2 };
+    actual = o.reverseList(createList(values));
+    values = { 2,1 };
+    check.checkSingleList(createList(values), actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
