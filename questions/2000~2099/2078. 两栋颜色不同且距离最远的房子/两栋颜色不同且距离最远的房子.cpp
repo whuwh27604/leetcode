bc015978edbd7eec7e21381edd705445300405d6
whuwh27604 <5991277@qq.com>
2021-11-23 17:07:26 +0800

    add

---
+++ questions/2000~2099/2078. 两栋颜色不同且距离最远的房子/两栋颜色不同且距离最远的房子.cpp
@@ -0,0 +1,93 @@
+﻿/* 两栋颜色不同且距离最远的房子.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+街上有 n 栋房子整齐地排成一列，每栋房子都粉刷上了漂亮的颜色。给你一个下标从 0 开始且长度为 n 的整数数组 colors ，其中 colors[i] 表示第  i 栋房子的颜色。
+
+返回 两栋 颜色 不同 房子之间的 最大 距离。
+
+第 i 栋房子和第 j 栋房子之间的距离是 abs(i - j) ，其中 abs(x) 是 x 的绝对值。
+
+ 
+
+示例 1：
+
+
+
+输入：colors = [1,1,1,6,1,1,1]
+输出：3
+解释：上图中，颜色 1 标识成蓝色，颜色 6 标识成红色。
+两栋颜色不同且距离最远的房子是房子 0 和房子 3 。
+房子 0 的颜色是颜色 1 ，房子 3 的颜色是颜色 6 。两栋房子之间的距离是 abs(0 - 3) = 3 。
+注意，房子 3 和房子 6 也可以产生最佳答案。
+示例 2：
+
+
+
+输入：colors = [1,8,3,8,3]
+输出：4
+解释：上图中，颜色 1 标识成蓝色，颜色 8 标识成黄色，颜色 3 标识成绿色。
+两栋颜色不同且距离最远的房子是房子 0 和房子 4 。
+房子 0 的颜色是颜色 1 ，房子 4 的颜色是颜色 3 。两栋房子之间的距离是 abs(0 - 4) = 4 。
+示例 3：
+
+输入：colors = [0,1]
+输出：1
+解释：两栋颜色不同且距离最远的房子是房子 0 和房子 1 。
+房子 0 的颜色是颜色 0 ，房子 1 的颜色是颜色 1 。两栋房子之间的距离是 abs(0 - 1) = 1 。
+ 
+
+提示：
+
+n == colors.length
+2 <= n <= 100
+0 <= colors[i] <= 100
+生成的测试数据满足 至少 存在 2 栋颜色不同的房子
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-furthest-houses-with-different-colors
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int size = colors.size(), i = 0, j = size - 1;
+
+        for (; colors[i] == colors[size - 1] && colors[0] == colors[j]; ++i, --j) {}
+
+        return max(size - 1 - i, j);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> colors = { 1,1,1,6,1,1,1 };
+    check.checkInt(3, o.maxDistance(colors));
+    
+    colors = { 1,8,3,8,3 };
+    check.checkInt(4, o.maxDistance(colors));
+
+    colors = { 0,1 };
+    check.checkInt(1, o.maxDistance(colors));
+
+    colors = { 0,0,0,0,0,1 };
+    check.checkInt(5, o.maxDistance(colors));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
