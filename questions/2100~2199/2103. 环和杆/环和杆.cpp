1cd32642c374382e017f998fa7e13f9882cf87ff
whuwh27604 <5991277@qq.com>
2021-12-15 17:42:52 +0800

    add

---
+++ questions/2100~2199/2103. 环和杆/环和杆.cpp
@@ -0,0 +1,108 @@
+﻿/* 环和杆.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+总计有 n 个环，环的颜色可以是红、绿、蓝中的一种。这些环分布穿在 10 根编号为 0 到 9 的杆上。
+
+给你一个长度为 2n 的字符串 rings ，表示这 n 个环在杆上的分布。rings 中每两个字符形成一个 颜色位置对 ，用于描述每个环：
+
+第 i 对中的 第一个 字符表示第 i 个环的 颜色（'R'、'G'、'B'）。
+第 i 对中的 第二个 字符表示第 i 个环的 位置，也就是位于哪根杆上（'0' 到 '9'）。
+例如，"R3G2B1" 表示：共有 n == 3 个环，红色的环在编号为 3 的杆上，绿色的环在编号为 2 的杆上，蓝色的环在编号为 1 的杆上。
+
+找出所有集齐 全部三种颜色 环的杆，并返回这种杆的数量。
+
+ 
+
+示例 1：
+
+
+输入：rings = "B0B6G0R6R0R6G9"
+输出：1
+解释：
+- 编号 0 的杆上有 3 个环，集齐全部颜色：红、绿、蓝。
+- 编号 6 的杆上有 3 个环，但只有红、蓝两种颜色。
+- 编号 9 的杆上只有 1 个绿色环。
+因此，集齐全部三种颜色环的杆的数目为 1 。
+示例 2：
+
+
+输入：rings = "B0R0G0R9R0B0G0"
+输出：1
+解释：
+- 编号 0 的杆上有 6 个环，集齐全部颜色：红、绿、蓝。
+- 编号 9 的杆上只有 1 个红色环。
+因此，集齐全部三种颜色环的杆的数目为 1 。
+示例 3：
+
+输入：rings = "G4"
+输出：0
+解释：
+只给了一个环，因此，不存在集齐全部三种颜色环的杆。
+ 
+
+提示：
+
+rings.length == 2 * n
+1 <= n <= 100
+如 i 是 偶数 ，则 rings[i] 的值可以取 'R'、'G' 或 'B'（下标从 0 开始计数）
+如 i 是 奇数 ，则 rings[i] 的值可以取 '0' 到 '9' 中的一个数字（下标从 0 开始计数）
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/rings-and-rods
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int countPoints(string rings) {
+        vector<vector<int>> count(10, vector<int>(3, 0));
+        int points = 0;
+
+        for (int i = 0; i < (int)rings.size(); i += 2) {
+            char color = rings[i];
+            int rod = rings[i + 1] - '0';
+
+            if (color == 'R') {
+                ++count[rod][0];
+            }
+            else if (color == 'G') {
+                ++count[rod][1];
+            }
+            else {
+                ++count[rod][2];
+            }
+        }
+
+        for (auto& c : count) {
+            if (c[0] > 0 && c[1] > 0 && c[2] > 0) {
+                ++points;
+            }
+        }
+
+        return points;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkInt(1, o.countPoints("B0B6G0R6R0R6G9"));
+    check.checkInt(1, o.countPoints("B0R0G0R9R0B0G0"));
+    check.checkInt(0, o.countPoints("G4"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
