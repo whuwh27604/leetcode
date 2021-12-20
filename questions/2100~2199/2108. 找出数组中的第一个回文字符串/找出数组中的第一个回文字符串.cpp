f87e25ef0bc809397a199c792671638f4081722e
whuwh27604 <5991277@qq.com>
2021-12-20 15:57:18 +0800

    add

---
+++ questions/2100~2199/2108. 找出数组中的第一个回文字符串/找出数组中的第一个回文字符串.cpp
@@ -0,0 +1,92 @@
+﻿/* 找出数组中的第一个回文字符串.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个字符串数组 words ，找出并返回数组中的 第一个回文字符串 。如果不存在满足要求的字符串，返回一个 空字符串 "" 。
+
+回文字符串 的定义为：如果一个字符串正着读和反着读一样，那么该字符串就是一个 回文字符串 。
+
+ 
+
+示例 1：
+
+输入：words = ["abc","car","ada","racecar","cool"]
+输出："ada"
+解释：第一个回文字符串是 "ada" 。
+注意，"racecar" 也是回文字符串，但它不是第一个。
+示例 2：
+
+输入：words = ["notapalindrome","racecar"]
+输出："racecar"
+解释：第一个也是唯一一个回文字符串是 "racecar" 。
+示例 3：
+
+输入：words = ["def","ghi"]
+输出：""
+解释：不存在回文字符串，所以返回一个空字符串。
+ 
+
+提示：
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] 仅由小写英文字母组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-first-palindromic-string-in-the-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for (string& word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+
+        return "";
+    }
+
+    bool isPalindrome(string& word) {
+        for (int i = 0, j = word.size() - 1; i < j; ++i, --j) {
+            if (word[i] != word[j]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<string> words = { "abc","car","ada","racecar","cool" };
+    check.checkString("ada", o.firstPalindrome(words));
+
+    words = { "notapalindrome","racecar" };
+    check.checkString("racecar", o.firstPalindrome(words));
+
+    words = { "def","ghi" };
+    check.checkString("", o.firstPalindrome(words));
+
+    words = { "a","aba" };
+    check.checkString("a", o.firstPalindrome(words));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
