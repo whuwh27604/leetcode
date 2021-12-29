a72accbb713bffd5beb617ddaf2b43aeb1044c6c
whuwh27604 <5991277@qq.com>
2021-12-29 10:50:59 +0800

    add

---
+++ questions/2100~2199/2119. 反转两次的数字/反转两次的数字.cpp
@@ -0,0 +1,35 @@
+﻿// 反转两次的数字.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        return num == 0 || (num % 10) != 0;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkBool(true, o.isSameAfterReversals(526));
+    check.checkBool(false, o.isSameAfterReversals(1800));
+    check.checkBool(true, o.isSameAfterReversals(0));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
