54540c7e40962c0c15150518a411117598d6f8a3
whuwh27604 <5991277@qq.com>
2022-01-05 19:15:59 +0800

    add

---
+++ questions/2100~2199/2125. 银行中的激光束数量/银行中的激光束数量.cpp
@@ -0,0 +1,100 @@
+﻿/* 银行中的激光束数量.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+银行内部的防盗安全装置已经激活。给你一个下标从 0 开始的二进制字符串数组 bank ，表示银行的平面图，这是一个大小为 m x n 的二维矩阵。 bank[i] 表示第 i 行的设备分布，由若干 '0' 和若干 '1' 组成。'0' 表示单元格是空的，而 '1' 表示单元格有一个安全设备。
+
+对任意两个安全设备而言，如果同时 满足下面两个条件，则二者之间存在 一个 激光束：
+
+两个设备位于两个 不同行 ：r1 和 r2 ，其中 r1 < r2 。
+满足 r1 < i < r2 的 所有 行 i ，都 没有安全设备 。
+激光束是独立的，也就是说，一个激光束既不会干扰另一个激光束，也不会与另一个激光束合并成一束。
+
+返回银行中激光束的总数量。
+
+ 
+
+示例 1：
+
+
+
+输入：bank = ["011001","000000","010100","001000"]
+输出：8
+解释：在下面每组设备对之间，存在一条激光束。总共是 8 条激光束：
+ * bank[0][1] -- bank[2][1]
+ * bank[0][1] -- bank[2][3]
+ * bank[0][2] -- bank[2][1]
+ * bank[0][2] -- bank[2][3]
+ * bank[0][5] -- bank[2][1]
+ * bank[0][5] -- bank[2][3]
+ * bank[2][1] -- bank[3][2]
+ * bank[2][3] -- bank[3][2]
+注意，第 0 行和第 3 行上的设备之间不存在激光束。
+这是因为第 2 行存在安全设备，这不满足第 2 个条件。
+示例 2：
+
+
+
+输入：bank = ["000","111","000"]
+输出：0
+解释：不存在两个位于不同行的设备
+ 
+
+提示：
+
+m == bank.length
+n == bank[i].length
+1 <= m, n <= 500
+bank[i][j] 为 '0' 或 '1'
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-laser-beams-in-a-bank
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int beams = 0, prev = 0;
+
+        for (string& row : bank) {
+            int count = 0;
+
+            for (char c : row) {
+                count += (c - '0');
+            }
+
+            if (count != 0) {
+                beams += (prev * count);
+                prev = count;
+            }
+        }
+
+        return beams;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<string> bank = { "011001","000000","010100","001000" };
+    check.checkInt(8, o.numberOfBeams(bank));
+
+    bank = { "000","111","000" };
+    check.checkInt(0, o.numberOfBeams(bank));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
