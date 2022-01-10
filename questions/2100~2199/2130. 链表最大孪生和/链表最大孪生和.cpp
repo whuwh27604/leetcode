1953b32324f11b3b8739d01b104873fa7d873c83
whuwh27604 <5991277@qq.com>
2022-01-10 15:55:40 +0800

    add

---
+++ questions/2100~2199/2130. 链表最大孪生和/链表最大孪生和.cpp
@@ -0,0 +1,102 @@
+﻿/* 链表最大孪生和.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+在一个大小为 n 且 n 为 偶数 的链表中，对于 0 <= i <= (n / 2) - 1 的 i ，第 i 个节点（下标从 0 开始）的孪生节点为第 (n-1-i) 个节点 。
+
+比方说，n = 4 那么节点 0 是节点 3 的孪生节点，节点 1 是节点 2 的孪生节点。这是长度为 n = 4 的链表中所有的孪生节点。
+孪生和 定义为一个节点和它孪生节点两者值之和。
+
+给你一个长度为偶数的链表的头节点 head ，请你返回链表的 最大孪生和 。
+
+ 
+
+示例 1：
+
+
+
+输入：head = [5,4,2,1]
+输出：6
+解释：
+节点 0 和节点 1 分别是节点 3 和 2 的孪生节点。孪生和都为 6 。
+链表中没有其他孪生节点。
+所以，链表的最大孪生和是 6 。
+示例 2：
+
+
+
+输入：head = [4,2,2,3]
+输出：7
+解释：
+链表中的孪生节点为：
+- 节点 0 是节点 3 的孪生节点，孪生和为 4 + 3 = 7 。
+- 节点 1 是节点 2 的孪生节点，孪生和为 2 + 2 = 4 。
+所以，最大孪生和为 max(7, 4) = 7 。
+示例 3：
+
+
+
+输入：head = [1,100000]
+输出：100001
+解释：
+链表中只有一对孪生节点，孪生和为 1 + 100000 = 100001 。
+ 
+
+提示：
+
+链表的节点数目是 [2, 105] 中的 偶数 。
+1 <= Node.val <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-twin-sum-of-a-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        int maxSum = 0;
+        vector<int> nums;
+        ListNode* node = head;
+
+        while (node != NULL) {
+            nums.push_back(node->val);
+            node = node->next;
+        }
+
+        for (int i = 0, j = nums.size() - 1; i < j; ++i, --j) {
+            maxSum = max(maxSum, nums[i] + nums[j]);
+        }
+
+        return maxSum;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> values = { 5,4,2,1 };
+    check.checkInt(6, o.pairSum(createList(values)));
+
+    values = { 4,2,2,3 };
+    check.checkInt(7, o.pairSum(createList(values)));
+
+    values = { 1,100000 };
+    check.checkInt(100001, o.pairSum(createList(values)));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
