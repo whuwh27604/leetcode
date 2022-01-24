f23cb0e244735c2513ada21c4a33e77bde861516
whuwh27604 <5991277@qq.com>
2022-01-24 18:36:22 +0800

    add

---
+++ questions/2100~2199/2148. 元素计数/元素计数.cpp
@@ -0,0 +1,76 @@
+﻿/* 元素计数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数数组 nums ，统计并返回在 nums 中同时具有一个严格较小元素和一个严格较大元素的元素数目。
+
+ 
+
+示例 1：
+
+输入：nums = [11,7,2,15]
+输出：2
+解释：元素 7 ：严格较小元素是元素 2 ，严格较大元素是元素 11 。
+元素 11 ：严格较小元素是元素 7 ，严格较大元素是元素 15 。
+总计有 2 个元素都满足在 nums 中同时存在一个严格较小元素和一个严格较大元素。
+示例 2：
+
+输入：nums = [-3,3,3,90]
+输出：2
+解释：元素 3 ：严格较小元素是元素 -3 ，严格较大元素是元素 90 。
+由于有两个元素的值为 3 ，总计有 2 个元素都满足在 nums 中同时存在一个严格较小元素和一个严格较大元素。
+ 
+
+提示：
+
+1 <= nums.length <= 100
+-105 <= nums[i] <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/count-elements-with-strictly-smaller-and-greater-elements
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int countElements(vector<int>& nums) {
+        int count = 0, maximum = *max_element(nums.begin(), nums.end()), minimum = *min_element(nums.begin(), nums.end());
+
+        for (int num : nums) {
+            if (num != maximum && num != minimum) {
+                ++count;
+            }
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 11,7,2,15 };
+    check.checkInt(2, o.countElements(nums));
+
+    nums = { -3,3,3,90 };
+    check.checkInt(2, o.countElements(nums));
+
+    nums = { 1,1,1,2,2,2,3,3,3,4,4,4 };
+    check.checkInt(6, o.countElements(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
