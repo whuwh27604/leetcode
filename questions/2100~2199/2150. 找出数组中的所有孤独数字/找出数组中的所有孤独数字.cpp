33113bdf36b22770d92b54b6c2bca6a8c1d22f31
whuwh27604 <5991277@qq.com>
2022-01-27 11:06:27 +0800

    add

---
+++ questions/2100~2199/2150. 找出数组中的所有孤独数字/找出数组中的所有孤独数字.cpp
@@ -0,0 +1,91 @@
+﻿/* 找出数组中的所有孤独数字.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数数组 nums 。如果数字 x 在数组中仅出现 一次 ，且没有 相邻 数字（即，x + 1 和 x - 1）出现在数组中，则认为数字 x 是 孤独数字 。
+
+返回 nums 中的 所有 孤独数字。你可以按 任何顺序 返回答案。
+
+ 
+
+示例 1：
+
+输入：nums = [10,6,5,8]
+输出：[10,8]
+解释：
+- 10 是一个孤独数字，因为它只出现一次，并且 9 和 11 没有在 nums 中出现。
+- 8 是一个孤独数字，因为它只出现一次，并且 7 和 9 没有在 nums 中出现。
+- 5 不是一个孤独数字，因为 6 出现在 nums 中，反之亦然。
+因此，nums 中的孤独数字是 [10, 8] 。
+注意，也可以返回 [8, 10] 。
+示例 2：
+
+输入：nums = [1,3,5,3]
+输出：[1,5]
+解释：
+- 1 是一个孤独数字，因为它只出现一次，并且 0 和 2 没有在 nums 中出现。
+- 5 是一个孤独数字，因为它只出现一次，并且 4 和 6 没有在 nums 中出现。
+- 3 不是一个孤独数字，因为它出现两次。
+因此，nums 中的孤独数字是 [1, 5] 。
+注意，也可以返回 [5, 1] 。
+ 
+
+提示：
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 106
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-all-lonely-numbers-in-the-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findLonely(vector<int>& nums) {
+        unordered_map<int, int> numsCount;
+        vector<int> lonely;
+
+        for (int num : nums) {
+            ++numsCount[num];
+        }
+
+        for (auto& count : numsCount) {
+            if (count.second == 1 && numsCount.count(count.first - 1) == 0 && numsCount.count(count.first + 1) == 0) {
+                lonely.push_back(count.first);
+            }
+        }
+
+        return lonely;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 10,6,5,8 };
+    vector<int> actual = o.findLonely(nums);
+    vector<int> expected = { 10,8 };
+    check.checkIntVectorRandomOrder(expected, actual);
+
+    nums = { 1,3,5,3 };
+    actual = o.findLonely(nums);
+    expected = { 1,5 };
+    check.checkIntVectorRandomOrder(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
