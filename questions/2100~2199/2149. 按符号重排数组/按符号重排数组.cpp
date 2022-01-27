8d18fbb5aa9b63b03d0dbb596011a25bebf57b5e
whuwh27604 <5991277@qq.com>
2022-01-27 11:03:07 +0800

    add

---
+++ questions/2100~2199/2149. 按符号重排数组/按符号重排数组.cpp
@@ -0,0 +1,93 @@
+﻿/* 按符号重排数组.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums ，数组长度为 偶数 ，由数目相等的正整数和负整数组成。
+
+你需要 重排 nums 中的元素，使修改后的数组满足下述条件：
+
+任意 连续 的两个整数 符号相反
+对于符号相同的所有整数，保留 它们在 nums 中的 顺序 。
+重排后数组以正整数开头。
+重排元素满足上述条件后，返回修改后的数组。
+
+ 
+
+示例 1：
+
+输入：nums = [3,1,-2,-5,2,-4]
+输出：[3,-2,1,-5,2,-4]
+解释：
+nums 中的正整数是 [3,1,2] ，负整数是 [-2,-5,-4] 。
+重排的唯一可行方案是 [3,-2,1,-5,2,-4]，能满足所有条件。
+像 [1,-2,2,-5,3,-4]、[3,1,2,-2,-5,-4]、[-2,3,-5,1,-4,2] 这样的其他方案是不正确的，因为不满足一个或者多个条件。
+示例 2：
+
+输入：nums = [-1,1]
+输出：[1,-1]
+解释：
+1 是 nums 中唯一一个正整数，-1 是 nums 中唯一一个负整数。
+所以 nums 重排为 [1,-1] 。
+ 
+
+提示：
+
+2 <= nums.length <= 2 * 105
+nums.length 是 偶数
+1 <= |nums[i]| <= 105
+nums 由 相等 数量的正整数和负整数组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/rearrange-array-elements-by-sign
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int even = 0, odd = 1;
+
+        for (int num : nums) {
+            if (num < 0) {
+                ans[odd] = num;
+                odd += 2;
+            }
+            else {
+                ans[even] = num;
+                even += 2;
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 3,1,-2,-5,2,-4 };
+    vector<int> actual = o.rearrangeArray(nums);
+    vector<int> expected = { 3,-2,1,-5,2,-4 };
+    check.checkIntVector(expected, actual);
+
+    nums = { -1,1 };
+    actual = o.rearrangeArray(nums);
+    expected = { 1,-1 };
+    check.checkIntVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
