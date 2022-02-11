eb67a9b9d128e211d61a76254a0cd149478e96ac
whuwh27604 <5991277@qq.com>
2022-02-11 17:10:59 +0800

    add

---
+++ questions/2100~2199/2154. 将找到的值乘以 2/将找到的值乘以 2.cpp
@@ -0,0 +1,83 @@
+﻿/* 将找到的值乘以 2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数数组 nums ，另给你一个整数 original ，这是需要在 nums 中搜索的第一个数字。
+
+接下来，你需要按下述步骤操作：
+
+如果在 nums 中找到 original ，将 original 乘以 2 ，得到新 original（即，令 original = 2 * original）。
+否则，停止这一过程。
+只要能在数组中找到新 original ，就对新 original 继续 重复 这一过程。
+返回 original 的 最终 值。
+
+ 
+
+示例 1：
+
+输入：nums = [5,3,6,1,12], original = 3
+输出：24
+解释：
+- 3 能在 nums 中找到。3 * 2 = 6 。
+- 6 能在 nums 中找到。6 * 2 = 12 。
+- 12 能在 nums 中找到。12 * 2 = 24 。
+- 24 不能在 nums 中找到。因此，返回 24 。
+示例 2：
+
+输入：nums = [2,7,9], original = 4
+输出：4
+解释：
+- 4 不能在 nums 中找到。因此，返回 4 。
+ 
+
+提示：
+
+1 <= nums.length <= 1000
+1 <= nums[i], original <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/keep-multiplying-found-values-by-two
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        vector<bool> exist(1001, false);
+
+        for (int num : nums) {
+            exist[num] = true;
+        }
+
+        while (original <= 1000 && exist[original]) {
+            original *= 2;
+        }
+
+        return original;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 5,3,6,1,12 };
+    check.checkInt(24, o.findFinalValue(nums, 3));
+
+    nums = { 2,7,9 };
+    check.checkInt(4, o.findFinalValue(nums, 4));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
