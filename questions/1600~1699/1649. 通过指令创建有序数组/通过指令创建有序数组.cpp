ba3f1c178b216e44abc7cd9a1d3149d150d04eb5
whuwh27604 <5991277@qq.com>
2022-02-16 16:52:40 +0800

    add

---
+++ questions/1600~1699/1649. 通过指令创建有序数组/通过指令创建有序数组.cpp
@@ -0,0 +1,150 @@
+﻿/* 通过指令创建有序数组.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数数组 instructions ，你需要根据 instructions 中的元素创建一个有序数组。一开始你有一个空的数组 nums ，你需要 从左到右 遍历 instructions 中的元素，将它们依次插入 nums 数组中。每一次插入操作的 代价 是以下两者的 较小值 ：
+
+nums 中 严格小于  instructions[i] 的数字数目。
+nums 中 严格大于  instructions[i] 的数字数目。
+比方说，如果要将 3 插入到 nums = [1,2,3,5] ，那么插入操作的 代价 为 min(2, 1) (元素 1 和  2 小于 3 ，元素 5 大于 3 ），插入后 nums 变成 [1,2,3,3,5] 。
+
+请你返回将 instructions 中所有元素依次插入 nums 后的 总最小代价 。由于答案会很大，请将它对 109 + 7 取余 后返回。
+
+ 
+
+示例 1：
+
+输入：instructions = [1,5,6,2]
+输出：1
+解释：一开始 nums = [] 。
+插入 1 ，代价为 min(0, 0) = 0 ，现在 nums = [1] 。
+插入 5 ，代价为 min(1, 0) = 0 ，现在 nums = [1,5] 。
+插入 6 ，代价为 min(2, 0) = 0 ，现在 nums = [1,5,6] 。
+插入 2 ，代价为 min(1, 2) = 1 ，现在 nums = [1,2,5,6] 。
+总代价为 0 + 0 + 0 + 1 = 1 。
+示例 2:
+
+输入：instructions = [1,2,3,6,5,4]
+输出：3
+解释：一开始 nums = [] 。
+插入 1 ，代价为 min(0, 0) = 0 ，现在 nums = [1] 。
+插入 2 ，代价为 min(1, 0) = 0 ，现在 nums = [1,2] 。
+插入 3 ，代价为 min(2, 0) = 0 ，现在 nums = [1,2,3] 。
+插入 6 ，代价为 min(3, 0) = 0 ，现在 nums = [1,2,3,6] 。
+插入 5 ，代价为 min(3, 1) = 1 ，现在 nums = [1,2,3,5,6] 。
+插入 4 ，代价为 min(3, 2) = 2 ，现在 nums = [1,2,3,4,5,6] 。
+总代价为 0 + 0 + 0 + 0 + 1 + 2 = 3 。
+示例 3：
+
+输入：instructions = [1,3,3,3,2,4,2,1,2]
+输出：4
+解释：一开始 nums = [] 。
+插入 1 ，代价为 min(0, 0) = 0 ，现在 nums = [1] 。
+插入 3 ，代价为 min(1, 0) = 0 ，现在 nums = [1,3] 。
+插入 3 ，代价为 min(1, 0) = 0 ，现在 nums = [1,3,3] 。
+插入 3 ，代价为 min(1, 0) = 0 ，现在 nums = [1,3,3,3] 。
+插入 2 ，代价为 min(1, 3) = 1 ，现在 nums = [1,2,3,3,3] 。
+插入 4 ，代价为 min(5, 0) = 0 ，现在 nums = [1,2,3,3,3,4] 。
+​​​​​插入 2 ，代价为 min(1, 4) = 1 ，现在 nums = [1,2,2,3,3,3,4] 。
+插入 1 ，代价为 min(0, 6) = 0 ，现在 nums = [1,1,2,2,3,3,3,4] 。
+插入 2 ，代价为 min(2, 4) = 2 ，现在 nums = [1,1,2,2,2,3,3,3,4] 。
+总代价为 0 + 0 + 0 + 0 + 1 + 0 + 1 + 0 + 2 = 4 。
+ 
+
+提示：
+
+1 <= instructions.length <= 105
+1 <= instructions[i] <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/create-sorted-array-through-instructions
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class BIT {
+public:
+    BIT(int n) {
+        size = n;
+        tree.resize(n + 1, 0);
+    }
+
+    int lowbit(int index) {
+        return index & (-index);  // 保留最低位的1
+    }
+
+    void update(int index, int value) {
+        while (index <= size) {
+            tree[index] += value;
+            index += lowbit(index);
+        }
+    }
+
+    int query(int index) {
+        int value = 0;
+
+        while (index != 0) {
+            value += tree[index];
+            index -= lowbit(index);
+        }
+
+        return value;
+    }
+
+private:
+    int size;
+    vector<int> tree;
+};
+
+class Solution {
+public:
+    int createSortedArray(vector<int>& instructions) {
+        long long cost = 0;
+        int i, size = instructions.size(), maxIns = *max_element(instructions.begin(), instructions.end());
+        BIT bit(maxIns + 1);
+
+        for (i = 0; i < size; ++i) {
+            int ins = instructions[i];
+            int lessIns = bit.query(ins - 1);
+            int greaterIns = i - bit.query(ins);
+            cost += min(lessIns, greaterIns);
+            bit.update(ins, 1);
+        }
+
+        return (int)(cost % 1000000007);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> instructions = { 1,5,6,2 };
+    check.checkInt(1, o.createSortedArray(instructions));
+
+    instructions = { 1,2,3,6,5,4 };
+    check.checkInt(3, o.createSortedArray(instructions));
+
+    instructions = { 1,3,3,3,2,4,2,1,2 };
+    check.checkInt(4, o.createSortedArray(instructions));
+
+    instructions = { 1 };
+    check.checkInt(0, o.createSortedArray(instructions));
+
+    instructions = { 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4 };
+    check.checkInt(0, o.createSortedArray(instructions));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
