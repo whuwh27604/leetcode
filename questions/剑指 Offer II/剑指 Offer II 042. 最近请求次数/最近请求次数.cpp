5811672be9f65531d2ab316e0605118017e1aff3
whuwh27604 <5991277@qq.com>
2022-03-02 17:17:44 +0800

    add

---
+++ questions/剑指 Offer II/剑指 Offer II 042. 最近请求次数/最近请求次数.cpp
@@ -0,0 +1,91 @@
+﻿/* 最近请求次数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+写一个 RecentCounter 类来计算特定时间范围内最近的请求。
+
+请实现 RecentCounter 类：
+
+RecentCounter() 初始化计数器，请求数为 0 。
+int ping(int t) 在时间 t 添加一个新请求，其中 t 表示以毫秒为单位的某个时间，并返回过去 3000 毫秒内发生的所有请求数（包括新请求）。确切地说，返回在 [t-3000, t] 内发生的请求数。
+保证 每次对 ping 的调用都使用比之前更大的 t 值。
+
+ 
+
+示例：
+
+输入：
+inputs = ["RecentCounter", "ping", "ping", "ping", "ping"]
+inputs = [[], [1], [100], [3001], [3002]]
+输出：
+[null, 1, 2, 3, 3]
+
+解释：
+RecentCounter recentCounter = new RecentCounter();
+recentCounter.ping(1);     // requests = [1]，范围是 [-2999,1]，返回 1
+recentCounter.ping(100);   // requests = [1, 100]，范围是 [-2900,100]，返回 2
+recentCounter.ping(3001);  // requests = [1, 100, 3001]，范围是 [1,3001]，返回 3
+recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002]，范围是 [2,3002]，返回 3
+ 
+
+提示：
+
+1 <= t <= 109
+保证每次对 ping 调用所使用的 t 值都 严格递增
+至多调用 ping 方法 104 次
+ 
+
+注意：本题与主站 933 题相同： https://leetcode-cn.com/problems/number-of-recent-calls/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/H8086Q
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <queue>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class RecentCounter {
+public:
+    RecentCounter() {
+
+    }
+
+    int ping(int t) {
+        times.push(t);
+
+        while (t - times.front() > 3000) {
+            times.pop();
+        }
+
+        return times.size();
+    }
+
+    queue<int> times;
+};
+
+int main()
+{
+    CheckResult check;
+
+    RecentCounter* obj = new RecentCounter();
+    int param_1 = obj->ping(1);
+    check.checkInt(1, param_1);
+    param_1 = obj->ping(100);
+    check.checkInt(2, param_1);
+    param_1 = obj->ping(3001);
+    check.checkInt(3, param_1);
+    param_1 = obj->ping(3002);
+    check.checkInt(3, param_1);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
