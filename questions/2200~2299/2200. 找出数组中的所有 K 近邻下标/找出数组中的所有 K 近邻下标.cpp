4dda50c5889289d3f31947e0064ad9a60524e917
whuwh27604 <5991277@qq.com>
2022-03-17 13:50:41 +0800

    add

---
+++ questions/2200~2299/2200. 找出数组中的所有 K 近邻下标/找出数组中的所有 K 近邻下标.cpp
@@ -0,0 +1,99 @@
+﻿/* 找出数组中的所有 K 近邻下标.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums 和两个整数 key 和 k 。K 近邻下标 是 nums 中的一个下标 i ，并满足至少存在一个下标 j 使得 |i - j| <= k 且 nums[j] == key 。
+
+以列表形式返回按 递增顺序 排序的所有 K 近邻下标。
+
+ 
+
+示例 1：
+
+输入：nums = [3,4,9,1,3,9,5], key = 9, k = 1
+输出：[1,2,3,4,5,6]
+解释：因此，nums[2] == key 且 nums[5] == key 。
+- 对下标 0 ，|0 - 2| > k 且 |0 - 5| > k ，所以不存在 j 使得 |0 - j| <= k 且 nums[j] == key 。所以 0 不是一个 K 近邻下标。
+- 对下标 1 ，|1 - 2| <= k 且 nums[2] == key ，所以 1 是一个 K 近邻下标。
+- 对下标 2 ，|2 - 2| <= k 且 nums[2] == key ，所以 2 是一个 K 近邻下标。
+- 对下标 3 ，|3 - 2| <= k 且 nums[2] == key ，所以 3 是一个 K 近邻下标。
+- 对下标 4 ，|4 - 5| <= k 且 nums[5] == key ，所以 4 是一个 K 近邻下标。
+- 对下标 5 ，|5 - 5| <= k 且 nums[5] == key ，所以 5 是一个 K 近邻下标。
+- 对下标 6 ，|6 - 5| <= k 且 nums[5] == key ，所以 6 是一个 K 近邻下标。
+因此，按递增顺序返回 [1,2,3,4,5,6] 。
+示例 2：
+
+输入：nums = [2,2,2,2,2], key = 2, k = 2
+输出：[0,1,2,3,4]
+解释：对 nums 的所有下标 i ，总存在某个下标 j 使得 |i - j| <= k 且 nums[j] == key ，所以每个下标都是一个 K 近邻下标。
+因此，返回 [0,1,2,3,4] 。
+ 
+
+提示：
+
+1 <= nums.length <= 1000
+1 <= nums[i] <= 1000
+key 是数组 nums 中的一个整数
+1 <= k <= nums.length
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-all-k-distant-indices-in-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        int i, size = nums.size(), last = 0;
+        vector<int> indices;
+
+        for (i = 0; i < size; ++i) {
+            if (nums[i] == key) {
+                int left = max({ last, i - k });
+                int right = min(i + k, size - 1);
+                last = right + 1;
+
+                for (int index = left; index <= right; ++index) {
+                    indices.push_back(index);
+                }
+            }
+        }
+
+        return indices;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 3,4,9,1,3,9,5 };
+    vector<int> actual = o.findKDistantIndices(nums, 9, 1);
+    vector<int> expected = { 1,2,3,4,5,6 };
+    check.checkIntVector(expected, actual);
+
+    nums = { 2,2,2,2,2 };
+    actual = o.findKDistantIndices(nums, 2, 2);
+    expected = { 0,1,2,3,4 };
+    check.checkIntVector(expected, actual);
+
+    nums = { 5 };
+    actual = o.findKDistantIndices(nums, 5, 1);
+    expected = { 0 };
+    check.checkIntVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
