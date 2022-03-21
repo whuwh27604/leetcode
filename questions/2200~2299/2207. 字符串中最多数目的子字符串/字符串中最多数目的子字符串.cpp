f65d97337c9cb224fdc89ec925bf2089f9c86249
whuwh27604 <5991277@qq.com>
2022-03-21 17:30:42 +0800

    add

---
+++ questions/2200~2299/2207. 字符串中最多数目的子字符串/字符串中最多数目的子字符串.cpp
@@ -0,0 +1,88 @@
+﻿/* 字符串中最多数目的子字符串.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的字符串 text 和另一个下标从 0 开始且长度为 2 的字符串 pattern ，两者都只包含小写英文字母。
+
+你可以在 text 中任意位置插入 一个 字符，这个插入的字符必须是 pattern[0] 或者 pattern[1] 。注意，这个字符可以插入在 text 开头或者结尾的位置。
+
+请你返回插入一个字符后，text 中最多包含多少个等于 pattern 的 子序列 。
+
+子序列 指的是将一个字符串删除若干个字符后（也可以不删除），剩余字符保持原本顺序得到的字符串。
+
+ 
+
+示例 1：
+
+输入：text = "abdcdbc", pattern = "ac"
+输出：4
+解释：
+如果我们在 text[1] 和 text[2] 之间添加 pattern[0] = 'a' ，那么我们得到 "abadcdbc" 。那么 "ac" 作为子序列出现 4 次。
+其他得到 4 个 "ac" 子序列的方案还有 "aabdcdbc" 和 "abdacdbc" 。
+但是，"abdcadbc" ，"abdccdbc" 和 "abdcdbcc" 这些字符串虽然是可行的插入方案，但是只出现了 3 次 "ac" 子序列，所以不是最优解。
+可以证明插入一个字符后，无法得到超过 4 个 "ac" 子序列。
+示例 2：
+
+输入：text = "aabb", pattern = "ab"
+输出：6
+解释：
+可以得到 6 个 "ab" 子序列的部分方案为 "aaabb" ，"aaabb" 和 "aabbb" 。
+ 
+
+提示：
+
+1 <= text.length <= 105
+pattern.length == 2
+text 和 pattern 都只包含小写英文字母。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximize-number-of-subsequences-in-a-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    long long maximumSubsequenceCount(string text, string pattern) {
+        int i, size = text.size();
+        long long count1 = 0, count2 = 0, p1 = 1;
+
+        for (i = size - 1; i >= 0; --i) {
+            if (text[i] == pattern[0]) {
+                count1 += p1;
+                count2 += (p1 - 1);
+            }
+
+            if (text[i] == pattern[1]) {
+                ++p1;
+            }
+        }
+
+        return max(count1, count2 + p1 - 1);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkLongLong(4, o.maximumSubsequenceCount("abdcdbc", "ac"));
+    check.checkLongLong(6, o.maximumSubsequenceCount("aabb", "ab"));
+    check.checkLongLong(1, o.maximumSubsequenceCount("a", "ab"));
+    check.checkLongLong(0, o.maximumSubsequenceCount("z", "ab"));
+    check.checkLongLong(3081, o.maximumSubsequenceCount("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "aa"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
