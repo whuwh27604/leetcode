e362839affface905650ed037f2d815cc6b976d5
whuwh27604 <5991277@qq.com>
2022-03-28 12:15:40 +0800

    add

---
+++ questions/2200~2299/2215. 找出两数组的不同/找出两数组的不同.cpp
@@ -0,0 +1,91 @@
+﻿/* 找出两数组的不同.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你两个下标从 0 开始的整数数组 nums1 和 nums2 ，请你返回一个长度为 2 的列表 answer ，其中：
+
+answer[0] 是 nums1 中所有 不 存在于 nums2 中的 不同 整数组成的列表。
+answer[1] 是 nums2 中所有 不 存在于 nums1 中的 不同 整数组成的列表。
+注意：列表中的整数可以按 任意 顺序返回。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,2,3], nums2 = [2,4,6]
+输出：[[1,3],[4,6]]
+解释：
+对于 nums1 ，nums1[1] = 2 出现在 nums2 中下标 0 处，然而 nums1[0] = 1 和 nums1[2] = 3 没有出现在 nums2 中。因此，answer[0] = [1,3]。
+对于 nums2 ，nums2[0] = 2 出现在 nums1 中下标 1 处，然而 nums2[1] = 4 和 nums2[2] = 6 没有出现在 nums2 中。因此，answer[1] = [4,6]。
+示例 2：
+
+输入：nums1 = [1,2,3,3], nums2 = [1,1,2,2]
+输出：[[3],[]]
+解释：
+对于 nums1 ，nums1[2] 和 nums1[3] 没有出现在 nums2 中。由于 nums1[2] == nums1[3] ，二者的值只需要在 answer[0] 中出现一次，故 answer[0] = [3]。
+nums2 中的每个整数都在 nums1 中出现，因此，answer[1] = [] 。
+ 
+
+提示：
+
+1 <= nums1.length, nums2.length <= 1000
+-1000 <= nums1[i], nums2[i] <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-the-difference-of-two-arrays
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <unordered_set>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> n1(nums1.begin(), nums1.end()), n2(nums2.begin(), nums2.end());
+        vector<vector<int>> ans(2);
+
+        for (int num : n1) {
+            if (n2.count(num) == 0) {
+                ans[0].push_back(num);
+            }
+        }
+
+        for (int num : n2) {
+            if (n1.count(num) == 0) {
+                ans[1].push_back(num);
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums1 = { 1,2,3 };
+    vector<int> nums2 = { 2,4,6 };
+    vector<vector<int>> actual = o.findDifference(nums1, nums2);
+    vector<vector<int>> expected = { {1,3},{4,6} };
+    check.checkIntVectorVectorRandomOrder(expected, actual);
+
+    nums1 = { 1,2,3,3 };
+    nums2 = { 1,1,2,2 };
+    actual = o.findDifference(nums1, nums2);
+    expected = { {3},{} };
+    check.checkIntVectorVectorRandomOrder(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
