14fda09bd8d7f0558cd001fe809575e96b680bc0
whuwh27604 <5991277@qq.com>
2022-04-07 12:35:40 +0800

    add

---
+++ questions/2200~2299/2221. 数组的三角和/数组的三角和.cpp
@@ -0,0 +1,84 @@
+﻿/* 数组的三角和.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums ，其中 nums[i] 是 0 到 9 之间（两者都包含）的一个数字。
+
+nums 的 三角和 是执行以下操作以后最后剩下元素的值：
+
+nums 初始包含 n 个元素。如果 n == 1 ，终止 操作。否则，创建 一个新的下标从 0 开始的长度为 n - 1 的整数数组 newNums 。
+对于满足 0 <= i < n - 1 的下标 i ，newNums[i] 赋值 为 (nums[i] + nums[i+1]) % 10 ，% 表示取余运算。
+将 newNums 替换 数组 nums 。
+从步骤 1 开始 重复 整个过程。
+请你返回 nums 的三角和。
+
+ 
+
+示例 1：
+
+
+
+输入：nums = [1,2,3,4,5]
+输出：8
+解释：
+上图展示了得到数组三角和的过程。
+示例 2：
+
+输入：nums = [5]
+输出：5
+解释：
+由于 nums 中只有一个元素，数组的三角和为这个元素自己。
+ 
+
+提示：
+
+1 <= nums.length <= 1000
+0 <= nums[i] <= 9
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-triangular-sum-of-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        int i, j, size = nums.size();
+
+        for (i = size - 1; i > 0; --i) {
+            for (j = 0; j < i; ++j) {
+                nums[j] = (nums[j] + nums[j + 1]) % 10;
+            }
+        }
+
+        return nums[0];
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,2,3,4,5 };
+    check.checkInt(8, o.triangularSum(nums));
+
+    nums = { 5 };
+    check.checkInt(5, o.triangularSum(nums));
+
+    nums = { 0,3,3,4,1,2,6,4,9,3,5,1,7,7,3,0,3,2,5,1,9,0,2,6,3,9,2,5,9,2,6,4,2,9,7,2,0,3,0,1,1,2,7,8,6,4,4,5 };
+    check.checkInt(5, o.triangularSum(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
