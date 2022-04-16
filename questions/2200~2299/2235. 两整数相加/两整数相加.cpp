6cfd9d86bac82d544330a0dbc8c4647297aca0cf
whuwh27604 <5991277@qq.com>
2022-04-16 14:23:07 +0800

    add

---
+++ questions/2200~2299/2235. 两整数相加/两整数相加.cpp
@@ -0,0 +1,56 @@
+﻿/* 两整数相加.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你两个整数 num1 和 num2，返回这两个整数的和。
+ 
+
+示例 1：
+
+输入：num1 = 12, num2 = 5
+输出：17
+解释：num1 是 12，num2 是 5 ，它们的和是 12 + 5 = 17 ，因此返回 17 。
+示例 2：
+
+输入：num1 = -10, num2 = 4
+输出：-6
+解释：num1 + num2 = -6 ，因此返回 -6 。
+ 
+
+提示：
+
+-100 <= num1, num2 <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-integers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkInt(17, o.sum(12, 5));
+    check.checkInt(-6, o.sum(-10, 4));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
