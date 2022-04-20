1b1707ee10a66582db64985a2ead8fb407357e86
whuwh27604 <5991277@qq.com>
2022-04-20 12:01:53 +0800

    add

---
+++ questions/2200~2299/2239. 找到最接近 0 的数字/找到最接近 0 的数字.cpp
@@ -0,0 +1,80 @@
+﻿/* 找到最接近 0 的数字.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个长度为 n 的整数数组 nums ，请你返回 nums 中最 接近 0 的数字。如果有多个答案，请你返回它们中的 最大值 。
+
+ 
+
+示例 1：
+
+输入：nums = [-4,-2,1,4,8]
+输出：1
+解释：
+-4 到 0 的距离为 |-4| = 4 。
+-2 到 0 的距离为 |-2| = 2 。
+1 到 0 的距离为 |1| = 1 。
+4 到 0 的距离为 |4| = 4 。
+8 到 0 的距离为 |8| = 8 。
+所以，数组中距离 0 最近的数字为 1 。
+示例 2：
+
+输入：nums = [2,-1,1]
+输出：1
+解释：1 和 -1 都是距离 0 最近的数字，所以返回较大值 1 。
+ 
+
+提示：
+
+1 <= n <= 1000
+-105 <= nums[i] <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-closest-number-to-zero
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+        int minDist = INT_MAX, closestNumber = 0;
+
+        for (int num : nums) {
+            if (abs(num) < minDist) {
+                minDist = abs(num);
+                closestNumber = num;
+            }
+            else if (abs(num) == minDist) {
+                closestNumber = max(closestNumber, num);
+            }
+        }
+
+        return closestNumber;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { -4,-2,1,4,8 };
+    check.checkInt(1, o.findClosestNumber(nums));
+
+    nums = { 2,-1,1 };
+    check.checkInt(1, o.findClosestNumber(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
