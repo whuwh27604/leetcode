327f0a96761b70f7ed95dc75af7bbcb8dffe1f73
whuwh27604 <5991277@qq.com>
2022-04-20 11:58:46 +0800

    add

---
+++ questions/2200~2299/2240. 买钢笔和铅笔的方案数/买钢笔和铅笔的方案数.cpp
@@ -0,0 +1,69 @@
+﻿/* 买钢笔和铅笔的方案数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数 total ，表示你拥有的总钱数。同时给你两个整数 cost1 和 cost2 ，分别表示一支钢笔和一支铅笔的价格。你可以花费你部分或者全部的钱，去买任意数目的两种笔。
+
+请你返回购买钢笔和铅笔的 不同方案数目 。
+
+ 
+
+示例 1：
+
+输入：total = 20, cost1 = 10, cost2 = 5
+输出：9
+解释：一支钢笔的价格为 10 ，一支铅笔的价格为 5 。
+- 如果你买 0 支钢笔，那么你可以买 0 ，1 ，2 ，3 或者 4 支铅笔。
+- 如果你买 1 支钢笔，那么你可以买 0 ，1 或者 2 支铅笔。
+- 如果你买 2 支钢笔，那么你没法买任何铅笔。
+所以买钢笔和铅笔的总方案数为 5 + 3 + 1 = 9 种。
+示例 2：
+
+输入：total = 5, cost1 = 10, cost2 = 10
+输出：1
+解释：钢笔和铅笔的价格都为 10 ，都比拥有的钱数多，所以你没法购买任何文具。所以只有 1 种方案：买 0 支钢笔和 0 支铅笔。
+ 
+
+提示：
+
+1 <= total, cost1, cost2 <= 106
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-ways-to-buy-pens-and-pencils
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        long long ways = 0;
+
+        for (int pens = 0; pens <= total / cost1; ++pens) {
+            ways += ((total - (long long)cost1 * pens) / cost2 + 1);
+        }
+
+        return ways;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkLongLong(9, o.waysToBuyPensPencils(20, 10, 5));
+    check.checkLongLong(1, o.waysToBuyPensPencils(5, 10, 10));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
