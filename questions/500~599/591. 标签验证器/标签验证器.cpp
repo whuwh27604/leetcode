5632187d022ff4a2496ef296278fbf4b3b17ad57
whuwh27604 <5991277@qq.com>
2022-05-02 20:18:41 +0800

    add

---
+++ questions/500~599/591. 标签验证器/标签验证器.cpp
@@ -0,0 +1,193 @@
+﻿/* 标签验证器.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给定一个表示代码片段的字符串，你需要实现一个验证器来解析这段代码，并返回它是否合法。合法的代码片段需要遵守以下的所有规则：
+
+代码必须被合法的闭合标签包围。否则，代码是无效的。
+闭合标签（不一定合法）要严格符合格式：<TAG_NAME>TAG_CONTENT</TAG_NAME>。其中，<TAG_NAME>是起始标签，</TAG_NAME>是结束标签。起始和结束标签中的 TAG_NAME 应当相同。当且仅当 TAG_NAME 和 TAG_CONTENT 都是合法的，闭合标签才是合法的。
+合法的 TAG_NAME 仅含有大写字母，长度在范围 [1,9] 之间。否则，该 TAG_NAME 是不合法的。
+合法的 TAG_CONTENT 可以包含其他合法的闭合标签，cdata （请参考规则7）和任意字符（注意参考规则1）除了不匹配的<、不匹配的起始和结束标签、不匹配的或带有不合法 TAG_NAME 的闭合标签。否则，TAG_CONTENT 是不合法的。
+一个起始标签，如果没有具有相同 TAG_NAME 的结束标签与之匹配，是不合法的。反之亦然。不过，你也需要考虑标签嵌套的问题。
+一个<，如果你找不到一个后续的>与之匹配，是不合法的。并且当你找到一个<或</时，所有直到下一个>的前的字符，都应当被解析为 TAG_NAME（不一定合法）。
+cdata 有如下格式：<![CDATA[CDATA_CONTENT]]>。CDATA_CONTENT 的范围被定义成 <![CDATA[ 和后续的第一个 ]]>之间的字符。
+CDATA_CONTENT 可以包含任意字符。cdata 的功能是阻止验证器解析CDATA_CONTENT，所以即使其中有一些字符可以被解析为标签（无论合法还是不合法），也应该将它们视为常规字符。
+合法代码的例子:
+
+输入: "<DIV>This is the first line <![CDATA[<div>]]></DIV>"
+
+输出: True
+
+解释:
+
+代码被包含在了闭合的标签内： <DIV> 和 </DIV> 。
+
+TAG_NAME 是合法的，TAG_CONTENT 包含了一些字符和 cdata 。
+
+即使 CDATA_CONTENT 含有不匹配的起始标签和不合法的 TAG_NAME，它应该被视为普通的文本，而不是标签。
+
+所以 TAG_CONTENT 是合法的，因此代码是合法的。最终返回True。
+
+
+输入: "<DIV>>>  ![cdata[]] <![CDATA[<div>]>]]>]]>>]</DIV>"
+
+输出: True
+
+解释:
+
+我们首先将代码分割为： start_tag|tag_content|end_tag 。
+
+start_tag -> "<DIV>"
+
+end_tag -> "</DIV>"
+
+tag_content 也可被分割为： text1|cdata|text2 。
+
+text1 -> ">>  ![cdata[]] "
+
+cdata -> "<![CDATA[<div>]>]]>" ，其中 CDATA_CONTENT 为 "<div>]>"
+
+text2 -> "]]>>]"
+
+
+start_tag 不是 "<DIV>>>" 的原因参照规则 6 。
+cdata 不是 "<![CDATA[<div>]>]]>]]>" 的原因参照规则 7 。
+不合法代码的例子:
+
+输入: "<A>  <B> </A>   </B>"
+输出: False
+解释: 不合法。如果 "<A>" 是闭合的，那么 "<B>" 一定是不匹配的，反之亦然。
+
+输入: "<DIV>  div tag is not closed  <DIV>"
+输出: False
+
+输入: "<DIV>  unmatched <  </DIV>"
+输出: False
+
+输入: "<DIV> closed tags with invalid tag name  <b>123</b> </DIV>"
+输出: False
+
+输入: "<DIV> unmatched tags with invalid tag name  </1234567890> and <CDATA[[]]>  </DIV>"
+输出: False
+
+输入: "<DIV>  unmatched start tag <B>  and unmatched end tag </C>  </DIV>"
+输出: False
+注意:
+
+为简明起见，你可以假设输入的代码（包括提到的任意字符）只包含数字, 字母, '<','>','/','!','[',']'和' '。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/tag-validator
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string code) {
+        int i = 0, n = code.size();
+        stack<string> tags;
+
+        while (i < n) {
+            if (code[i] == '<') {
+                if (i == n - 1) {
+                    return false;
+                }
+
+                if (code[i + 1] == '/') {
+                    int j = code.find('>', i);
+                    if (j == string::npos) {
+                        return false;
+                    }
+
+                    string tagname = code.substr(i + 2, j - (i + 2));
+                    if (tags.empty() || tags.top() != tagname) {
+                        return false;
+                    }
+
+                    tags.pop();
+                    i = j + 1;
+
+                    if (tags.empty() && i != n) {
+                        return false;
+                    }
+                }
+                else if (code[i + 1] == '!') {
+                    if (tags.empty()) {
+                        return false;
+                    }
+
+                    string cdata = code.substr(i + 2, 7);
+                    if (cdata != "[CDATA[") {
+                        return false;
+                    }
+
+                    int j = code.find("]]>", i);
+                    if (j == string::npos) {
+                        return false;
+                    }
+
+                    i = j + 1;
+                }
+                else {
+                    int j = code.find('>', i);
+                    if (j == string::npos) {
+                        return false;
+                    }
+
+                    string tagname = code.substr(i + 1, j - (i + 1));
+                    if (tagname.size() < 1 || tagname.size() > 9) {
+                        return false;
+                    }
+
+                    if (!all_of(tagname.begin(), tagname.end(), [](unsigned char c) { return isupper(c); })) {
+                        return false;
+                    }
+
+                    tags.push(move(tagname));
+                    i = j + 1;
+                }
+            }
+            else {
+                if (tags.empty()) {
+                    return false;
+                }
+
+                ++i;
+            }
+        }
+
+        return tags.empty();
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkBool(true, o.isValid("<DIV>This is the first line <![CDATA[<div>]]></DIV>"));
+    check.checkBool(true, o.isValid("<DIV>>>  ![cdata[]] <![CDATA[<div>]>]]>]]>>]</DIV>"));
+    check.checkBool(true, o.isValid("<A><A></A></A>"));
+    check.checkBool(false, o.isValid("<A>  <B> </A>   </B>"));
+    check.checkBool(false, o.isValid("<DIV>  div tag is not closed  <DIV>"));
+    check.checkBool(false, o.isValid("<DIV>  unmatched <  </DIV>"));
+    check.checkBool(false, o.isValid("<DIV> closed tags with invalid tag name  <b>123</b> </DIV>"));
+    check.checkBool(false, o.isValid("<DIV> unmatched tags with invalid tag name  </1234567890> and <CDATA[[]]>  </DIV>"));
+    check.checkBool(false, o.isValid("<DIV>  unmatched start tag <B>  and unmatched end tag </C>  </DIV>"));
+    check.checkBool(false, o.isValid("<A></A><B></B>"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
