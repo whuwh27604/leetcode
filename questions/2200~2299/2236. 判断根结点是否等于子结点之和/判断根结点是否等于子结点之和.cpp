f9edf1fbaa0f40d956b2aca8c2fd18876e105d62
whuwh27604 <5991277@qq.com>
2022-05-14 10:56:53 +0800

    add

---
+++ questions/2200~2299/2236. 判断根结点是否等于子结点之和/判断根结点是否等于子结点之和.cpp
@@ -0,0 +1,67 @@
+﻿/* 判断根结点是否等于子结点之和.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个 二叉树 的根结点 root，该二叉树由恰好 3 个结点组成：根结点、左子结点和右子结点。
+
+如果根结点值等于两个子结点值之和，返回 true ，否则返回 false 。
+
+ 
+
+示例 1：
+
+
+输入：root = [10,4,6]
+输出：true
+解释：根结点、左子结点和右子结点的值分别是 10 、4 和 6 。
+由于 10 等于 4 + 6 ，因此返回 true 。
+示例 2：
+
+
+输入：root = [5,3,1]
+输出：false
+解释：根结点、左子结点和右子结点的值分别是 5 、3 和 1 。
+由于 5 不等于 3 + 1 ，因此返回 false 。
+ 
+
+提示：
+
+树只包含根结点、左子结点和右子结点
+-100 <= Node.val <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/root-equals-sum-of-children
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        return root->val == root->left->val + root->right->val;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> values = { 10,4,6 };
+    check.checkBool(true, o.checkTree(createTree(values)));
+
+    values = { 5,3,1 };
+    check.checkBool(false, o.checkTree(createTree(values)));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
