f7013fa8f5f5c4ad507983624d4a6f7a4da50147
whuwh27604 <5991277@qq.com>
2022-05-18 00:26:00 +0800

    add

---
+++ questions/2200~2299/2274. 不含特殊楼层的最大连续楼层数/不含特殊楼层的最大连续楼层数.cpp
@@ -0,0 +1,79 @@
+﻿/* 不含特殊楼层的最大连续楼层数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+Alice 管理着一家公司，并租用大楼的部分楼层作为办公空间。Alice 决定将一些楼层作为 特殊楼层 ，仅用于放松。
+
+给你两个整数 bottom 和 top ，表示 Alice 租用了从 bottom 到 top（含 bottom 和 top 在内）的所有楼层。另给你一个整数数组 special ，其中 special[i] 表示  Alice 指定用于放松的特殊楼层。
+
+返回不含特殊楼层的 最大 连续楼层数。
+
+ 
+
+示例 1：
+
+输入：bottom = 2, top = 9, special = [4,6]
+输出：3
+解释：下面列出的是不含特殊楼层的连续楼层范围：
+- (2, 3) ，楼层数为 2 。
+- (5, 5) ，楼层数为 1 。
+- (7, 9) ，楼层数为 3 。
+因此，返回最大连续楼层数 3 。
+示例 2：
+
+输入：bottom = 6, top = 8, special = [7,6,8]
+输出：0
+解释：每层楼都被规划为特殊楼层，所以返回 0 。
+ 
+
+提示
+
+1 <= special.length <= 105
+1 <= bottom <= special[i] <= top <= 109
+special 中的所有值 互不相同
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/maximum-consecutive-floors-without-special-floors
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxConsecutive(int bottom, int top, vector<int>& special) {
+        sort(special.begin(), special.end());
+
+        int maxConsecutiveFloors = special[0] - bottom;
+
+        for (int i = 1; i < (int)special.size(); ++i) {
+            maxConsecutiveFloors = max(maxConsecutiveFloors, special[i] - special[i - 1] - 1);
+        }
+
+        return max(maxConsecutiveFloors, top - special.back());
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> special = { 4,6 };
+    check.checkInt(3, o.maxConsecutive(2, 9, special));
+
+    special = { 7,6,8 };
+    check.checkInt(0, o.maxConsecutive(6, 8, special));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
