e3ac9fc63c2426ad6ec753d40c7e7286d42b8576
whuwh27604 <5991277@qq.com>
2022-06-01 21:28:57 +0800

    add

---
+++ questions/2200~2299/2283. 判断一个数的数字计数是否等于数位的值/判断一个数的数字计数是否等于数位的值.cpp
@@ -0,0 +1,82 @@
+﻿/* 判断一个数的数字计数是否等于数位的值.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始长度为 n 的字符串 num ，它只包含数字。
+
+如果对于 每个 0 <= i < n 的下标 i ，都满足数位 i 在 num 中出现了 num[i]次，那么请你返回 true ，否则返回 false 。
+
+ 
+
+示例 1：
+
+输入：num = "1210"
+输出：true
+解释：
+num[0] = '1' 。数字 0 在 num 中出现了一次。
+num[1] = '2' 。数字 1 在 num 中出现了两次。
+num[2] = '1' 。数字 2 在 num 中出现了一次。
+num[3] = '0' 。数字 3 在 num 中出现了零次。
+"1210" 满足题目要求条件，所以返回 true 。
+示例 2：
+
+输入：num = "030"
+输出：false
+解释：
+num[0] = '0' 。数字 0 应该出现 0 次，但是在 num 中出现了一次。
+num[1] = '3' 。数字 1 应该出现 3 次，但是在 num 中出现了零次。
+num[2] = '0' 。数字 2 在 num 中出现了 0 次。
+下标 0 和 1 都违反了题目要求，所以返回 false 。
+ 
+
+提示：
+
+n == num.length
+1 <= n <= 10
+num 只包含数字。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/check-if-number-has-equal-digit-count-and-digit-value
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool digitCount(string num) {
+        int arr[10] = { 0 };
+
+        for (char c : num) {
+            ++arr[c - '0'];
+        }
+
+        for (int i = 0; i < (int)num.size(); ++i) {
+            if (arr[i] != num[i] - '0') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkBool(true, o.digitCount("1210"));
+    check.checkBool(false, o.digitCount("030"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
