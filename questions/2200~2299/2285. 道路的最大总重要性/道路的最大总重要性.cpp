43d66b3b01a4f903a6b015cdf148a22379c38519
whuwh27604 <5991277@qq.com>
2022-06-01 21:32:00 +0800

    add

---
+++ questions/2200~2299/2285. 道路的最大总重要性/道路的最大总重要性.cpp
@@ -0,0 +1,103 @@
+﻿/* 道路的最大总重要性.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数 n ，表示一个国家里的城市数目。城市编号为 0 到 n - 1 。
+
+给你一个二维整数数组 roads ，其中 roads[i] = [ai, bi] 表示城市 ai 和 bi 之间有一条 双向 道路。
+
+你需要给每个城市安排一个从 1 到 n 之间的整数值，且每个值只能被使用 一次 。道路的 重要性 定义为这条道路连接的两座城市数值 之和 。
+
+请你返回在最优安排下，所有道路重要性 之和 最大 为多少。
+
+ 
+
+示例 1：
+
+
+
+输入：n = 5, roads = [[0,1],[1,2],[2,3],[0,2],[1,3],[2,4]]
+输出：43
+解释：上图展示了国家图和每个城市被安排的值 [2,4,5,3,1] 。
+- 道路 (0,1) 重要性为 2 + 4 = 6 。
+- 道路 (1,2) 重要性为 4 + 5 = 9 。
+- 道路 (2,3) 重要性为 5 + 3 = 8 。
+- 道路 (0,2) 重要性为 2 + 5 = 7 。
+- 道路 (1,3) 重要性为 4 + 3 = 7 。
+- 道路 (2,4) 重要性为 5 + 1 = 6 。
+所有道路重要性之和为 6 + 9 + 8 + 7 + 7 + 6 = 43 。
+可以证明，重要性之和不可能超过 43 。
+示例 2：
+
+
+
+输入：n = 5, roads = [[0,3],[2,4],[1,3]]
+输出：20
+解释：上图展示了国家图和每个城市被安排的值 [4,3,2,5,1] 。
+- 道路 (0,3) 重要性为 4 + 5 = 9 。
+- 道路 (2,4) 重要性为 2 + 1 = 3 。
+- 道路 (1,3) 重要性为 3 + 5 = 8 。
+所有道路重要性之和为 9 + 3 + 8 = 20 。
+可以证明，重要性之和不可能超过 20 。
+ 
+
+提示：
+
+2 <= n <= 5 * 104
+1 <= roads.length <= 5 * 104
+roads[i].length == 2
+0 <= ai, bi <= n - 1
+ai != bi
+没有重复道路。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/maximum-total-importance-of-roads
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        vector<int> degrees(n, 0);
+        long long importance = 0;
+
+        for (auto& road : roads) {
+            ++degrees[road[0]];
+            ++degrees[road[1]];
+        }
+
+        sort(degrees.begin(), degrees.end());
+
+        for (int i = 0; i < n; ++i) {
+            importance += (((long long)i + 1) * degrees[i]);
+        }
+
+        return importance;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> roads = { {0,1},{1,2},{2,3},{0,2},{1,3},{2,4} };
+    check.checkLongLong(43, o.maximumImportance(5, roads));
+
+    roads = { {0,3},{2,4},{1,3} };
+    check.checkLongLong(20, o.maximumImportance(5, roads));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
