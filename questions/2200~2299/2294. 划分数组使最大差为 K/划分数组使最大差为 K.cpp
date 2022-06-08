6481308e0b15d83fdc2fd151ca8a20ae93bbb562
whuwh27604 <5991277@qq.com>
2022-06-08 21:44:24 +0800

    add

---
+++ questions/2200~2299/2294. 划分数组使最大差为 K/划分数组使最大差为 K.cpp
@@ -0,0 +1,102 @@
+﻿/* 划分数组使最大差为 K.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数数组 nums 和一个整数 k 。你可以将 nums 划分成一个或多个 子序列 ，使 nums 中的每个元素都 恰好 出现在一个子序列中。
+
+在满足每个子序列中最大值和最小值之间的差值最多为 k 的前提下，返回需要划分的 最少 子序列数目。
+
+子序列 本质是一个序列，可以通过删除另一个序列中的某些元素（或者不删除）但不改变剩下元素的顺序得到。
+
+ 
+
+示例 1：
+
+输入：nums = [3,6,1,2,5], k = 2
+输出：2
+解释：
+可以将 nums 划分为两个子序列 [3,1,2] 和 [6,5] 。
+第一个子序列中最大值和最小值的差值是 3 - 1 = 2 。
+第二个子序列中最大值和最小值的差值是 6 - 5 = 1 。
+由于创建了两个子序列，返回 2 。可以证明需要划分的最少子序列数目就是 2 。
+示例 2：
+
+输入：nums = [1,2,3], k = 1
+输出：2
+解释：
+可以将 nums 划分为两个子序列 [1,2] 和 [3] 。
+第一个子序列中最大值和最小值的差值是 2 - 1 = 1 。
+第二个子序列中最大值和最小值的差值是 3 - 3 = 0 。
+由于创建了两个子序列，返回 2 。注意，另一种最优解法是将 nums 划分成子序列 [1] 和 [2,3] 。
+示例 3：
+
+输入：nums = [2,2,4,5], k = 0
+输出：3
+解释：
+可以将 nums 划分为三个子序列 [2,2]、[4] 和 [5] 。
+第一个子序列中最大值和最小值的差值是 2 - 2 = 0 。
+第二个子序列中最大值和最小值的差值是 4 - 4 = 0 。
+第三个子序列中最大值和最小值的差值是 5 - 5 = 0 。
+由于创建了三个子序列，返回 3 。可以证明需要划分的最少子序列数目就是 3 。
+ 
+
+提示：
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 105
+0 <= k <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/partition-array-such-that-maximum-difference-is-k
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <set>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        set<int> numbers;
+        int minimum = -1000000, subseqs = 0;
+
+        for (int num : nums) {
+            numbers.insert(num);
+        }
+
+        for (int num : numbers) {
+            if (num - minimum > k) {
+                minimum = num;
+                ++subseqs;
+            }
+        }
+
+        return subseqs;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 3,6,1,2,5 };
+    check.checkInt(2, o.partitionArray(nums, 2));
+
+    nums = { 1,2,3 };
+    check.checkInt(2, o.partitionArray(nums, 1));
+
+    nums = { 2,2,4,5 };
+    check.checkInt(3, o.partitionArray(nums, 0));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
