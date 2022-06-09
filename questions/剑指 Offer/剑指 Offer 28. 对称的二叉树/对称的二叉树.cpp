25bf0e3e1ebaffca927f5dc7a426a7545297b96e
whuwh27604 <5991277@qq.com>
2022-06-10 01:03:06 +0800

    add

---
+++ questions/剑指 Offer/剑指 Offer 28. 对称的二叉树/对称的二叉树.cpp
@@ -0,0 +1,94 @@
+﻿/* 对称的二叉树.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+ 
+
+示例 1：
+
+输入：root = [1,2,2,3,4,4,3]
+输出：true
+示例 2：
+
+输入：root = [1,2,2,null,3,null,3]
+输出：false
+ 
+
+限制：
+
+0 <= 节点个数 <= 1000
+
+注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/dui-cheng-de-er-cha-shu-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL) {
+            return true;
+        }
+
+        return DFS(root->left, root->right);
+    }
+
+    bool DFS(TreeNode* root1, TreeNode* root2) {
+        if (root1 == NULL && root2 == NULL) {
+            return true;
+        }
+
+        if (root1 == NULL || root2 == NULL) {
+            return false;
+        }
+
+        return root1->val == root2->val && DFS(root1->left, root2->right) && DFS(root1->right, root2->left);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> values = {};
+    check.checkBool(true, o.isSymmetric(createTree(values)));
+
+    values = { 1,2,2,3,4,4,3 };
+    check.checkBool(true, o.isSymmetric(createTree(values)));
+
+    values = { 1,2,2,INT_MIN,3,INT_MIN,3 };
+    check.checkBool(false, o.isSymmetric(createTree(values)));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
