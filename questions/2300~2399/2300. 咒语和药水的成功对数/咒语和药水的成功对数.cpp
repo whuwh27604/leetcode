5bf448ecd8fb2330222cffa31979dc91817aa16c
whuwh27604 <5991277@qq.com>
2022-06-13 21:06:07 +0800

    add

---
+++ questions/2300~2399/2300. 咒语和药水的成功对数/咒语和药水的成功对数.cpp
@@ -0,0 +1,103 @@
+﻿/* 咒语和药水的成功对数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你两个正整数数组 spells 和 potions ，长度分别为 n 和 m ，其中 spells[i] 表示第 i 个咒语的能量强度，potions[j] 表示第 j 瓶药水的能量强度。
+
+同时给你一个整数 success 。一个咒语和药水的能量强度 相乘 如果 大于等于 success ，那么它们视为一对 成功 的组合。
+
+请你返回一个长度为 n 的整数数组 pairs，其中 pairs[i] 是能跟第 i 个咒语成功组合的 药水 数目。
+
+ 
+
+示例 1：
+
+输入：spells = [5,1,3], potions = [1,2,3,4,5], success = 7
+输出：[4,0,3]
+解释：
+- 第 0 个咒语：5 * [1,2,3,4,5] = [5,10,15,20,25] 。总共 4 个成功组合。
+- 第 1 个咒语：1 * [1,2,3,4,5] = [1,2,3,4,5] 。总共 0 个成功组合。
+- 第 2 个咒语：3 * [1,2,3,4,5] = [3,6,9,12,15] 。总共 3 个成功组合。
+所以返回 [4,0,3] 。
+示例 2：
+
+输入：spells = [3,1,2], potions = [8,5,8], success = 16
+输出：[2,0,2]
+解释：
+- 第 0 个咒语：3 * [8,5,8] = [24,15,24] 。总共 2 个成功组合。
+- 第 1 个咒语：1 * [8,5,8] = [8,5,8] 。总共 0 个成功组合。
+- 第 2 个咒语：2 * [8,5,8] = [16,10,16] 。总共 2 个成功组合。
+所以返回 [2,0,2] 。
+ 
+
+提示：
+
+n == spells.length
+m == potions.length
+1 <= n, m <= 105
+1 <= spells[i], potions[i] <= 105
+1 <= success <= 1010
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/successful-pairs-of-spells-and-potions
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n = spells.size(), m = potions.size();
+        vector<int> pairs(n);
+
+        sort(potions.begin(), potions.end());
+
+        for (int i = 0; i < n; ++i) {
+            long long potion = success / spells[i];
+
+            if ((success % spells[i]) != 0) {
+                potion += 1;
+            }
+
+            if (potion > potions.back()) {
+                pairs[i] = 0;
+            }
+            else {
+                pairs[i] = distance(lower_bound(potions.begin(), potions.end(), (int)potion), potions.end());
+            }
+        }
+
+        return pairs;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> spells = { 5,1,3 };
+    vector<int> potions = { 1,2,3,4,5 };
+    vector<int> actual = o.successfulPairs(spells, potions, 7);
+    vector<int> expected = { 4,0,3 };
+    check.checkIntVector(expected, actual);
+
+    spells = { 3,1,2 };
+    potions = { 8,5,8 };
+    actual = o.successfulPairs(spells, potions, 16);
+    expected = { 2,0,2 };
+    check.checkIntVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
