7f92ddc8de1939e696d74c83cb73e03a5586125b
whuwh27604 <5991277@qq.com>
2022-06-29 00:07:02 +0800

    add

---
+++ questions/2300~2399/2319. 判断矩阵是否是一个 X 矩阵/判断矩阵是否是一个 X 矩阵.cpp
@@ -0,0 +1,82 @@
+﻿/* 判断矩阵是否是一个 X 矩阵.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+如果一个正方形矩阵满足下述 全部 条件，则称之为一个 X 矩阵 ：
+
+矩阵对角线上的所有元素都 不是 0
+矩阵中所有其他元素都是 0
+给你一个大小为 n x n 的二维整数数组 grid ，表示一个正方形矩阵。如果 grid 是一个 X 矩阵 ，返回 true ；否则，返回 false 。
+
+ 
+
+示例 1：
+
+
+输入：grid = [[2,0,0,1],[0,3,1,0],[0,5,2,0],[4,0,0,2]]
+输出：true
+解释：矩阵如上图所示。
+X 矩阵应该满足：绿色元素（对角线上）都不是 0 ，红色元素都是 0 。
+因此，grid 是一个 X 矩阵。
+示例 2：
+
+
+输入：grid = [[5,7,0],[0,3,1],[0,5,0]]
+输出：false
+解释：矩阵如上图所示。
+X 矩阵应该满足：绿色元素（对角线上）都不是 0 ，红色元素都是 0 。
+因此，grid 不是一个 X 矩阵。
+ 
+
+提示：
+
+n == grid.length == grid[i].length
+3 <= n <= 100
+0 <= grid[i][j] <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/check-if-matrix-is-x-matrix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (((i == j || i + j == n - 1) && (grid[i][j] == 0)) || ((i != j && i + j != n - 1) && (grid[i][j] != 0))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> grid = { {2,0,0,1},{0,3,1,0},{0,5,2,0},{4,0,0,2} };
+    check.checkBool(true, o.checkXMatrix(grid));
+
+    grid = { {5,7,0},{0,3,1},{0,5,0} };
+    check.checkBool(false, o.checkXMatrix(grid));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
