1d59b7fbd6f5c9e205951a9b40e56240777fb168
whuwh27604 <5991277@qq.com>
2022-07-12 00:31:24 +0800

    add

---
+++ questions/2300~2399/2336. 无限集中的最小数字/无限集中的最小数字.cpp
@@ -0,0 +1,95 @@
+﻿/* 无限集中的最小数字.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+现有一个包含所有正整数的集合 [1, 2, 3, 4, 5, ...] 。
+
+实现 SmallestInfiniteSet 类：
+
+SmallestInfiniteSet() 初始化 SmallestInfiniteSet 对象以包含 所有 正整数。
+int popSmallest() 移除 并返回该无限集中的最小整数。
+void addBack(int num) 如果正整数 num 不 存在于无限集中，则将一个 num 添加 到该无限集中。
+ 
+
+示例：
+
+输入
+["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest", "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+[[], [2], [], [], [], [1], [], [], []]
+输出
+[null, null, 1, 2, 3, null, 1, 4, 5]
+
+解释
+SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
+smallestInfiniteSet.addBack(2);    // 2 已经在集合中，所以不做任何变更。
+smallestInfiniteSet.popSmallest(); // 返回 1 ，因为 1 是最小的整数，并将其从集合中移除。
+smallestInfiniteSet.popSmallest(); // 返回 2 ，并将其从集合中移除。
+smallestInfiniteSet.popSmallest(); // 返回 3 ，并将其从集合中移除。
+smallestInfiniteSet.addBack(1);    // 将 1 添加到该集合中。
+smallestInfiniteSet.popSmallest(); // 返回 1 ，因为 1 在上一步中被添加到集合中，
+                                   // 且 1 是最小的整数，并将其从集合中移除。
+smallestInfiniteSet.popSmallest(); // 返回 4 ，并将其从集合中移除。
+smallestInfiniteSet.popSmallest(); // 返回 5 ，并将其从集合中移除。
+ 
+
+提示：
+
+1 <= num <= 1000
+最多调用 popSmallest 和 addBack 方法 共计 1000 次
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/smallest-number-in-infinite-set
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <set>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class SmallestInfiniteSet {
+public:
+    SmallestInfiniteSet() {
+        for (int num = 1; num < 1001; ++num) {
+            nums.insert(num);
+        }
+    }
+
+    int popSmallest() {
+        int num = *nums.begin();
+        nums.erase(nums.begin());
+
+        return num;
+    }
+
+    void addBack(int num) {
+        nums.insert(num);
+    }
+
+private:
+    set<int> nums;
+};
+
+int main()
+{
+    CheckResult check;
+
+    SmallestInfiniteSet o;
+    o.addBack(2);
+    check.checkInt(1, o.popSmallest());
+    check.checkInt(2, o.popSmallest());
+    check.checkInt(3, o.popSmallest());
+    o.addBack(1);
+    check.checkInt(1, o.popSmallest());
+    check.checkInt(4, o.popSmallest());
+    check.checkInt(5, o.popSmallest());
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
