762b9c6ecff738c310cb6a830701540ee137f497
whuwh27604 <5991277@qq.com>
2022-07-12 00:27:50 +0800

    add

---
+++ questions/2300~2399/2335. 装满杯子需要的最短总时长/装满杯子需要的最短总时长.cpp
@@ -0,0 +1,92 @@
+﻿/* 装满杯子需要的最短总时长.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+现有一台饮水机，可以制备冷水、温水和热水。每秒钟，可以装满 2 杯 不同 类型的水或者 1 杯任意类型的水。
+
+给你一个下标从 0 开始、长度为 3 的整数数组 amount ，其中 amount[0]、amount[1] 和 amount[2] 分别表示需要装满冷水、温水和热水的杯子数量。返回装满所有杯子所需的 最少 秒数。
+
+ 
+
+示例 1：
+
+输入：amount = [1,4,2]
+输出：4
+解释：下面给出一种方案：
+第 1 秒：装满一杯冷水和一杯温水。
+第 2 秒：装满一杯温水和一杯热水。
+第 3 秒：装满一杯温水和一杯热水。
+第 4 秒：装满一杯温水。
+可以证明最少需要 4 秒才能装满所有杯子。
+示例 2：
+
+输入：amount = [5,4,4]
+输出：7
+解释：下面给出一种方案：
+第 1 秒：装满一杯冷水和一杯热水。
+第 2 秒：装满一杯冷水和一杯温水。
+第 3 秒：装满一杯冷水和一杯温水。
+第 4 秒：装满一杯温水和一杯热水。
+第 5 秒：装满一杯冷水和一杯热水。
+第 6 秒：装满一杯冷水和一杯温水。
+第 7 秒：装满一杯热水。
+示例 3：
+
+输入：amount = [5,0,0]
+输出：5
+解释：每秒装满一杯冷水。
+ 
+
+提示：
+
+amount.length == 3
+0 <= amount[i] <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/minimum-amount-of-time-to-fill-cups
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int fillCups(vector<int>& amount) {
+        sort(amount.begin(), amount.end());
+
+        if (amount[0] + amount[1] <= amount[2]) {
+            return amount[2];
+        }
+
+        int left = amount[0] + amount[1] - amount[2];
+
+        return left / 2 + left % 2 + amount[2];
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> amount = { 1,4,2 };
+    check.checkInt(4, o.fillCups(amount));
+
+    amount = { 5,4,4 };
+    check.checkInt(7, o.fillCups(amount));
+
+    amount = { 5,0,0 };
+    check.checkInt(5, o.fillCups(amount));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
