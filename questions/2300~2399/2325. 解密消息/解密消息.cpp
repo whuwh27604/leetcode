0cb15d8b1ffe9601b1a4faa78206bc8b1d25cc6b
whuwh27604 <5991277@qq.com>
2022-07-19 21:37:28 +0800

    add

---
+++ questions/2300~2399/2325. 解密消息/解密消息.cpp
@@ -0,0 +1,89 @@
+﻿/* 解密消息.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你字符串 key 和 message ，分别表示一个加密密钥和一段加密消息。解密 message 的步骤如下：
+
+使用 key 中 26 个英文小写字母第一次出现的顺序作为替换表中的字母 顺序 。
+将替换表与普通英文字母表对齐，形成对照表。
+按照对照表 替换 message 中的每个字母。
+空格 ' ' 保持不变。
+例如，key = "happy boy"（实际的加密密钥会包含字母表中每个字母 至少一次），据此，可以得到部分对照表（'h' -> 'a'、'a' -> 'b'、'p' -> 'c'、'y' -> 'd'、'b' -> 'e'、'o' -> 'f'）。
+返回解密后的消息。
+
+ 
+
+示例 1：
+
+
+
+输入：key = "the quick brown fox jumps over the lazy dog", message = "vkbs bs t suepuv"
+输出："this is a secret"
+解释：对照表如上图所示。
+提取 "the quick brown fox jumps over the lazy dog" 中每个字母的首次出现可以得到替换表。
+示例 2：
+
+
+
+输入：key = "eljuxhpwnyrdgtqkviszcfmabo", message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb"
+输出："the five boxing wizards jump quickly"
+解释：对照表如上图所示。
+提取 "eljuxhpwnyrdgtqkviszcfmabo" 中每个字母的首次出现可以得到替换表。
+ 
+
+提示：
+
+26 <= key.length <= 2000
+key 由小写英文字母及 ' ' 组成
+key 包含英文字母表中每个字符（'a' 到 'z'）至少一次
+1 <= message.length <= 2000
+message 由小写英文字母和 ' ' 组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/decode-the-message
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        vector<char> table('z' + 1, 0);
+        table[' '] = ' ';
+        char last = 'a';
+        string decode;
+
+        for (char c : key) {
+            if (table[c] == 0) {
+                table[c] = last++;
+            }
+        }
+
+        for (char c : message) {
+            decode += table[c];
+        }
+
+        return decode;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkString("this is a secret", o.decodeMessage("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv"));
+    check.checkString("the five boxing wizards jump quickly", o.decodeMessage("eljuxhpwnyrdgtqkviszcfmabo", "zwx hnfx lqantp mnoeius ycgk vcnjrdb"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
