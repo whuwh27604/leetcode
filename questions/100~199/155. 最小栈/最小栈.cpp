cdad602a2e8c5db9b570d9f8124fd7146a45f37b
whuwh27604 <5991277@qq.com>
2022-07-30 22:08:10 +0800

    add

--- questions/100~199/155. 最小栈/最小栈.cpp
+++ questions/100~199/155. 最小栈/最小栈.cpp
@@ -1,12 +1,25 @@
 ﻿/* 最小栈.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
 
-push(x) —— 将元素 x 推入栈中。
-pop() —— 删除栈顶的元素。
-top() —— 获取栈顶元素。
-getMin() —— 检索栈中的最小元素。
-示例:
+实现 MinStack 类:
 
+MinStack() 初始化堆栈对象。
+void push(int val) 将元素val推入堆栈。
+void pop() 删除堆栈顶部的元素。
+int top() 获取堆栈顶部的元素。
+int getMin() 获取堆栈中的最小元素。
+ 
+
+示例 1:
+
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+输出：
+[null,null,null,null,-3,null,0,-2]
+
+解释：
 MinStack minStack = new MinStack();
 minStack.push(-2);
 minStack.push(0);
@@ -15,159 +28,114 @@ minStack.getMin();   --> 返回 -3.
 minStack.pop();
 minStack.top();      --> 返回 0.
 minStack.getMin();   --> 返回 -2.
+ 
+
+提示：
+
+-231 <= val <= 231 - 1
+pop、top 和 getMin 操作总是在 非空栈 上调用
+push, pop, top, and getMin最多被调用 3 * 104 次
 
 来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/min-stack
+链接：https://leetcode.cn/problems/min-stack
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
 #include <iostream>
+#include <stack>
+#include <algorithm>
 #include "../check/CheckResult.h"
 
 using namespace std;
 
 class MinStack {
 public:
-    /** initialize your data structure here. */
     MinStack() {
-        topNode = NULL;
-        minVal = INT_MAX;
+
     }
 
-    ~MinStack() {
-        while (topNode != NULL) {
-            pop();
-        }
-    }
-
-    void push(int x) {
-        StackListNode* node;
-        if (x <= minVal) {
-            // 如果新进栈的数不大于当前最小值，先保存当前最小值
-            node = new StackListNode(minVal);
-            node->prev = topNode;
-            topNode = node;
-            minVal = x;
-        }
-
-        node = new StackListNode(x);
-        node->prev = topNode;
-        topNode = node;
+    void push(int val) {
+        int minimum = valueMins.empty() ? val : min(val, valueMins.top().second);
+        valueMins.push({ val,minimum });
     }
 
     void pop() {
-        if (topNode == NULL) {
-            return;
-        }
-
-        // 先删除栈顶
-        StackListNode* node = topNode;
-        int val = node->val;
-        topNode = topNode->prev;
-        delete node;
-
-        // 如果最小值出栈了，恢复一个最小值出来
-        if (val == minVal) {
-            node = topNode;
-            minVal = node->val;
-            topNode = topNode->prev;
-            delete node;
-        }
+        valueMins.pop();
     }
 
     int top() {
-        if (topNode != NULL) {
-            return topNode->val;
-        }
-        return INT_MIN;
+        return valueMins.top().first;
     }
 
     int getMin() {
-        if (topNode != NULL) {
-            return minVal;
-        }
-        return INT_MIN;
+        return valueMins.top().second;
     }
 
 private:
-    int minVal;
-    struct StackListNode {
-        int val;
-        StackListNode* prev;
-        StackListNode(int x) : val(x), prev(NULL) {}
-    };
-    StackListNode* topNode;
+    stack<pair<int, int>> valueMins;
 };
 
 int main()
 {
-    MinStack myStack;
     CheckResult check;
-    
-    myStack.push(-2);
-    myStack.push(0);
-    myStack.push(-3);
-    check.checkInt(-3, myStack.getMin());
-    myStack.pop();
-    myStack.top();
-    check.checkInt(0, myStack.top());
-    check.checkInt(-2, myStack.getMin());
 
-    MinStack myStack1;
-    myStack1.pop();
-    check.checkInt(INT_MIN, myStack1.top());
-    check.checkInt(INT_MIN, myStack1.getMin());
-    
-    MinStack myStack2;
-    myStack2.push(8);
-    check.checkInt(8, myStack2.top());
-    check.checkInt(8, myStack2.getMin());
-    myStack2.push(7);
-    check.checkInt(7, myStack2.top());
-    check.checkInt(7, myStack2.getMin());
-    myStack2.push(6);
-    check.checkInt(6, myStack2.top());
-    check.checkInt(6, myStack2.getMin());
-    myStack2.push(5);
-    check.checkInt(5, myStack2.top());
-    check.checkInt(5, myStack2.getMin());
-    myStack2.push(1);
-    check.checkInt(1, myStack2.top());
-    check.checkInt(1, myStack2.getMin());
-    myStack2.push(2);
-    check.checkInt(2, myStack2.top());
-    check.checkInt(1, myStack2.getMin());
-    myStack2.push(3);
-    check.checkInt(3, myStack2.top());
-    check.checkInt(1, myStack2.getMin());
-    myStack2.push(4);
-    check.checkInt(4, myStack2.top());
-    check.checkInt(1, myStack2.getMin());
-    myStack2.pop();
-    check.checkInt(3, myStack2.top());
-    check.checkInt(1, myStack2.getMin());
-    myStack2.pop();
-    check.checkInt(2, myStack2.top());
-    check.checkInt(1, myStack2.getMin());
-    myStack2.pop();
-    check.checkInt(1, myStack2.top());
-    check.checkInt(1, myStack2.getMin());
-    myStack2.pop();
-    check.checkInt(5, myStack2.top());
-    check.checkInt(5, myStack2.getMin());
-    myStack2.pop();
-    check.checkInt(6, myStack2.top());
-    check.checkInt(6, myStack2.getMin());
-    myStack2.pop();
-    check.checkInt(7, myStack2.top());
-    check.checkInt(7, myStack2.getMin());
-    myStack2.pop();
-    check.checkInt(8, myStack2.top());
-    check.checkInt(8, myStack2.getMin());
-    myStack2.pop();
-    check.checkInt(INT_MIN, myStack2.top());
-    check.checkInt(INT_MIN, myStack2.getMin());
+    MinStack o1;
+    o1.push(-2);
+    o1.push(0);
+    o1.push(-3);
+    check.checkInt(-3, o1.getMin());
+    o1.pop();
+    o1.top();
+    check.checkInt(0, o1.top());
+    check.checkInt(-2, o1.getMin());
 
+    MinStack o2;
+    o2.push(8);
+    check.checkInt(8, o2.top());
+    check.checkInt(8, o2.getMin());
+    o2.push(7);
+    check.checkInt(7, o2.top());
+    check.checkInt(7, o2.getMin());
+    o2.push(6);
+    check.checkInt(6, o2.top());
+    check.checkInt(6, o2.getMin());
+    o2.push(5);
+    check.checkInt(5, o2.top());
+    check.checkInt(5, o2.getMin());
+    o2.push(1);
+    check.checkInt(1, o2.top());
+    check.checkInt(1, o2.getMin());
+    o2.push(2);
+    check.checkInt(2, o2.top());
+    check.checkInt(1, o2.getMin());
+    o2.push(3);
+    check.checkInt(3, o2.top());
+    check.checkInt(1, o2.getMin());
+    o2.push(4);
+    check.checkInt(4, o2.top());
+    check.checkInt(1, o2.getMin());
+    o2.pop();
+    check.checkInt(3, o2.top());
+    check.checkInt(1, o2.getMin());
+    o2.pop();
+    check.checkInt(2, o2.top());
+    check.checkInt(1, o2.getMin());
+    o2.pop();
+    check.checkInt(1, o2.top());
+    check.checkInt(1, o2.getMin());
+    o2.pop();
+    check.checkInt(5, o2.top());
+    check.checkInt(5, o2.getMin());
+    o2.pop();
+    check.checkInt(6, o2.top());
+    check.checkInt(6, o2.getMin());
+    o2.pop();
+    check.checkInt(7, o2.top());
+    check.checkInt(7, o2.getMin());
+    o2.pop();
+    check.checkInt(8, o2.top());
+    check.checkInt(8, o2.getMin());
+    o2.pop();
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
