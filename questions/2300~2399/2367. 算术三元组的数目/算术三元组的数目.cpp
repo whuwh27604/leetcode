d6821600e39a88fdf27bff0e77bbdd6ac3396f5a
whuwh27604 <5991277@qq.com>
2022-08-20 00:10:39 +0800

    add

---
+++ questions/2300~2399/2367. 算术三元组的数目/算术三元组的数目.cpp
@@ -0,0 +1,85 @@
+﻿/* 算术三元组的数目.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始、严格递增 的整数数组 nums 和一个正整数 diff 。如果满足下述全部条件，则三元组 (i, j, k) 就是一个 算术三元组 ：
+
+i < j < k ，
+nums[j] - nums[i] == diff 且
+nums[k] - nums[j] == diff
+返回不同 算术三元组 的数目。
+
+ 
+
+示例 1：
+
+输入：nums = [0,1,4,6,7,10], diff = 3
+输出：2
+解释：
+(1, 2, 4) 是算术三元组：7 - 4 == 3 且 4 - 1 == 3 。
+(2, 4, 5) 是算术三元组：10 - 7 == 3 且 7 - 4 == 3 。
+示例 2：
+
+输入：nums = [4,5,6,7,8,9], diff = 2
+输出：2
+解释：
+(0, 2, 4) 是算术三元组：8 - 6 == 2 且 6 - 4 == 2 。
+(1, 3, 5) 是算术三元组：9 - 7 == 2 且 7 - 5 == 2 。
+ 
+
+提示：
+
+3 <= nums.length <= 200
+0 <= nums[i] <= 200
+1 <= diff <= 50
+nums 严格 递增
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/number-of-arithmetic-triplets
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        vector<bool> exist(nums.back() + diff + 1, false);
+        int triplets = 0;
+
+        for (int num : nums) {
+            exist[num] = true;
+        }
+
+        for (int num : nums) {
+            if (exist[num + diff] && exist[num + diff + diff]) {
+                ++triplets;
+            }
+        }
+
+        return triplets;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 0,1,4,6,7,10 };
+    check.checkInt(2, o.arithmeticTriplets(nums, 3));
+
+    nums = { 4,5,6,7,8,9 };
+    check.checkInt(2, o.arithmeticTriplets(nums, 2));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
