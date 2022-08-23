a375f99da5cdb6b05ac1f7630f3631668240df50
whuwh27604 <5991277@qq.com>
2022-08-23 12:59:34 +0800

    add

---
+++ questions/2300~2399/2374. 边积分最高的节点/边积分最高的节点.cpp
@@ -0,0 +1,96 @@
+﻿/* 边积分最高的节点.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个有向图，图中有 n 个节点，节点编号从 0 到 n - 1 ，其中每个节点都 恰有一条 出边。
+
+图由一个下标从 0 开始、长度为 n 的整数数组 edges 表示，其中 edges[i] 表示存在一条从节点 i 到节点 edges[i] 的 有向 边。
+
+节点 i 的 边积分 定义为：所有存在一条指向节点 i 的边的节点的 编号 总和。
+
+返回 边积分 最高的节点。如果多个节点的 边积分 相同，返回编号 最小 的那个。
+
+ 
+
+示例 1：
+
+
+输入：edges = [1,0,0,0,0,7,7,5]
+输出：7
+解释：
+- 节点 1、2、3 和 4 都有指向节点 0 的边，节点 0 的边积分等于 1 + 2 + 3 + 4 = 10 。
+- 节点 0 有一条指向节点 1 的边，节点 1 的边积分等于 0 。
+- 节点 7 有一条指向节点 5 的边，节点 5 的边积分等于 7 。
+- 节点 5 和 6 都有指向节点 7 的边，节点 7 的边积分等于 5 + 6 = 11 。
+节点 7 的边积分最高，所以返回 7 。
+示例 2：
+
+
+输入：edges = [2,0,0,2]
+输出：0
+解释：
+- 节点 1 和 2 都有指向节点 0 的边，节点 0 的边积分等于 1 + 2 = 3 。
+- 节点 0 和 3 都有指向节点 2 的边，节点 2 的边积分等于 0 + 3 = 3 。
+节点 0 和 2 的边积分都是 3 。由于节点 0 的编号更小，返回 0 。
+ 
+
+提示：
+
+n == edges.length
+2 <= n <= 105
+0 <= edges[i] < n
+edges[i] != i
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/node-with-highest-edge-score
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int edgeScore(vector<int>& edges) {
+        int n = edges.size(), maxNode = 0;
+        long long maxScore = -1;
+        vector<long long> scores(n, 0);
+
+        for (int node = 0; node < n; ++node) {
+            if (edges[node] != -1) {
+                scores[edges[node]] += node;
+            }
+        }
+
+        for (int node = 0; node < n; ++node) {
+            if (scores[node] > maxScore) {
+                maxNode = node;
+                maxScore = scores[node];
+            }
+        }
+
+        return maxNode;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> edges = { 1,0,0,0,0,7,7,5 };
+    check.checkInt(7, o.edgeScore(edges));
+
+    edges = { 2,0,0,2 };
+    check.checkInt(0, o.edgeScore(edges));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
