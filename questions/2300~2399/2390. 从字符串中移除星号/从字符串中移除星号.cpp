1fb39486b7745ebde4ed125ba5be132f707fd6f3
whuwh27604 <5991277@qq.com>
2022-09-01 20:36:03 +0800

    add

---
+++ questions/2300~2399/2390. 从字符串中移除星号/从字符串中移除星号.cpp
@@ -0,0 +1,84 @@
+﻿/* 从字符串中移除星号.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个包含若干星号 * 的字符串 s 。
+
+在一步操作中，你可以：
+
+选中 s 中的一个星号。
+移除星号 左侧 最近的那个 非星号 字符，并移除该星号自身。
+返回移除 所有 星号之后的字符串。
+
+注意：
+
+生成的输入保证总是可以执行题面中描述的操作。
+可以证明结果字符串是唯一的。
+ 
+
+示例 1：
+
+输入：s = "leet**cod*e"
+输出："lecoe"
+解释：从左到右执行移除操作：
+- 距离第 1 个星号最近的字符是 "leet**cod*e" 中的 't' ，s 变为 "lee*cod*e" 。
+- 距离第 2 个星号最近的字符是 "lee*cod*e" 中的 'e' ，s 变为 "lecod*e" 。
+- 距离第 3 个星号最近的字符是 "lecod*e" 中的 'd' ，s 变为 "lecoe" 。
+不存在其他星号，返回 "lecoe" 。
+示例 2：
+
+输入：s = "erase*****"
+输出：""
+解释：整个字符串都会被移除，所以返回空字符串。
+ 
+
+提示：
+
+1 <= s.length <= 105
+s 由小写英文字母和星号 * 组成
+s 可以执行上述操作
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/removing-stars-from-a-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans;
+
+        for (char c : s) {
+            if (c == '*') {
+                ans.pop_back();
+            }
+            else {
+                ans.push_back(c);
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkString("lecoe", o.removeStars("leet**cod*e"));
+    check.checkString("", o.removeStars("erase*****"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
