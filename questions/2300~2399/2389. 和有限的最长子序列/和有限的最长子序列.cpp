b74891518719448bd903b5fe263c6919a64dfe51
whuwh27604 <5991277@qq.com>
2022-09-04 15:57:27 +0800

    add

---
+++ questions/2300~2399/2389. 和有限的最长子序列/和有限的最长子序列.cpp
@@ -0,0 +1,90 @@
+﻿/* 和有限的最长子序列.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个长度为 n 的整数数组 nums ，和一个长度为 m 的整数数组 queries 。
+
+返回一个长度为 m 的数组 answer ，其中 answer[i] 是 nums 中 元素之和小于等于 queries[i] 的 子序列 的 最大 长度  。
+
+子序列 是由一个数组删除某些元素（也可以不删除）但不改变剩余元素顺序得到的一个数组。
+
+ 
+
+示例 1：
+
+输入：nums = [4,5,2,1], queries = [3,10,21]
+输出：[2,3,4]
+解释：queries 对应的 answer 如下：
+- 子序列 [2,1] 的和小于或等于 3 。可以证明满足题目要求的子序列的最大长度是 2 ，所以 answer[0] = 2 。
+- 子序列 [4,5,1] 的和小于或等于 10 。可以证明满足题目要求的子序列的最大长度是 3 ，所以 answer[1] = 3 。
+- 子序列 [4,5,2,1] 的和小于或等于 21 。可以证明满足题目要求的子序列的最大长度是 4 ，所以 answer[2] = 4 。
+示例 2：
+
+输入：nums = [2,3,4,5], queries = [1]
+输出：[0]
+解释：空子序列是唯一一个满足元素和小于或等于 1 的子序列，所以 answer[0] = 0 。
+ 
+
+提示：
+
+n == nums.length
+m == queries.length
+1 <= n, m <= 1000
+1 <= nums[i], queries[i] <= 106
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/longest-subsequence-with-limited-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size(), m = queries.size();
+        vector<int> sums(n + 1, 0), ans(m);
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n; ++i) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+
+        for (int i = 0; i < m; ++i) {
+            ans[i] = distance(sums.begin(), upper_bound(sums.begin(), sums.end(), queries[i])) - 1;
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 4,5,2,1 };
+    vector<int> queries = { 3,10,21 };
+    vector<int> actual = o.answerQueries(nums, queries);
+    vector<int> expected = { 2,3,4 };
+    check.checkIntVector(expected, actual);
+
+    nums = { 2,3,4,5 };
+    queries = { 1 };
+    actual = o.answerQueries(nums, queries);
+    expected = { 0 };
+    check.checkIntVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
