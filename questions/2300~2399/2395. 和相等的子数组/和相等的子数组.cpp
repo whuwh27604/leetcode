f0770b87e7cc9d09313e0caf6b32eac07312faf8
whuwh27604 <5991277@qq.com>
2022-09-06 00:22:13 +0800

    add

---
+++ questions/2300~2399/2395. 和相等的子数组/和相等的子数组.cpp
@@ -0,0 +1,83 @@
+﻿/* 和相等的子数组.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums ，判断是否存在 两个 长度为 2 的子数组且它们的 和 相等。注意，这两个子数组起始位置的下标必须 不相同 。
+
+如果这样的子数组存在，请返回 true，否则返回 false 。
+
+子数组 是一个数组中一段连续非空的元素组成的序列。
+
+ 
+
+示例 1：
+
+输入：nums = [4,2,4]
+输出：true
+解释：元素为 [4,2] 和 [2,4] 的子数组有相同的和 6 。
+示例 2：
+
+输入：nums = [1,2,3,4,5]
+输出：false
+解释：没有长度为 2 的两个子数组和相等。
+示例 3：
+
+输入：nums = [0,0,0]
+输出：true
+解释：子数组 [nums[0],nums[1]] 和 [nums[1],nums[2]] 的和相等，都为 0 。
+注意即使子数组的元素相同，这两个子数组也视为不相同的子数组，因为它们在原数组中的起始位置不同。
+ 
+
+提示：
+
+2 <= nums.length <= 1000
+-109 <= nums[i] <= 109
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/find-subarrays-with-equal-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool findSubarrays(vector<int>& nums) {
+        unordered_map<int, int> sums;
+
+        for (int i = 0; i < (int)nums.size() - 1; ++i) {
+            if (++sums[nums[i] + nums[i + 1]] == 2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 4,2,4 };
+    check.checkBool(true, o.findSubarrays(nums));
+
+    nums = { 1,2,3,4,5 };
+    check.checkBool(false, o.findSubarrays(nums));
+
+    nums = { 0,0,0 };
+    check.checkBool(true, o.findSubarrays(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
