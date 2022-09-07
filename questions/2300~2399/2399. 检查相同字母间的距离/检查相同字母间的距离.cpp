795819389887a5387147a23106f0e97a9ffcdf93
whuwh27604 <5991277@qq.com>
2022-09-08 00:09:06 +0800

    add

---
+++ questions/2300~2399/2399. 检查相同字母间的距离/检查相同字母间的距离.cpp
@@ -0,0 +1,89 @@
+﻿/* 检查相同字母间的距离.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的字符串 s ，该字符串仅由小写英文字母组成，s 中的每个字母都 恰好 出现 两次 。另给你一个下标从 0 开始、长度为 26 的的整数数组 distance 。
+
+字母表中的每个字母按从 0 到 25 依次编号（即，'a' -> 0, 'b' -> 1, 'c' -> 2, ... , 'z' -> 25）。
+
+在一个 匀整 字符串中，第 i 个字母的两次出现之间的字母数量是 distance[i] 。如果第 i 个字母没有在 s 中出现，那么 distance[i] 可以 忽略 。
+
+如果 s 是一个 匀整 字符串，返回 true ；否则，返回 false 。
+
+ 
+
+示例 1：
+
+输入：s = "abaccb", distance = [1,3,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+输出：true
+解释：
+- 'a' 在下标 0 和下标 2 处出现，所以满足 distance[0] = 1 。
+- 'b' 在下标 1 和下标 5 处出现，所以满足 distance[1] = 3 。
+- 'c' 在下标 3 和下标 4 处出现，所以满足 distance[2] = 0 。
+注意 distance[3] = 5 ，但是由于 'd' 没有在 s 中出现，可以忽略。
+因为 s 是一个匀整字符串，返回 true 。
+示例 2：
+
+输入：s = "aa", distance = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+输出：false
+解释：
+- 'a' 在下标 0 和 1 处出现，所以两次出现之间的字母数量为 0 。
+但是 distance[0] = 1 ，s 不是一个匀整字符串。
+ 
+
+提示：
+
+2 <= s.length <= 52
+s 仅由小写英文字母组成
+s 中的每个字母恰好出现两次
+distance.length == 26
+0 <= distance[i] <= 50
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/check-distances-between-same-letters
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkDistances(string s, vector<int>& distance) {
+        vector<vector<int>> indices(26);
+
+        for (int i = 0; i < (int)s.size(); ++i) {
+            indices[s[i] - 'a'].push_back(i);
+        }
+
+        for (int i = 0; i < 26; ++i) {
+            if (!indices[i].empty() && indices[i][1] - indices[i][0] - 1 != distance[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> distance = { 1,3,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+    check.checkBool(true, o.checkDistances("abaccb", distance));
+
+    distance = { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+    check.checkBool(false, o.checkDistances("aa", distance));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
