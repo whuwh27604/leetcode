dcb5a43f300a562709cb0e4650bc59e6e27f9e36
whuwh27604 <5991277@qq.com>
2022-09-17 11:35:32 +0800

    add

---
+++ questions/2400~2499/2405. 子字符串的最优划分/子字符串的最优划分.cpp
@@ -0,0 +1,78 @@
+﻿/* 子字符串的最优划分.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个字符串 s ，请你将该字符串划分成一个或多个 子字符串 ，并满足每个子字符串中的字符都是 唯一 的。也就是说，在单个子字符串中，字母的出现次数都不超过 一次 。
+
+满足题目要求的情况下，返回 最少 需要划分多少个子字符串。
+
+注意，划分后，原字符串中的每个字符都应该恰好属于一个子字符串。
+
+ 
+
+示例 1：
+
+输入：s = "abacaba"
+输出：4
+解释：
+两种可行的划分方法分别是 ("a","ba","cab","a") 和 ("ab","a","ca","ba") 。
+可以证明最少需要划分 4 个子字符串。
+示例 2：
+
+输入：s = "ssssss"
+输出：6
+解释：
+只存在一种可行的划分方法 ("s","s","s","s","s","s") 。
+ 
+
+提示：
+
+1 <= s.length <= 105
+s 仅由小写英文字母组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/optimal-partition-of-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int partitionString(string s) {
+        vector<bool> exist(26, true);
+        int count = 0;
+
+        for (char c : s) {
+            if (exist[c - 'a']) {
+                ++count;
+                exist = vector<bool>(26, false);
+            }
+
+            exist[c - 'a'] = true;
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkInt(4, o.partitionString("abacaba"));
+    check.checkInt(6, o.partitionString("ssssss"));
+    check.checkInt(1, o.partitionString("z"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
