87550b03cd9f75d9260844460863a02613247d97
whuwh27604 <5991277@qq.com>
2022-09-27 18:11:00 +0800

    add

---
+++ questions/2400~2499/2419. 按位与最大的最长子数组/按位与最大的最长子数组.cpp
@@ -0,0 +1,86 @@
+﻿/* 按位与最大的最长子数组.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个长度为 n 的整数数组 nums 。
+
+考虑 nums 中进行 按位与（bitwise AND）运算得到的值 最大 的 非空 子数组。
+
+换句话说，令 k 是 nums 任意 子数组执行按位与运算所能得到的最大值。那么，只需要考虑那些执行一次按位与运算后等于 k 的子数组。
+返回满足要求的 最长 子数组的长度。
+
+数组的按位与就是对数组中的所有数字进行按位与运算。
+
+子数组 是数组中的一个连续元素序列。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,3,3,2,2]
+输出：2
+解释：
+子数组按位与运算的最大值是 3 。
+能得到此结果的最长子数组是 [3,3]，所以返回 2 。
+示例 2：
+
+输入：nums = [1,2,3,4]
+输出：1
+解释：
+子数组按位与运算的最大值是 4 。
+能得到此结果的最长子数组是 [4]，所以返回 1 。
+ 
+
+提示：
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/longest-subarray-with-maximum-bitwise-and
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxNum = *max_element(nums.begin(), nums.end()), len = 0, maxLen = 0;
+
+        for (int num : nums) {
+            if (num == maxNum) {
+                maxLen = max(maxLen, ++len);
+            }
+            else {
+                len = 0;
+            }
+        }
+
+        return maxLen;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,2,3,3,2,2 };
+    check.checkInt(2, o.longestSubarray(nums));
+
+    nums = { 1,2,3,4 };
+    check.checkInt(1, o.longestSubarray(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
