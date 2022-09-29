788438a3771d51e7e0ee98483c51a9485f66a0df
whuwh27604 <5991277@qq.com>
2022-09-29 22:15:44 +0800

    add

---
+++ questions/面试题/面试题 01.09. 字符串轮转/字符串轮转.cpp
@@ -0,0 +1,56 @@
+﻿/* 字符串轮转.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成（比如，waterbottle是erbottlewat旋转后的字符串）。
+
+示例1:
+
+ 输入：s1 = "waterbottle", s2 = "erbottlewat"
+ 输出：True
+示例2:
+
+ 输入：s1 = "aa", s2 = "aba"
+ 输出：False
+提示：
+
+字符串长度在[0, 100000]范围内。
+说明:
+
+你能只调用一次检查子串的方法吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/string-rotation-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isFlipedString(string s1, string s2) {
+        string s = s1 + s1;
+
+        return s1.size() == s2.size() && s.find(s2) != s.npos;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkBool(true, o.isFlipedString("waterbottle", "erbottlewat"));
+    check.checkBool(false, o.isFlipedString("aa", "aba"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
