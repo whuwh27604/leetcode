9f379680fb2535ba559c377eb3b95e561c8835c5
whuwh27604 <5991277@qq.com>
2022-10-19 00:26:42 +0800

    add

---
+++ questions/2400~2499/2439. 最小化数组中的最大值/最小化数组中的最大值.cpp
@@ -0,0 +1,85 @@
+﻿/* 最小化数组中的最大值.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的数组 nums ，它含有 n 个非负整数。
+
+每一步操作中，你需要：
+
+选择一个满足 1 <= i < n 的整数 i ，且 nums[i] > 0 。
+将 nums[i] 减 1 。
+将 nums[i - 1] 加 1 。
+你可以对数组执行 任意 次上述操作，请你返回可以得到的 nums 数组中 最大值 最小 为多少。
+
+ 
+
+示例 1：
+
+输入：nums = [3,7,1,6]
+输出：5
+解释：
+一串最优操作是：
+1. 选择 i = 1 ，nums 变为 [4,6,1,6] 。
+2. 选择 i = 3 ，nums 变为 [4,6,2,5] 。
+3. 选择 i = 1 ，nums 变为 [5,5,2,5] 。
+nums 中最大值为 5 。无法得到比 5 更小的最大值。
+所以我们返回 5 。
+示例 2：
+
+输入：nums = [10,1]
+输出：10
+解释：
+最优解是不改动 nums ，10 是最大值，所以返回 10 。
+ 
+
+提示：
+
+n == nums.length
+2 <= n <= 105
+0 <= nums[i] <= 109
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/minimize-maximum-of-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        long long sum = 0;
+        int ans = 0;
+
+        for (int i = 0; i < (int)nums.size(); ++i) {
+            sum += nums[i];
+            ans = max(ans, (int)((sum + i) / ((long long)i + 1)));
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 3,7,1,6 };
+    check.checkInt(5, o.minimizeArrayValue(nums));
+
+    nums = { 10,1 };
+    check.checkInt(10, o.minimizeArrayValue(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
