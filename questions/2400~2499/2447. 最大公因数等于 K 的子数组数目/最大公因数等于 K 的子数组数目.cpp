056ca43bd1d71bb6e74eb0acc4c1d09f59ca8829
whuwh27604 <5991277@qq.com>
2022-10-27 20:54:07 +0800

    add

---
+++ questions/2400~2499/2447. 最大公因数等于 K 的子数组数目/最大公因数等于 K 的子数组数目.cpp
@@ -0,0 +1,86 @@
+﻿/* 最大公因数等于 K 的子数组数目.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数数组 nums 和一个整数 k ，请你统计并返回 nums 的子数组中元素的最大公因数等于 k 的子数组数目。
+
+子数组 是数组中一个连续的非空序列。
+
+数组的最大公因数 是能整除数组中所有元素的最大整数。
+
+ 
+
+示例 1：
+
+输入：nums = [9,3,1,2,6,3], k = 3
+输出：4
+解释：nums 的子数组中，以 3 作为最大公因数的子数组如下：
+- [9,3,1,2,6,3]
+- [9,3,1,2,6,3]
+- [9,3,1,2,6,3]
+- [9,3,1,2,6,3]
+示例 2：
+
+输入：nums = [4], k = 7
+输出：0
+解释：不存在以 7 作为最大公因数的子数组。
+ 
+
+提示：
+
+1 <= nums.length <= 1000
+1 <= nums[i], k <= 109
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/number-of-subarrays-with-gcd-equal-to-k
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int subarrayGCD(vector<int>& nums, int k) {
+        int size = nums.size(), subarrays = 0;
+
+        for (int i = 0; i < size; ++i) {
+            int result = nums[i];
+
+            for (int j = i; j < size && result >= k; ++j) {
+                result = gcd(result, nums[j]);
+                if (result == k) {
+                    ++subarrays;
+                }
+            }
+        }
+
+        return subarrays;
+    }
+
+    int gcd(int x, int y) {
+        return y == 0 ? x : gcd(y, x % y);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 9,3,1,2,6,3 };
+    check.checkInt(4, o.subarrayGCD(nums, 3));
+
+    nums = { 4 };
+    check.checkInt(0, o.subarrayGCD(nums, 7));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
