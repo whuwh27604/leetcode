135655953c232aaae0eef009c2943c52534afcdd
whuwh27604 <5991277@qq.com>
2022-10-31 20:26:52 +0800

    add

---
+++ questions/2400~2499/2453. 摧毁一系列目标/摧毁一系列目标.cpp
@@ -0,0 +1,97 @@
+﻿/* 摧毁一系列目标.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的数组 nums ，它包含若干正整数，表示数轴上你需要摧毁的目标所在的位置。同时给你一个整数 space 。
+
+你有一台机器可以摧毁目标。给机器 输入 nums[i] ，这台机器会摧毁所有位置在 nums[i] + c * space 的目标，其中 c 是任意非负整数。你想摧毁 nums 中 尽可能多 的目标。
+
+请你返回在摧毁数目最多的前提下，nums[i] 的 最小值 。
+
+ 
+
+示例 1：
+
+输入：nums = [3,7,8,1,1,5], space = 2
+输出：1
+解释：如果我们输入 nums[3] ，我们可以摧毁位于 1,3,5,7,9,... 这些位置的目标。
+这种情况下， 我们总共可以摧毁 5 个目标（除了 nums[2]）。
+没有办法摧毁多于 5 个目标，所以我们返回 nums[3] 。
+示例 2：
+
+输入：nums = [1,3,5,2,4,6], space = 2
+输出：1
+解释：输入 nums[0] 或者 nums[3] 都会摧毁 3 个目标。
+没有办法摧毁多于 3 个目标。
+由于 nums[0] 是最小的可以摧毁 3 个目标的整数，所以我们返回 1 。
+示例 3：
+
+输入：nums = [6,2,5], space = 100
+输出：2
+解释：无论我们输入哪个数字，都只能摧毁 1 个目标。输入的最小整数是 nums[1] 。
+ 
+
+提示：
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+1 <= space <= 109
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/destroy-sequential-targets
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int destroyTargets(vector<int>& nums, int space) {
+        unordered_map<int, vector<int>> remainders;
+        int ans = INT_MAX, maxSize = 0;
+
+        for (int num : nums) {
+            remainders[num % space].push_back(num);
+        }
+
+        for (auto& remainder : remainders) {
+            maxSize = max(maxSize, (int)remainder.second.size());
+        }
+
+        for (auto& remainder : remainders) {
+            if (remainder.second.size() == maxSize) {
+                ans = min(ans, *min_element(remainder.second.begin(), remainder.second.end()));
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 3,7,8,1,1,5 };
+    check.checkInt(1, o.destroyTargets(nums, 2));
+
+    nums = { 1,3,5,2,4,6 };
+    check.checkInt(1, o.destroyTargets(nums, 2));
+
+    nums = { 6,2,5 };
+    check.checkInt(2, o.destroyTargets(nums, 100));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
