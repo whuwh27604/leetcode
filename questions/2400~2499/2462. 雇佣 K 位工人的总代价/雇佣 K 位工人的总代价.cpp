816f3184e93781ecc71d4ed443fdce3cdb7d7c61
whuwh27604 <5991277@qq.com>
2022-11-12 16:07:11 +0800

    add

---
+++ questions/2400~2499/2462. 雇佣 K 位工人的总代价/雇佣 K 位工人的总代价.cpp
@@ -0,0 +1,111 @@
+﻿/* 雇佣 K 位工人的总代价.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 costs ，其中 costs[i] 是雇佣第 i 位工人的代价。
+
+同时给你两个整数 k 和 candidates 。我们想根据以下规则恰好雇佣 k 位工人：
+
+总共进行 k 轮雇佣，且每一轮恰好雇佣一位工人。
+在每一轮雇佣中，从最前面 candidates 和最后面 candidates 人中选出代价最小的一位工人，如果有多位代价相同且最小的工人，选择下标更小的一位工人。
+比方说，costs = [3,2,7,7,1,2] 且 candidates = 2 ，第一轮雇佣中，我们选择第 4 位工人，因为他的代价最小 [3,2,7,7,1,2] 。
+第二轮雇佣，我们选择第 1 位工人，因为他们的代价与第 4 位工人一样都是最小代价，而且下标更小，[3,2,7,7,2] 。注意每一轮雇佣后，剩余工人的下标可能会发生变化。
+如果剩余员工数目不足 candidates 人，那么下一轮雇佣他们中代价最小的一人，如果有多位代价相同且最小的工人，选择下标更小的一位工人。
+一位工人只能被选择一次。
+返回雇佣恰好 k 位工人的总代价。
+
+
+
+示例 1：
+
+输入：costs = [17,12,10,2,7,2,11,20,8], k = 3, candidates = 4
+输出：11
+解释：我们总共雇佣 3 位工人。总代价一开始为 0 。
+- 第一轮雇佣，我们从 [17,12,10,2,7,2,11,20,8] 中选择。最小代价是 2 ，有两位工人，我们选择下标更小的一位工人，即第 3 位工人。总代价是 0 + 2 = 2 。
+- 第二轮雇佣，我们从 [17,12,10,7,2,11,20,8] 中选择。最小代价是 2 ，下标为 4 ，总代价是 2 + 2 = 4 。
+- 第三轮雇佣，我们从 [17,12,10,7,11,20,8] 中选择，最小代价是 7 ，下标为 3 ，总代价是 4 + 7 = 11 。注意下标为 3 的工人同时在最前面和最后面 4 位工人中。
+总雇佣代价是 11 。
+示例 2：
+
+输入：costs = [1,2,4,1], k = 3, candidates = 3
+输出：4
+解释：我们总共雇佣 3 位工人。总代价一开始为 0 。
+- 第一轮雇佣，我们从 [1,2,4,1] 中选择。最小代价为 1 ，有两位工人，我们选择下标更小的一位工人，即第 0 位工人，总代价是 0 + 1 = 1 。注意，下标为 1 和 2 的工人同时在最前面和最后面 3 位工人中。
+- 第二轮雇佣，我们从 [2,4,1] 中选择。最小代价为 1 ，下标为 2 ，总代价是 1 + 1 = 2 。
+- 第三轮雇佣，少于 3 位工人，我们从剩余工人 [2,4] 中选择。最小代价是 2 ，下标为 0 。总代价为 2 + 2 = 4 。
+总雇佣代价是 4 。
+
+
+提示：
+
+1 <= costs.length <= 105
+1 <= costs[i] <= 105
+1 <= k, candidates <= costs.length
+*/
+
+#include <iostream>
+#include <queue>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        int size = costs.size(), left, right;
+        long long kCost = 0;
+        priority_queue<int, vector<int>, greater<int>> leftCosts, rightCosts;
+
+        for (left = 0; left < candidates; ++left) {
+            leftCosts.push(costs[left]);
+        }
+
+        --left;
+        for (right = size - 1; right > left && size - right <= candidates; --right) {
+            rightCosts.push(costs[right]);
+        }
+
+        ++right;
+        for (int i = 0; i < k; ++i) {
+            if (leftCosts.empty() || (!leftCosts.empty() && !rightCosts.empty() && rightCosts.top() < leftCosts.top())) {
+                kCost += rightCosts.top();
+                rightCosts.pop();
+                if (right - left > 1) {
+                    rightCosts.push(costs[--right]);
+                }
+            }
+            else {
+                kCost += leftCosts.top();
+                leftCosts.pop();
+                if (right - left > 1) {
+                    leftCosts.push(costs[++left]);
+                }
+            }
+        }
+
+        return kCost;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> costs = { 17,12,10,2,7,2,11,20,8 };
+    check.checkLongLong(11, o.totalCost(costs, 3, 4));
+
+    costs = { 1,2,4,1 };
+    check.checkLongLong(4, o.totalCost(costs, 3, 3));
+
+    costs = { 31,25,72,79,74,65,84,91,18,59,27,9,81,33,17,58 };
+    check.checkLongLong(423, o.totalCost(costs, 11, 2));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
