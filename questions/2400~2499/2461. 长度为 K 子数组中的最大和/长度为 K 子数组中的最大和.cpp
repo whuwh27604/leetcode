b4104b79182edbf3822bd85243f1b664d47ed72d
whuwh27604 <5991277@qq.com>
2022-11-12 15:08:54 +0800

    add

---
+++ questions/2400~2499/2461. 长度为 K 子数组中的最大和/长度为 K 子数组中的最大和.cpp
@@ -0,0 +1,93 @@
+﻿/* 长度为 K 子数组中的最大和.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数数组 nums 和一个整数 k 。请你从 nums 中满足下述条件的全部子数组中找出最大子数组和：
+
+子数组的长度是 k，且
+子数组中的所有元素 各不相同 。
+返回满足题面要求的最大子数组和。如果不存在子数组满足这些条件，返回 0 。
+
+子数组 是数组中一段连续非空的元素序列。
+
+
+
+示例 1：
+
+输入：nums = [1,5,4,2,9,9,9], k = 3
+输出：15
+解释：nums 中长度为 3 的子数组是：
+- [1,5,4] 满足全部条件，和为 10 。
+- [5,4,2] 满足全部条件，和为 11 。
+- [4,2,9] 满足全部条件，和为 15 。
+- [2,9,9] 不满足全部条件，因为元素 9 出现重复。
+- [9,9,9] 不满足全部条件，因为元素 9 出现重复。
+因为 15 是满足全部条件的所有子数组中的最大子数组和，所以返回 15 。
+示例 2：
+
+输入：nums = [4,4,4], k = 3
+输出：0
+解释：nums 中长度为 3 的子数组是：
+- [4,4,4] 不满足全部条件，因为元素 4 出现重复。
+因为不存在满足全部条件的子数组，所以返回 0 。
+
+
+提示：
+
+1 <= k <= nums.length <= 105
+1 <= nums[i] <= 105
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        int left = 0, right, size = nums.size();
+        long long sum = 0, maxSum = 0;
+        unordered_map<int, int> numCounts;
+
+        for (right = 0; right < size; ++right) {
+            if (right >= k) {
+                if (--numCounts[nums[left]] == 0) {
+                    numCounts.erase(nums[left]);
+                }
+                sum -= nums[left++];
+            }
+
+            ++numCounts[nums[right]];
+            sum += nums[right];
+
+            if (numCounts.size() == k) {
+                maxSum = max(maxSum, sum);
+            }
+        }
+
+        return maxSum;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,5,4,2,9,9,9 };
+    check.checkLongLong(15, o.maximumSubarraySum(nums, 3));
+
+    nums = { 4,4,4 };
+    check.checkLongLong(0, o.maximumSubarraySum(nums, 3));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
