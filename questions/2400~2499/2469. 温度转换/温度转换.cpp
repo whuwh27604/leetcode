8c3c3541d651c59e314f2e1b885110f76017aa41
whuwh27604 <5991277@qq.com>
2022-11-19 11:52:05 +0800

    add

---
+++ questions/2400~2499/2469. 温度转换/温度转换.cpp
@@ -0,0 +1,66 @@
+﻿/* 温度转换.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个四舍五入到两位小数的非负浮点数 celsius 来表示温度，以 摄氏度（Celsius）为单位。
+
+你需要将摄氏度转换为 开氏度（Kelvin）和 华氏度（Fahrenheit），并以数组 ans = [kelvin, fahrenheit] 的形式返回结果。
+
+返回数组 ans 。与实际答案误差不超过 10-5 的会视为正确答案。
+
+注意：
+
+开氏度 = 摄氏度 + 273.15
+华氏度 = 摄氏度 * 1.80 + 32.00
+
+
+示例 1 ：
+
+输入：celsius = 36.50
+输出：[309.65000,97.70000]
+解释：36.50 摄氏度：转换为开氏度是 309.65 ，转换为华氏度是 97.70 。
+示例 2 ：
+
+输入：celsius = 122.11
+输出：[395.26000,251.79800]
+解释：122.11 摄氏度：转换为开氏度是 395.26 ，转换为华氏度是 251.798 。
+
+
+提示：
+
+0 <= celsius <= 1000
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        return { celsius + 273.15, celsius * 1.8 + 32 };
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<double> actual = o.convertTemperature(36.5);
+    vector<double> expected = { 309.65000,97.70000 };
+    check.checkDoubleVector(expected, actual);
+
+    actual = o.convertTemperature(122.11);
+    expected = { 395.26000,251.79800 };
+    check.checkDoubleVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
