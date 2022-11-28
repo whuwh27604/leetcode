a6813b8ba8bfc68a6c5106f4fd0d406ed74a3205
whuwh27604 <5991277@qq.com>
2022-11-28 19:19:39 +0800

    add

---
+++ questions/2400~2499/2486. 追加字符以获得子序列/追加字符以获得子序列.cpp
@@ -0,0 +1,78 @@
+﻿/* 追加字符以获得子序列.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你两个仅由小写英文字母组成的字符串 s 和 t 。
+
+现在需要通过向 s 末尾追加字符的方式使 t 变成 s 的一个 子序列 ，返回需要追加的最少字符数。
+
+子序列是一个可以由其他字符串删除部分（或不删除）字符但不改变剩下字符顺序得到的字符串。
+
+
+
+示例 1：
+
+输入：s = "coaching", t = "coding"
+输出：4
+解释：向 s 末尾追加字符串 "ding" ，s = "coachingding" 。
+现在，t 是 s ("coachingding") 的一个子序列。
+可以证明向 s 末尾追加任何 3 个字符都无法使 t 成为 s 的一个子序列。
+示例 2：
+
+输入：s = "abcde", t = "a"
+输出：0
+解释：t 已经是 s ("abcde") 的一个子序列。
+示例 3：
+
+输入：s = "z", t = "abcde"
+输出：5
+解释：向 s 末尾追加字符串 "abcde" ，s = "zabcde" 。
+现在，t 是 s ("zabcde") 的一个子序列。
+可以证明向 s 末尾追加任何 4 个字符都无法使 t 成为 s 的一个子序列。
+
+
+提示：
+
+1 <= s.length, t.length <= 105
+s 和 t 仅由小写英文字母组成
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int i = 0, j = 0, size1 = s.size(), size2 = t.size();
+
+        for (i = 0; i < size1; ++i) {
+            if (s[i] == t[j]) {
+                if (++j == size2) {
+                    break;
+                }
+            }
+        }
+
+        return size2 - j;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkInt(4, o.appendCharacters("coaching", "coding"));
+    check.checkInt(0, o.appendCharacters("abcde", "a"));
+    check.checkInt(5, o.appendCharacters("z", "abcde"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
