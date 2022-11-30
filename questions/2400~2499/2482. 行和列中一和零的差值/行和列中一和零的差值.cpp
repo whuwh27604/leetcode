f166f5dbcf9ba96f29152bd8fde743b2dfebf580
whuwh27604 <5991277@qq.com>
2022-11-30 19:26:31 +0800

    add

---
+++ questions/2400~2499/2482. 行和列中一和零的差值/行和列中一和零的差值.cpp
@@ -0,0 +1,115 @@
+﻿/* 行和列中一和零的差值.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的 m x n 二进制矩阵 grid 。
+
+我们按照如下过程，定义一个下标从 0 开始的 m x n 差值矩阵 diff ：
+
+令第 i 行一的数目为 onesRowi 。
+令第 j 列一的数目为 onesColj 。
+令第 i 行零的数目为 zerosRowi 。
+令第 j 列零的数目为 zerosColj 。
+diff[i][j] = onesRowi + onesColj - zerosRowi - zerosColj
+请你返回差值矩阵 diff 。
+
+
+
+示例 1：
+
+
+
+输入：grid = [[0,1,1],[1,0,1],[0,0,1]]
+输出：[[0,0,4],[0,0,4],[-2,-2,2]]
+解释：
+- diff[0][0] = onesRow0 + onesCol0 - zerosRow0 - zerosCol0 = 2 + 1 - 1 - 2 = 0
+- diff[0][1] = onesRow0 + onesCol1 - zerosRow0 - zerosCol1 = 2 + 1 - 1 - 2 = 0
+- diff[0][2] = onesRow0 + onesCol2 - zerosRow0 - zerosCol2 = 2 + 3 - 1 - 0 = 4
+- diff[1][0] = onesRow1 + onesCol0 - zerosRow1 - zerosCol0 = 2 + 1 - 1 - 2 = 0
+- diff[1][1] = onesRow1 + onesCol1 - zerosRow1 - zerosCol1 = 2 + 1 - 1 - 2 = 0
+- diff[1][2] = onesRow1 + onesCol2 - zerosRow1 - zerosCol2 = 2 + 3 - 1 - 0 = 4
+- diff[2][0] = onesRow2 + onesCol0 - zerosRow2 - zerosCol0 = 1 + 1 - 2 - 2 = -2
+- diff[2][1] = onesRow2 + onesCol1 - zerosRow2 - zerosCol1 = 1 + 1 - 2 - 2 = -2
+- diff[2][2] = onesRow2 + onesCol2 - zerosRow2 - zerosCol2 = 1 + 3 - 2 - 0 = 2
+示例 2：
+
+
+
+输入：grid = [[1,1,1],[1,1,1]]
+输出：[[5,5,5],[5,5,5]]
+解释：
+- diff[0][0] = onesRow0 + onesCol0 - zerosRow0 - zerosCol0 = 3 + 2 - 0 - 0 = 5
+- diff[0][1] = onesRow0 + onesCol1 - zerosRow0 - zerosCol1 = 3 + 2 - 0 - 0 = 5
+- diff[0][2] = onesRow0 + onesCol2 - zerosRow0 - zerosCol2 = 3 + 2 - 0 - 0 = 5
+- diff[1][0] = onesRow1 + onesCol0 - zerosRow1 - zerosCol0 = 3 + 2 - 0 - 0 = 5
+- diff[1][1] = onesRow1 + onesCol1 - zerosRow1 - zerosCol1 = 3 + 2 - 0 - 0 = 5
+- diff[1][2] = onesRow1 + onesCol2 - zerosRow1 - zerosCol2 = 3 + 2 - 0 - 0 = 5
+
+
+提示：
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 105
+1 <= m * n <= 105
+grid[i][j] 要么是 0 ，要么是 1 。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
+        int row = grid.size(), column = grid[0].size();
+        vector<int> onesRows(row, 0), zerosRows(row, 0), onesColumns(column, 0), zerosColumns(column, 0);
+        vector<vector<int>> diffs(row, vector<int>(column));
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < column; ++j) {
+                if (grid[i][j] == 0) {
+                    ++zerosRows[i];
+                    ++zerosColumns[j];
+                }
+                else {
+                    ++onesRows[i];
+                    ++onesColumns[j];
+                }
+            }
+        }
+        
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < column; ++j) {
+                diffs[i][j] = onesRows[i] + onesColumns[j] - zerosRows[i] - zerosColumns[j];
+            }
+        }
+
+        return diffs;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> grid = { {0,1,1},{1,0,1},{0,0,1} };
+    vector<vector<int>> actual = o.onesMinusZeros(grid);
+    vector<vector<int>> expected = { {0,0,4},{0,0,4},{-2,-2,2} };
+    check.checkIntVectorVector(expected, actual);
+
+    grid = { {1,1,1},{1,1,1} };
+    actual = o.onesMinusZeros(grid);
+    expected = { {5,5,5},{5,5,5} };
+    check.checkIntVectorVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
