618f6b38dbd64e360160cdc55b152d0a73718868
whuwh27604 <5991277@qq.com>
2022-12-20 20:02:13 +0800

    add

---
+++ questions/2400~2499/2498. 青蛙过河 II/青蛙过河 II.cpp
@@ -0,0 +1,88 @@
+﻿/* 青蛙过河 II.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 stones ，数组中的元素 严格递增 ，表示一条河中石头的位置。
+
+一只青蛙一开始在第一块石头上，它想到达最后一块石头，然后回到第一块石头。同时每块石头 至多 到达 一次。
+
+一次跳跃的 长度 是青蛙跳跃前和跳跃后所在两块石头之间的距离。
+
+更正式的，如果青蛙从 stones[i] 跳到 stones[j] ，跳跃的长度为 |stones[i] - stones[j]| 。
+一条路径的 代价 是这条路径里的 最大跳跃长度 。
+
+请你返回这只青蛙的 最小代价 。
+
+
+
+示例 1：
+
+
+
+输入：stones = [0,2,5,6,7]
+输出：5
+解释：上图展示了一条最优路径。
+这条路径的代价是 5 ，是这条路径中的最大跳跃长度。
+无法得到一条代价小于 5 的路径，我们返回 5 。
+示例 2：
+
+
+
+输入：stones = [0,3,9]
+输出：9
+解释：
+青蛙可以直接跳到最后一块石头，然后跳回第一块石头。
+在这条路径中，每次跳跃长度都是 9 。所以路径代价是 max(9, 9) = 9 。
+这是可行路径中的最小代价。
+
+
+提示：
+
+2 <= stones.length <= 105
+0 <= stones[i] <= 109
+stones[0] == 0
+stones 中的元素严格递增。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxJump(vector<int>& stones) {
+        int i = 0, size = stones.size(), jump = max(stones[1] - stones[0], stones[size - 1] - stones[size - 2]);
+
+        for (i = 2; i < size; i += 2) {
+            jump = max(jump, stones[i] - stones[i - 2]);
+        }
+
+        for (i = 3; i < size; i += 2) {
+            jump = max(jump, stones[i] - stones[i - 2]);
+        }
+
+        return jump;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> stones = { 0,2,5,6,7 };
+    check.checkInt(5, o.maxJump(stones));
+
+    stones = { 0,3,9 };
+    check.checkInt(9, o.maxJump(stones));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
