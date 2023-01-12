b4126491d76b03660cb6b9066b4fe21d6e3d85a6
whuwh27604 <5991277@qq.com>
2023-01-12 18:36:02 +0800

    add

---
+++ questions/2500~2599/2530. 执行 K 次操作后的最大分数/执行 K 次操作后的最大分数.cpp
@@ -0,0 +1,85 @@
+﻿/* 执行 K 次操作后的最大分数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums 和一个整数 k 。你的 起始分数 为 0 。
+
+在一步 操作 中：
+
+选出一个满足 0 <= i < nums.length 的下标 i ，
+将你的 分数 增加 nums[i] ，并且
+将 nums[i] 替换为 ceil(nums[i] / 3) 。
+返回在 恰好 执行 k 次操作后，你可能获得的最大分数。
+
+向上取整函数 ceil(val) 的结果是大于或等于 val 的最小整数。
+
+
+
+示例 1：
+
+输入：nums = [10,10,10,10,10], k = 5
+输出：50
+解释：对数组中每个元素执行一次操作。最后分数是 10 + 10 + 10 + 10 + 10 = 50 。
+示例 2：
+
+输入：nums = [1,10,3,3,3], k = 3
+输出：17
+解释：可以执行下述操作：
+第 1 步操作：选中 i = 1 ，nums 变为 [1,4,3,3,3] 。分数增加 10 。
+第 2 步操作：选中 i = 1 ，nums 变为 [1,2,3,3,3] 。分数增加 4 。
+第 3 步操作：选中 i = 2 ，nums 变为 [1,1,1,3,3] 。分数增加 3 。
+最后分数是 10 + 4 + 3 = 17 。
+
+
+提示：
+
+1 <= nums.length, k <= 105
+1 <= nums[i] <= 109
+*/
+
+#include <iostream>
+#include <queue>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> q;
+        long long sum = 0;
+
+        for (int num : nums) {
+            q.push(num);
+        }
+
+        for (int i = 0; i < k; ++i) {
+            int num = q.top();
+            q.pop();
+            sum += num;
+            q.push((num + 2) / 3);
+        }
+
+        return sum;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 10,10,10,10,10 };
+    check.checkLongLong(50, o.maxKelements(nums, 5));
+
+    nums = { 1,10,3,3,3 };
+    check.checkLongLong(17, o.maxKelements(nums, 3));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
