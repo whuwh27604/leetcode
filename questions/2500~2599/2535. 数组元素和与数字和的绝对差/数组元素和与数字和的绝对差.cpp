ede2d43056bea2e2883aa70bced92992c816d5ce
whuwh27604 <5991277@qq.com>
2023-01-19 17:43:09 +0800

    add

---
+++ questions/2500~2599/2535. 数组元素和与数字和的绝对差/数组元素和与数字和的绝对差.cpp
@@ -0,0 +1,80 @@
+﻿/* 数组元素和与数字和的绝对差.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个正整数数组 nums 。
+
+元素和 是 nums 中的所有元素相加求和。
+数字和 是 nums 中每一个元素的每一数位（重复数位需多次求和）相加求和。
+返回 元素和 与 数字和 的绝对差。
+
+注意：两个整数 x 和 y 的绝对差定义为 |x - y| 。
+
+
+
+示例 1：
+
+输入：nums = [1,15,6,3]
+输出：9
+解释：
+nums 的元素和是 1 + 15 + 6 + 3 = 25 。
+nums 的数字和是 1 + 1 + 5 + 6 + 3 = 16 。
+元素和与数字和的绝对差是 |25 - 16| = 9 。
+示例 2：
+
+输入：nums = [1,2,3,4]
+输出：0
+解释：
+nums 的元素和是 1 + 2 + 3 + 4 = 10 。
+nums 的数字和是 1 + 2 + 3 + 4 = 10 。
+元素和与数字和的绝对差是 |10 - 10| = 0 。
+
+
+提示：
+
+1 <= nums.length <= 2000
+1 <= nums[i] <= 2000
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int differenceOfSum(vector<int>& nums) {
+        int sum = 0, digitSum = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            while (num != 0) {
+                digitSum += (num % 10);
+                num /= 10;
+            }
+        }
+
+        return abs(sum - digitSum);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,15,6,3 };
+    check.checkInt(9, o.differenceOfSum(nums));
+
+    nums = { 1,2,3,4 };
+    check.checkInt(0, o.differenceOfSum(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
