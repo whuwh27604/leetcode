b57538327b92c2752969b0480d9d55332298e15e
whuwh27604 <5991277@qq.com>
2023-01-27 12:15:46 +0800

    add

---
+++ questions/2500~2599/2545. 根据第 K 场考试的分数排序/根据第 K 场考试的分数排序.cpp
@@ -0,0 +1,91 @@
+﻿/* 根据第 K 场考试的分数排序.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+班里有 m 位学生，共计划组织 n 场考试。给你一个下标从 0 开始、大小为 m x n 的整数矩阵 score ，其中每一行对应一位学生，而 score[i][j] 表示第 i 位学生在第 j 场考试取得的分数。矩阵 score 包含的整数 互不相同 。
+
+另给你一个整数 k 。请你按第 k 场考试分数从高到低完成对这些学生（矩阵中的行）的排序。
+
+返回排序后的矩阵。
+
+
+
+示例 1：
+
+
+
+输入：score = [[10,6,9,1],[7,5,11,2],[4,8,3,15]], k = 2
+输出：[[7,5,11,2],[10,6,9,1],[4,8,3,15]]
+解释：在上图中，S 表示学生，E 表示考试。
+- 下标为 1 的学生在第 2 场考试取得的分数为 11 ，这是考试的最高分，所以 TA 需要排在第一。
+- 下标为 0 的学生在第 2 场考试取得的分数为 9 ，这是考试的第二高分，所以 TA 需要排在第二。
+- 下标为 2 的学生在第 2 场考试取得的分数为 3 ，这是考试的最低分，所以 TA 需要排在第三。
+示例 2：
+
+
+
+输入：score = [[3,4],[5,6]], k = 0
+输出：[[5,6],[3,4]]
+解释：在上图中，S 表示学生，E 表示考试。
+- 下标为 1 的学生在第 0 场考试取得的分数为 5 ，这是考试的最高分，所以 TA 需要排在第一。
+- 下标为 0 的学生在第 0 场考试取得的分数为 3 ，这是考试的最低分，所以 TA 需要排在第二。
+
+
+提示：
+
+m == score.length
+n == score[i].length
+1 <= m, n <= 250
+1 <= score[i][j] <= 105
+score 由 不同 的整数组成
+0 <= k < n
+*/
+
+#include <iostream>
+#include <map>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        int size = score.size();
+        vector<vector<int>> ans(size);
+        map<int, vector<int>*> scores;
+
+        for (auto& s : score) {
+            scores[s[k]] = &s;
+        }
+
+        for (auto& kv : scores) {
+            ans[--size] = *kv.second;
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> score = { {10,6,9,1},{7,5,11,2},{4,8,3,15} };
+    vector<vector<int>> actual = o.sortTheStudents(score, 2);
+    vector<vector<int>> expected = { {7,5,11,2},{10,6,9,1},{4,8,3,15} };
+    check.checkIntVectorVector(expected, actual);
+
+    score = { {3,4},{5,6} };
+    actual = o.sortTheStudents(score, 0);
+    expected = { {5,6},{3,4} };
+    check.checkIntVectorVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
