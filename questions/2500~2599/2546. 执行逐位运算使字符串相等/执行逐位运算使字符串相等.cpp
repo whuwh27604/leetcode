f502e33bf4add49409088565f503549abac09395
whuwh27604 <5991277@qq.com>
2023-01-27 12:12:05 +0800

    add

---
+++ questions/2500~2599/2546. 执行逐位运算使字符串相等/执行逐位运算使字符串相等.cpp
@@ -0,0 +1,74 @@
+﻿/* 执行逐位运算使字符串相等.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你两个下标从 0 开始的 二元 字符串 s 和 target ，两个字符串的长度均为 n 。你可以对 s 执行下述操作 任意 次：
+
+选择两个 不同 的下标 i 和 j ，其中 0 <= i, j < n 。
+同时，将 s[i] 替换为 (s[i] OR s[j]) ，s[j] 替换为 (s[i] XOR s[j]) 。
+例如，如果 s = "0110" ，你可以选择 i = 0 和 j = 2，然后同时将 s[0] 替换为 (s[0] OR s[2] = 0 OR 1 = 1)，并将 s[2] 替换为 (s[0] XOR s[2] = 0 XOR 1 = 1)，最终得到 s = "1110" 。
+
+如果可以使 s 等于 target ，返回 true ，否则，返回 false 。
+
+
+
+示例 1：
+
+输入：s = "1010", target = "0110"
+输出：true
+解释：可以执行下述操作：
+- 选择 i = 2 和 j = 0 ，得到 s = "0010".
+- 选择 i = 2 和 j = 1 ，得到 s = "0110".
+可以使 s 等于 target ，返回 true 。
+示例 2：
+
+输入：s = "11", target = "00"
+输出：false
+解释：执行任意次操作都无法使 s 等于 target 。
+
+
+提示：
+
+n == s.length == target.length
+2 <= n <= 105
+s 和 target 仅由数字 0 和 1 组成
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool makeStringsEqual(string s, string target) {
+        return (countOne(s) ^ countOne(target)) == 0;
+    }
+
+    int countOne(string& s) {
+        for (char c : s) {
+            if (c == '1') {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkBool(true, o.makeStringsEqual("1010", "0110"));
+    check.checkBool(false, o.makeStringsEqual("11", "00"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
