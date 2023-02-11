49da7bdf1190ca7be811bc293fbfab88df771969
whuwh27604 <5991277@qq.com>
2023-02-11 14:41:55 +0800

    add

---
+++ questions/2500~2599/2560. 打家劫舍 IV/打家劫舍 IV.cpp
@@ -0,0 +1,102 @@
+﻿/* 打家劫舍 IV.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+沿街有一排连续的房屋。每间房屋内都藏有一定的现金。现在有一位小偷计划从这些房屋中窃取现金。
+
+由于相邻的房屋装有相互连通的防盗系统，所以小偷 不会窃取相邻的房屋 。
+
+小偷的 窃取能力 定义为他在窃取过程中能从单间房屋中窃取的 最大金额 。
+
+给你一个整数数组 nums 表示每间房屋存放的现金金额。形式上，从左起第 i 间房屋中放有 nums[i] 美元。
+
+另给你一个整数 k ，表示窃贼将会窃取的 最少 房屋数。小偷总能窃取至少 k 间房屋。
+
+返回小偷的 最小 窃取能力。
+
+
+
+示例 1：
+
+输入：nums = [2,3,5,9], k = 2
+输出：5
+解释：
+小偷窃取至少 2 间房屋，共有 3 种方式：
+- 窃取下标 0 和 2 处的房屋，窃取能力为 max(nums[0], nums[2]) = 5 。
+- 窃取下标 0 和 3 处的房屋，窃取能力为 max(nums[0], nums[3]) = 9 。
+- 窃取下标 1 和 3 处的房屋，窃取能力为 max(nums[1], nums[3]) = 9 。
+因此，返回 min(5, 9, 9) = 5 。
+示例 2：
+
+输入：nums = [2,7,9,3,1], k = 2
+输出：2
+解释：共有 7 种窃取方式。窃取能力最小的情况所对应的方式是窃取下标 0 和 4 处的房屋。返回 max(nums[0], nums[4]) = 2 。
+
+
+提示：
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+1 <= k <= (nums.length + 1)/2
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int minCapability(vector<int>& nums, int k) {
+        int low = *min_element(nums.begin(), nums.end()), high = *max_element(nums.begin(), nums.end()), minCap = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (check(nums, k, mid)) {
+                minCap = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return minCap;
+    }
+
+    bool check(vector<int>& nums, int k, int capability) {
+        int size = nums.size(), count = 0;
+
+        for (int i = 0; i < size; ++i) {
+            if (nums[i] <= capability) {  // 贪心，如果小于capability，打劫当前房屋
+                if (++count == k) {
+                    return true;
+                }
+                ++i;  // 打劫以后，跳过下一间
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 2,3,5,9 };
+    check.checkInt(5, o.minCapability(nums, 2));
+
+    nums = { 2,7,9,3,1 };
+    check.checkInt(2, o.minCapability(nums, 2));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
