315c6fd213a933533775047b149afa09fba59b9e
whuwh27604 <5991277@qq.com>
2023-03-08 21:30:07 +0800

    add

---
+++ questions/2500~2599/2585. 获得分数的方法数/获得分数的方法数.cpp
@@ -0,0 +1,103 @@
+﻿/* 获得分数的方法数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+考试中有 n 种类型的题目。给你一个整数 target 和一个下标从 0 开始的二维整数数组 types ，其中 types[i] = [counti, marksi] 表示第 i 种类型的题目有 counti 道，每道题目对应 marksi 分。
+
+返回你在考试中恰好得到 target 分的方法数。由于答案可能很大，结果需要对 109 +7 取余。
+
+注意，同类型题目无法区分。
+
+比如说，如果有 3 道同类型题目，那么解答第 1 和第 2 道题目与解答第 1 和第 3 道题目或者第 2 和第 3 道题目是相同的。
+
+
+示例 1：
+
+输入：target = 6, types = [[6,1],[3,2],[2,3]]
+输出：7
+解释：要获得 6 分，你可以选择以下七种方法之一：
+- 解决 6 道第 0 种类型的题目：1 + 1 + 1 + 1 + 1 + 1 = 6
+- 解决 4 道第 0 种类型的题目和 1 道第 1 种类型的题目：1 + 1 + 1 + 1 + 2 = 6
+- 解决 2 道第 0 种类型的题目和 2 道第 1 种类型的题目：1 + 1 + 2 + 2 = 6
+- 解决 3 道第 0 种类型的题目和 1 道第 2 种类型的题目：1 + 1 + 1 + 3 = 6
+- 解决 1 道第 0 种类型的题目、1 道第 1 种类型的题目和 1 道第 2 种类型的题目：1 + 2 + 3 = 6
+- 解决 3 道第 1 种类型的题目：2 + 2 + 2 = 6
+- 解决 2 道第 2 种类型的题目：3 + 3 = 6
+示例 2：
+
+输入：target = 5, types = [[50,1],[50,2],[50,5]]
+输出：4
+解释：要获得 5 分，你可以选择以下四种方法之一：
+- 解决 5 道第 0 种类型的题目：1 + 1 + 1 + 1 + 1 = 5
+- 解决 3 道第 0 种类型的题目和 1 道第 1 种类型的题目：1 + 1 + 1 + 2 = 5
+- 解决 1 道第 0 种类型的题目和 2 道第 1 种类型的题目：1 + 2 + 2 = 5
+- 解决 1 道第 2 种类型的题目：5
+示例 3：
+
+输入：target = 18, types = [[6,1],[3,2],[2,3]]
+输出：1
+解释：只有回答所有题目才能获得 18 分。
+
+
+提示：
+
+1 <= target <= 1000
+n == types.length
+1 <= n <= 50
+types[i].length == 2
+1 <= counti, marksi <= 50
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int waysToReachTarget(int target, vector<vector<int>>& types) {
+        int mod = 1000000007;
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+
+        for (auto& type : types) {
+            int count = type[0], mark = type[1];
+            vector<int> tmp = dp;
+
+            for (int i = count; i > 0; --i) {
+                int score = mark * i;
+
+                for (int j = target; j >= score; --j) {
+                    tmp[j] = (tmp[j] + dp[j - score]) % mod;
+                }
+            }
+
+            dp = tmp;
+        }
+
+        return dp[target];
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> types = { {6,1},{3,2},{2,3} };
+    check.checkInt(7, o.waysToReachTarget(6, types));
+
+    types = { {5,1},{5,2},{5,5} };
+    check.checkInt(4, o.waysToReachTarget(5, types));
+
+    types = { {6,1},{3,2},{2,3} };
+    check.checkInt(1, o.waysToReachTarget(18, types));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
