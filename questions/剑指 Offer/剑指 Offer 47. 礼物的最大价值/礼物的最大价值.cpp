abd6738e4fd38aa99356ce74eaa91be593574b24
whuwh27604 <5991277@qq.com>
2023-03-08 21:16:05 +0800

    add

---
+++ questions/剑指 Offer/剑指 Offer 47. 礼物的最大价值/礼物的最大价值.cpp
@@ -0,0 +1,70 @@
+﻿/* 礼物的最大价值.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+
+
+
+示例 1:
+
+输入:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+输出: 12
+解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物
+
+
+提示：
+
+0 < grid.length <= 200
+0 < grid[0].length <= 200
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxValue(vector<vector<int>>& grid) {
+        int row = grid.size(), column = grid[0].size();
+        vector<vector<int>> dp(row, vector<int>(column, 0));
+        dp[0][0] = grid[0][0];
+
+        for (int r = 0; r < row; ++r) {
+            for (int c = 0; c < column; ++c) {
+                if (r != 0) {
+                    dp[r][c] = max(dp[r][c], dp[r - 1][c] + grid[r][c]);
+                }
+                if (c != 0) {
+                    dp[r][c] = max(dp[r][c], dp[r][c - 1] + grid[r][c]);
+                }
+            }
+        }
+
+        return dp[row - 1][column - 1];
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<vector<int>> grid = { {1,3,1},{1,5,1},{4,2,1} };
+    check.checkInt(12, o.maxValue(grid));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
