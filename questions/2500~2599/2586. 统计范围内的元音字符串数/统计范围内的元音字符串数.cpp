308c4948bd9fc32b4a258796cc33c273e955a1b9
whuwh27604 <5991277@qq.com>
2023-03-14 00:06:08 +0800

    add

---
+++ questions/2500~2599/2586. 统计范围内的元音字符串数/统计范围内的元音字符串数.cpp
@@ -0,0 +1,84 @@
+﻿/* 统计范围内的元音字符串数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的字符串数组 words 和两个整数：left 和 right 。
+
+如果字符串以元音字母开头并以元音字母结尾，那么该字符串就是一个 元音字符串 ，其中元音字母是 'a'、'e'、'i'、'o'、'u' 。
+
+返回 words[i] 是元音字符串的数目，其中 i 在闭区间 [left, right] 内。
+
+
+
+示例 1：
+
+输入：words = ["are","amy","u"], left = 0, right = 2
+输出：2
+解释：
+- "are" 是一个元音字符串，因为它以 'a' 开头并以 'e' 结尾。
+- "amy" 不是元音字符串，因为它没有以元音字母结尾。
+- "u" 是一个元音字符串，因为它以 'u' 开头并以 'u' 结尾。
+在上述范围中的元音字符串数目为 2 。
+示例 2：
+
+输入：words = ["hey","aeo","mu","ooo","artro"], left = 1, right = 4
+输出：3
+解释：
+- "aeo" 是一个元音字符串，因为它以 'a' 开头并以 'o' 结尾。
+- "mu" 不是元音字符串，因为它没有以元音字母开头。
+- "ooo" 是一个元音字符串，因为它以 'o' 开头并以 'o' 结尾。
+- "artro" 是一个元音字符串，因为它以 'a' 开头并以 'o' 结尾。
+在上述范围中的元音字符串数目为 3 。
+
+
+提示：
+
+1 <= words.length <= 1000
+1 <= words[i].length <= 10
+words[i] 仅由小写英文字母组成
+0 <= left <= right < words.length
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int vowelStrings(vector<string>& words, int left, int right) {
+        int vowels = 0;
+
+        for (int i = left; i <= right; ++i) {
+            if (isVowel(words[i][0]) && isVowel(words[i].back())) {
+                ++vowels;
+            }
+        }
+
+        return vowels;
+    }
+
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<string> words = { "are","amy","u" };
+    check.checkInt(2, o.vowelStrings(words, 0, 2));
+
+    words = { "hey","aeo","mu","ooo","artro" };
+    check.checkInt(3, o.vowelStrings(words, 1, 4));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
