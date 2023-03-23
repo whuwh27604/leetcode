bacfbb2abbffe4abab443e51691f3e7652e7f646
whuwh27604 <5991277@qq.com>
2023-03-24 00:25:49 +0800

    add

---
+++ questions/2500~2599/2592. 最大化数组的伟大值/最大化数组的伟大值.cpp
@@ -0,0 +1,72 @@
+﻿/* 最大化数组的伟大值.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums 。你需要将 nums 重新排列成一个新的数组 perm 。
+
+定义 nums 的 伟大值 为满足 0 <= i < nums.length 且 perm[i] > nums[i] 的下标数目。
+
+请你返回重新排列 nums 后的 最大 伟大值。
+
+
+
+示例 1：
+
+输入：nums = [1,3,5,2,1,3,1]
+输出：4
+解释：一个最优安排方案为 perm = [2,5,1,3,3,1,1] 。
+在下标为 0, 1, 3 和 4 处，都有 perm[i] > nums[i] 。因此我们返回 4 。
+示例 2：
+
+输入：nums = [1,2,3,4]
+输出：3
+解释：最优排列为 [2,3,4,1] 。
+在下标为 0, 1 和 2 处，都有 perm[i] > nums[i] 。因此我们返回 3 。
+
+
+提示：
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 109
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maximizeGreatness(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int i = 0, j = 0, size = nums.size();
+
+        for (; j < size; ++i) {
+            for (++j; j < size && nums[j] == nums[i]; ++j) {}
+        }
+
+        return i - 1;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,3,5,2,1,3,1 };
+    check.checkInt(4, o.maximizeGreatness(nums));
+
+    nums = { 1,2,3,4 };
+    check.checkInt(3, o.maximizeGreatness(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
