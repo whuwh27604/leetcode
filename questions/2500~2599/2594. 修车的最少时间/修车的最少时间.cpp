f04fe235ef728757c3f9f9687100fba75a77dc7a
whuwh27604 <5991277@qq.com>
2023-03-25 17:12:53 +0800

    add

---
+++ questions/2500~2599/2594. 修车的最少时间/修车的最少时间.cpp
@@ -0,0 +1,99 @@
+﻿/* 修车的最少时间.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数数组 ranks ，表示一些机械工的 能力值 。ranksi 是第 i 位机械工的能力值。能力值为 r 的机械工可以在 r * n2 分钟内修好 n 辆车。
+
+同时给你一个整数 cars ，表示总共需要修理的汽车数目。
+
+请你返回修理所有汽车 最少 需要多少时间。
+
+注意：所有机械工可以同时修理汽车。
+
+
+
+示例 1：
+
+输入：ranks = [4,2,3,1], cars = 10
+输出：16
+解释：
+- 第一位机械工修 2 辆车，需要 4 * 2 * 2 = 16 分钟。
+- 第二位机械工修 2 辆车，需要 2 * 2 * 2 = 8 分钟。
+- 第三位机械工修 2 辆车，需要 3 * 2 * 2 = 12 分钟。
+- 第四位机械工修 4 辆车，需要 1 * 4 * 4 = 16 分钟。
+16 分钟是修理完所有车需要的最少时间。
+示例 2：
+
+输入：ranks = [5,1,8], cars = 6
+输出：16
+解释：
+- 第一位机械工修 1 辆车，需要 5 * 1 * 1 = 5 分钟。
+- 第二位机械工修 4 辆车，需要 1 * 4 * 4 = 16 分钟。
+- 第三位机械工修 1 辆车，需要 8 * 1 * 1 = 8 分钟。
+16 分钟时修理完所有车需要的最少时间。
+
+
+提示：
+
+1 <= ranks.length <= 105
+1 <= ranks[i] <= 100
+1 <= cars <= 106
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+        long long low = 1, high = (long long)100 * cars * cars, ans = high;
+
+        while (low <= high) {
+            long long mid = (low + high) / 2;
+            if (check(ranks, cars, mid)) {
+                high = mid - 1;
+                ans = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    bool check(vector<int>& ranks, int cars, long long time) {
+        long long repaired = 0;
+
+        for (int rank : ranks) {
+            repaired += (int)sqrt(time / rank);
+            if (repaired >= cars) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> ranks = { 4,2,3,1 };
+    check.checkLongLong(16, o.repairCars(ranks, 10));
+
+    ranks = { 5,1,8 };
+    check.checkLongLong(16, o.repairCars(ranks, 6));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
