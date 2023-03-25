4e8e51f2dc6daa0401cdb37523974b9fb2e3a05f
whuwh27604 <5991277@qq.com>
2023-03-25 17:30:20 +0800

    add

---
+++ questions/2500~2599/2598. 执行操作后的最大 MEX/执行操作后的最大 MEX.cpp
@@ -0,0 +1,95 @@
+﻿/* 执行操作后的最大 MEX.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums 和一个整数 value 。
+
+在一步操作中，你可以对 nums 中的任一元素加上或减去 value 。
+
+例如，如果 nums = [1,2,3] 且 value = 2 ，你可以选择 nums[0] 减去 value ，得到 nums = [-1,2,3] 。
+数组的 MEX (minimum excluded) 是指其中数组中缺失的最小非负整数。
+
+例如，[-1,2,3] 的 MEX 是 0 ，而 [1,0,3] 的 MEX 是 2 。
+返回在执行上述操作 任意次 后，nums 的最大 MEX 。
+
+
+
+示例 1：
+
+输入：nums = [1,-10,7,13,6,8], value = 5
+输出：4
+解释：执行下述操作可以得到这一结果：
+- nums[1] 加上 value 两次，nums = [1,0,7,13,6,8]
+- nums[2] 减去 value 一次，nums = [1,0,2,13,6,8]
+- nums[3] 减去 value 两次，nums = [1,0,2,3,6,8]
+nums 的 MEX 是 4 。可以证明 4 是可以取到的最大 MEX 。
+示例 2：
+
+输入：nums = [1,-10,7,13,6,8], value = 7
+输出：2
+解释：执行下述操作可以得到这一结果：
+- nums[2] 减去 value 一次，nums = [1,-10,0,13,6,8]
+nums 的 MEX 是 2 。可以证明 2 是可以取到的最大 MEX 。
+
+
+提示：
+
+1 <= nums.length, value <= 105
+-109 <= nums[i] <= 109
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int findSmallestInteger(vector<int>& nums, int value) {
+        vector<int> count(value, 0);
+        int smallest = INT_MAX;
+
+        for (int num : nums) {
+            if (num >= 0) {
+                ++count[num % value];
+            }
+            else {
+                int index = -num % value;
+                if (index != 0) {
+                    index = value - index;
+                }
+                ++count[index];
+            }
+        }
+
+        for (int i = 0; i < value; ++i) {
+            smallest = min(smallest, value * count[i] + i);
+        }
+
+        return smallest;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,-10,7,13,6,8 };
+    check.checkInt(4, o.findSmallestInteger(nums, 5));
+
+    nums = { 1,-10,7,13,6,8 };
+    check.checkInt(2, o.findSmallestInteger(nums, 7));
+
+    nums = { 3,0,3,2,4,2,1,1,0,4 };
+    check.checkInt(10, o.findSmallestInteger(nums, 5));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
