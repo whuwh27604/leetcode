717f499a6863175ed4c8d804b23fb16871229981
whuwh27604 <5991277@qq.com>
2023-04-05 16:45:21 +0800

    add

---
+++ questions/2600~2699/2607. 使子数组元素和相等/使子数组元素和相等.cpp
@@ -0,0 +1,107 @@
+﻿/* 使子数组元素和相等.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 arr 和一个整数 k 。数组 arr 是一个循环数组。换句话说，数组中的最后一个元素的下一个元素是数组中的第一个元素，数组中第一个元素的前一个元素是数组中的最后一个元素。
+
+你可以执行下述运算任意次：
+
+选中 arr 中任意一个元素，并使其值加上 1 或减去 1 。
+执行运算使每个长度为 k 的 子数组 的元素总和都相等，返回所需要的最少运算次数。
+
+子数组 是数组的一个连续部分。
+
+
+
+示例 1：
+
+输入：arr = [1,4,1,3], k = 2
+输出：1
+解释：在下标为 1 的元素那里执行一次运算，使其等于 3 。
+执行运算后，数组变为 [1,3,1,3] 。
+- 0 处起始的子数组为 [1, 3] ，元素总和为 4
+- 1 处起始的子数组为 [3, 1] ，元素总和为 4
+- 2 处起始的子数组为 [1, 3] ，元素总和为 4
+- 3 处起始的子数组为 [3, 1] ，元素总和为 4
+示例 2：
+
+输入：arr = [2,5,5,7], k = 3
+输出：5
+解释：在下标为 0 的元素那里执行三次运算，使其等于 5 。在下标为 3 的元素那里执行两次运算，使其等于 5 。
+执行运算后，数组变为 [5,5,5,5] 。
+- 0 处起始的子数组为 [5, 5, 5] ，元素总和为 15
+- 1 处起始的子数组为 [5, 5, 5] ，元素总和为 15
+- 2 处起始的子数组为 [5, 5, 5] ，元素总和为 15
+- 3 处起始的子数组为 [5, 5, 5] ，元素总和为 15
+
+
+提示：
+
+1 <= k <= arr.length <= 105
+1 <= arr[i] <= 109
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    long long makeSubKSumEqual(vector<int>& arr, int k) {
+        int size = arr.size(), len = gcd(size, k);
+        long long opers = 0;
+
+        for (int i = 0; i < len; ++i) {
+            vector<int> nums;
+
+            for (int j = i; j < size; j += len) {
+                nums.push_back(arr[j]);
+            }
+
+            opers += getOpers(nums);
+        }
+
+        return opers;
+    }
+
+    long long getOpers(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int median = nums[nums.size() / 2];
+        long long opers = 0;
+
+        for (int num : nums) {
+            opers += abs(num - median);
+        }
+
+        return opers;
+    }
+
+    int gcd(int x, int y) {
+        return y == 0 ? x : gcd(y, x % y);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> arr = { 1,4,1,3 };
+    check.checkLongLong(1, o.makeSubKSumEqual(arr, 2));
+
+    arr = { 2,5,5,7 };
+    check.checkLongLong(5, o.makeSubKSumEqual(arr, 3));
+
+    arr = { 6,2,8,5,7,10 };
+    check.checkLongLong(10, o.makeSubKSumEqual(arr, 4));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
