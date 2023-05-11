09b80b61368cc8c1e452969bb2009945f5b392f9
whuwh27604 <5991277@qq.com>
2023-05-12 00:24:17 +0800

    add

---
+++ questions/2600~2699/2670. 找出不同元素数目差数组/找出不同元素数目差数组.cpp
@@ -0,0 +1,104 @@
+﻿/* 找出不同元素数目差数组.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的数组 nums ，数组长度为 n 。
+
+nums 的 不同元素数目差 数组可以用一个长度为 n 的数组 diff 表示，其中 diff[i] 等于前缀 nums[0, ..., i] 中不同元素的数目 减去 后缀 nums[i + 1, ..., n - 1] 中不同元素的数目。
+
+返回 nums 的 不同元素数目差 数组。
+
+注意 nums[i, ..., j] 表示 nums 的一个从下标 i 开始到下标 j 结束的子数组（包含下标 i 和 j 对应元素）。特别需要说明的是，如果 i > j ，则 nums[i, ..., j] 表示一个空子数组。
+
+
+
+示例 1：
+
+输入：nums = [1,2,3,4,5]
+输出：[-3,-1,1,3,5]
+解释：
+对于 i = 0，前缀中有 1 个不同的元素，而在后缀中有 4 个不同的元素。因此，diff[0] = 1 - 4 = -3 。
+对于 i = 1，前缀中有 2 个不同的元素，而在后缀中有 3 个不同的元素。因此，diff[1] = 2 - 3 = -1 。
+对于 i = 2，前缀中有 3 个不同的元素，而在后缀中有 2 个不同的元素。因此，diff[2] = 3 - 2 = 1 。
+对于 i = 3，前缀中有 4 个不同的元素，而在后缀中有 1 个不同的元素。因此，diff[3] = 4 - 1 = 3 。
+对于 i = 4，前缀中有 5 个不同的元素，而在后缀中有 0 个不同的元素。因此，diff[4] = 5 - 0 = 5 。
+示例 2：
+
+输入：nums = [3,2,3,4,2]
+输出：[-2,-1,0,2,3]
+解释：
+对于 i = 0，前缀中有 1 个不同的元素，而在后缀中有 3 个不同的元素。因此，diff[0] = 1 - 3 = -2 。
+对于 i = 1，前缀中有 2 个不同的元素，而在后缀中有 3 个不同的元素。因此，diff[1] = 2 - 3 = -1 。
+对于 i = 2，前缀中有 2 个不同的元素，而在后缀中有 2 个不同的元素。因此，diff[2] = 2 - 2 = 0 。
+对于 i = 3，前缀中有 3 个不同的元素，而在后缀中有 1 个不同的元素。因此，diff[3] = 3 - 1 = 2 。
+对于 i = 4，前缀中有 3 个不同的元素，而在后缀中有 0 个不同的元素。因此，diff[4] = 3 - 0 = 3 。
+
+
+提示：
+
+1 <= n == nums.length <= 50
+1 <= nums[i] <= 50
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> distinctDifferenceArray(vector<int>& nums) {
+        int size = nums.size(), left = 0, right = 0;
+        vector<int> numsCount(51, 0), tmpCount(51, 0), diffs(size);
+
+        for (int num : nums) {
+            if (++numsCount[num] == 1) {
+                ++right;
+            }
+        }
+
+        for (int i = 0; i < size; ++i) {
+            int num = nums[i];
+            if (++tmpCount[num] == 1) {
+                ++left;
+            }
+
+            if (tmpCount[num] == numsCount[num]) {
+                --right;
+            }
+
+            diffs[i] = left - right;
+        }
+
+        return diffs;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,2,3,4,5 };
+    vector<int> actual = o.distinctDifferenceArray(nums);
+    vector<int> expected = { -3,-1,1,3,5 };
+    check.checkIntVector(expected, actual);
+
+    nums = { 3,2,3,4,2 };
+    actual = o.distinctDifferenceArray(nums);
+    expected = { -2,-1,0,2,3 };
+    check.checkIntVector(expected, actual);
+
+    nums = { 1 };
+    actual = o.distinctDifferenceArray(nums);
+    expected = { 1 };
+    check.checkIntVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
