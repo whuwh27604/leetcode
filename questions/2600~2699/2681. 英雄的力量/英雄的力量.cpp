3eeb91e4bb0f0d083a07f63c8b1c7d6df8feace8
whuwh27604 <5991277@qq.com>
2023-05-17 00:26:48 +0800

    add

---
+++ questions/2600~2699/2681. 英雄的力量/英雄的力量.cpp
@@ -0,0 +1,80 @@
+﻿/* 英雄的力量.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums ，它表示英雄的能力值。如果我们选出一部分英雄，这组英雄的 力量 定义为：
+
+i0 ，i1 ，... ik 表示这组英雄在数组中的下标。那么这组英雄的力量为 max(nums[i0],nums[i1] ... nums[ik])2 * min(nums[i0],nums[i1] ... nums[ik]) 。
+请你返回所有可能的 非空 英雄组的 力量 之和。由于答案可能非常大，请你将结果对 109 + 7 取余。
+
+
+
+示例 1：
+
+输入：nums = [2,1,4]
+输出：141
+解释：
+第 1 组：[2] 的力量为 22 * 2 = 8 。
+第 2 组：[1] 的力量为 12 * 1 = 1 。
+第 3 组：[4] 的力量为 42 * 4 = 64 。
+第 4 组：[2,1] 的力量为 22 * 1 = 4 。
+第 5 组：[2,4] 的力量为 42 * 2 = 32 。
+第 6 组：[1,4] 的力量为 42 * 1 = 16 。
+第​ ​​​​​​7 组：[2,1,4] 的力量为 42​​​​​​​ * 1 = 16 。
+所有英雄组的力量之和为 8 + 1 + 64 + 4 + 32 + 16 + 16 = 141 。
+示例 2：
+
+输入：nums = [1,1,1]
+输出：7
+解释：总共有 7 个英雄组，每一组的力量都是 1 。所以所有英雄组的力量之和为 7 。
+
+
+提示：
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int sumOfPower(vector<int>& nums) {
+        int size = nums.size();
+        long long minSum = 0, powerSum = 0, mod = 1000000007;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < size; ++i) {
+            long long current = (minSum + nums[i]) * nums[i] % mod * nums[i] % mod;
+            powerSum = (powerSum + current) % mod;
+            minSum = (minSum * 2 + nums[i]) % mod;
+        }
+
+        return (int)(powerSum % mod);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 2,1,4 };
+    check.checkInt(141, o.sumOfPower(nums));
+
+    nums = { 1,1,1 };
+    check.checkInt(7, o.sumOfPower(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
