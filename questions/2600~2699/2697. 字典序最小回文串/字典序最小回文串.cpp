75c490f8836e12553dc2612426ebf425b79fe5dc
whuwh27604 <5991277@qq.com>
2023-05-23 00:10:53 +0800

    add

---
+++ questions/2600~2699/2697. 字典序最小回文串/字典序最小回文串.cpp
@@ -0,0 +1,76 @@
+﻿/* 字典序最小回文串.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个由 小写英文字母 组成的字符串 s ，你可以对其执行一些操作。在一步操作中，你可以用其他小写英文字母 替换  s 中的一个字符。
+
+请你执行 尽可能少的操作 ，使 s 变成一个 回文串 。如果执行 最少 操作次数的方案不止一种，则只需选取 字典序最小 的方案。
+
+对于两个长度相同的字符串 a 和 b ，在 a 和 b 出现不同的第一个位置，如果该位置上 a 中对应字母比 b 中对应字母在字母表中出现顺序更早，则认为 a 的字典序比 b 的字典序要小。
+
+返回最终的回文字符串。
+
+
+
+示例 1：
+
+输入：s = "egcfe"
+输出："efcfe"
+解释：将 "egcfe" 变成回文字符串的最小操作次数为 1 ，修改 1 次得到的字典序最小回文字符串是 "efcfe"，只需将 'g' 改为 'f' 。
+示例 2：
+
+输入：s = "abcd"
+输出："abba"
+解释：将 "abcd" 变成回文字符串的最小操作次数为 2 ，修改 2 次得到的字典序最小回文字符串是 "abba" 。
+示例 3：
+
+输入：s = "seven"
+输出："neven"
+解释：将 "seven" 变成回文字符串的最小操作次数为 1 ，修改 1 次得到的字典序最小回文字符串是 "neven" 。
+
+
+提示：
+
+1 <= s.length <= 1000
+s 仅由小写英文字母组成
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        for (int i = 0, j = s.size() - 1; i < j; ++i, --j) {
+            if (s[i] < s[j]) {
+                s[j] = s[i];
+            }
+            else if (s[i] > s[j]) {
+                s[i] = s[j];
+            }
+        }
+
+        return s;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkString("efcfe", o.makeSmallestPalindrome("egcfe"));
+    check.checkString("abba", o.makeSmallestPalindrome("abcd"));
+    check.checkString("neven", o.makeSmallestPalindrome("seven"));
+    check.checkString("z", o.makeSmallestPalindrome("z"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
