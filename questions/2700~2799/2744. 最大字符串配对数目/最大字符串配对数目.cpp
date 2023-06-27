fd591d2f20dba09285d50336593f199568cad631
whuwh27604 <5991277@qq.com>
2023-06-27 18:16:23 +0800

    add

---
+++ questions/2700~2799/2744. 最大字符串配对数目/最大字符串配对数目.cpp
@@ -0,0 +1,93 @@
+﻿/* 最大字符串配对数目.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的数组 words ，数组中包含 互不相同 的字符串。
+
+如果字符串 words[i] 与字符串 words[j] 满足以下条件，我们称它们可以匹配：
+
+字符串 words[i] 等于 words[j] 的反转字符串。
+0 <= i < j < words.length
+请你返回数组 words 中的 最大 匹配数目。
+
+注意，每个字符串最多匹配一次。
+
+
+
+示例 1：
+
+输入：words = ["cd","ac","dc","ca","zz"]
+输出：2
+解释：在此示例中，我们可以通过以下方式匹配 2 对字符串：
+- 我们将第 0 个字符串与第 2 个字符串匹配，因为 word[0] 的反转字符串是 "dc" 并且等于 words[2]。
+- 我们将第 1 个字符串与第 3 个字符串匹配，因为 word[1] 的反转字符串是 "ca" 并且等于 words[3]。
+可以证明最多匹配数目是 2 。
+示例 2：
+
+输入：words = ["ab","ba","cc"]
+输出：1
+解释：在此示例中，我们可以通过以下方式匹配 1 对字符串：
+- 我们将第 0 个字符串与第 1 个字符串匹配，因为 words[1] 的反转字符串 "ab" 与 words[0] 相等。
+可以证明最多匹配数目是 1 。
+示例 3：
+
+输入：words = ["aa","ab"]
+输出：0
+解释：这个例子中，无法匹配任何字符串。
+
+
+提示：
+
+1 <= words.length <= 50
+words[i].length == 2
+words 包含的字符串互不相同。
+words[i] 只包含小写英文字母。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumNumberOfStringPairs(vector<string>& words) {
+        vector<vector<bool>> exist(127, vector<bool>(127, false));
+        int pairs = 0;
+
+        for (auto& word : words) {
+            if (word[0] != word[1]) {
+                if (exist[word[1]][word[0]]) {
+                    ++pairs;
+                }
+
+                exist[word[0]][word[1]] = true;
+            }
+        }
+
+        return pairs;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<string> words = { "cd","ac","dc","ca","zz" };
+    check.checkInt(2, o.maximumNumberOfStringPairs(words));
+
+    words = { "ab","ba","cc" };
+    check.checkInt(1, o.maximumNumberOfStringPairs(words));
+
+    words = { "aa","ab" };
+    check.checkInt(0, o.maximumNumberOfStringPairs(words));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
