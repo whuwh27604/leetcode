67b563e80b087b5c9443e5acffb296104b6c4229
whuwh27604 <5991277@qq.com>
2023-06-28 00:22:58 +0800

    add

---
+++ questions/2700~2799/2748. 美丽下标对的数目/美丽下标对的数目.cpp
@@ -0,0 +1,100 @@
+﻿/* 美丽下标对的数目.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums 。如果下标对 i、j 满足 0 ≤ i < j < nums.length ，如果 nums[i] 的 第一个数字 和 nums[j] 的 最后一个数字 互质 ，则认为 nums[i] 和 nums[j] 是一组 美丽下标对 。
+
+返回 nums 中 美丽下标对 的总数目。
+
+对于两个整数 x 和 y ，如果不存在大于 1 的整数可以整除它们，则认为 x 和 y 互质 。换而言之，如果 gcd(x, y) == 1 ，则认为 x 和 y 互质，其中 gcd(x, y) 是 x 和 k 最大公因数 。
+
+
+
+示例 1：
+
+输入：nums = [2,5,1,4]
+输出：5
+解释：nums 中共有 5 组美丽下标对：
+i = 0 和 j = 1 ：nums[0] 的第一个数字是 2 ，nums[1] 的最后一个数字是 5 。2 和 5 互质，因此 gcd(2,5) == 1 。
+i = 0 和 j = 2 ：nums[0] 的第一个数字是 2 ，nums[1] 的最后一个数字是 1 。2 和 5 互质，因此 gcd(2,1) == 1 。
+i = 1 和 j = 2 ：nums[0] 的第一个数字是 5 ，nums[1] 的最后一个数字是 1 。2 和 5 互质，因此 gcd(5,1) == 1 。
+i = 1 和 j = 3 ：nums[0] 的第一个数字是 5 ，nums[1] 的最后一个数字是 4 。2 和 5 互质，因此 gcd(5,4) == 1 。
+i = 2 和 j = 3 ：nums[0] 的第一个数字是 1 ，nums[1] 的最后一个数字是 4 。2 和 5 互质，因此 gcd(1,4) == 1 。
+因此，返回 5 。
+示例 2：
+
+输入：nums = [11,21,12]
+输出：2
+解释：共有 2 组美丽下标对：
+i = 0 和 j = 1 ：nums[0] 的第一个数字是 1 ，nums[1] 的最后一个数字是 1 。gcd(1,1) == 1 。
+i = 0 和 j = 2 ：nums[0] 的第一个数字是 1 ，nums[1] 的最后一个数字是 2 。gcd(1,2) == 1 。
+因此，返回 2 。
+
+
+提示：
+
+2 <= nums.length <= 100
+1 <= nums[i] <= 9999
+nums[i] % 10 != 0
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int countBeautifulPairs(vector<int>& nums) {
+        int pairs = 0, size = nums.size();
+
+        for (int i = 0; i < size - 1; ++i) {
+            int first = getFirst(nums[i]);
+
+            for (int j = i + 1; j < size; ++j) {
+                int last = nums[j] % 10;
+
+                if (gcd(first, last) == 1) {
+                    ++pairs;
+                }
+            }
+        }
+
+        return pairs;
+    }
+
+    int getFirst(int num) {
+        int first = 0;
+
+        while (num != 0) {
+            first = num % 10;
+            num /= 10;
+        }
+
+        return first;
+    }
+
+    int gcd(int x, int y) {
+        return y == 0 ? x : gcd(y, x % y);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 2,5,1,4 };
+    check.checkInt(5, o.countBeautifulPairs(nums));
+
+    nums = { 11,21,12 };
+    check.checkInt(2, o.countBeautifulPairs(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
