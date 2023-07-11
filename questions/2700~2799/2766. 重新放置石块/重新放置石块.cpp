a2379b82147cc0f0e9cd493c830d742118712dfd
whuwh27604 <5991277@qq.com>
2023-07-12 00:20:08 +0800

    add

---
+++ questions/2700~2799/2766. 重新放置石块/重新放置石块.cpp
@@ -0,0 +1,101 @@
+﻿/* 重新放置石块.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums ，表示一些石块的初始位置。再给你两个长度 相等 下标从 0 开始的整数数组 moveFrom 和 moveTo 。
+
+在 moveFrom.length 次操作内，你可以改变石块的位置。在第 i 次操作中，你将位置在 moveFrom[i] 的所有石块移到位置 moveTo[i] 。
+
+完成这些操作后，请你按升序返回所有 有 石块的位置。
+
+注意：
+
+如果一个位置至少有一个石块，我们称这个位置 有 石块。
+一个位置可能会有多个石块。
+
+
+示例 1：
+
+输入：nums = [1,6,7,8], moveFrom = [1,7,2], moveTo = [2,9,5]
+输出：[5,6,8,9]
+解释：一开始，石块在位置 1,6,7,8 。
+第 i = 0 步操作中，我们将位置 1 处的石块移到位置 2 处，位置 2,6,7,8 有石块。
+第 i = 1 步操作中，我们将位置 7 处的石块移到位置 9 处，位置 2,6,8,9 有石块。
+第 i = 2 步操作中，我们将位置 2 处的石块移到位置 5 处，位置 5,6,8,9 有石块。
+最后，至少有一个石块的位置为 [5,6,8,9] 。
+示例 2：
+
+输入：nums = [1,1,3,3], moveFrom = [1,3], moveTo = [2,2]
+输出：[2]
+解释：一开始，石块在位置 [1,1,3,3] 。
+第 i = 0 步操作中，我们将位置 1 处的石块移到位置 2 处，有石块的位置为 [2,2,3,3] 。
+第 i = 1 步操作中，我们将位置 3 处的石块移到位置 2 处，有石块的位置为 [2,2,2,2] 。
+由于 2 是唯一有石块的位置，我们返回 [2] 。
+
+
+提示：
+
+1 <= nums.length <= 105
+1 <= moveFrom.length <= 105
+moveFrom.length == moveTo.length
+1 <= nums[i], moveFrom[i], moveTo[i] <= 109
+测试数据保证在进行第 i 步操作时，moveFrom[i] 处至少有一个石块。
+*/
+
+#include <iostream>
+#include <set>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> relocateMarbles(vector<int>& nums, vector<int>& moveFrom, vector<int>& moveTo) {
+        int size = moveFrom.size();
+        set<int> stones;
+        vector<int> ans;
+
+        for (int num : nums) {
+            stones.insert(num);
+        }
+
+        for (int i = 0; i < size; ++i) {
+            stones.erase(moveFrom[i]);
+            stones.insert(moveTo[i]);
+        }
+
+        for (auto iter = stones.begin(); iter != stones.end(); ++iter) {
+            ans.push_back(*iter);
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,6,7,8 };
+    vector<int> moveFrom = { 1,7,2 };
+    vector<int> moveTo = { 2,9,5 };
+    vector<int> actual = o.relocateMarbles(nums, moveFrom, moveTo);
+    vector<int> expected = { 5,6,8,9 };
+    check.checkIntVector(expected, actual);
+
+    nums = { 1,1,3,3 };
+    moveFrom = { 1,3 };
+    moveTo = { 2,2 };
+    actual = o.relocateMarbles(nums, moveFrom, moveTo);
+    expected = { 2 };
+    check.checkIntVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
