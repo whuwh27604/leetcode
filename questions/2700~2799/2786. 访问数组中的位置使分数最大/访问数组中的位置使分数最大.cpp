476add3831d2544d980354c01450ea5c43811560
whuwh27604 <5991277@qq.com>
2023-07-30 07:58:06 +0800

    add

---
+++ questions/2700~2799/2786. 访问数组中的位置使分数最大/访问数组中的位置使分数最大.cpp
@@ -0,0 +1,84 @@
+﻿/* 访问数组中的位置使分数最大.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums 和一个正整数 x 。
+
+你 一开始 在数组的位置 0 处，你可以按照下述规则访问数组中的其他位置：
+
+如果你当前在位置 i ，那么你可以移动到满足 i < j 的 任意 位置 j 。
+对于你访问的位置 i ，你可以获得分数 nums[i] 。
+如果你从位置 i 移动到位置 j 且 nums[i] 和 nums[j] 的 奇偶性 不同，那么你将失去分数 x 。
+请你返回你能得到的 最大 得分之和。
+
+注意 ，你一开始的分数为 nums[0] 。
+
+
+
+示例 1：
+
+输入：nums = [2,3,6,1,9,2], x = 5
+输出：13
+解释：我们可以按顺序访问数组中的位置：0 -> 2 -> 3 -> 4 。
+对应位置的值为 2 ，6 ，1 和 9 。因为 6 和 1 的奇偶性不同，所以下标从 2 -> 3 让你失去 x = 5 分。
+总得分为：2 + 6 + 1 + 9 - 5 = 13 。
+示例 2：
+
+输入：nums = [2,4,6,8], x = 3
+输出：20
+解释：数组中的所有元素奇偶性都一样，所以我们可以将每个元素都访问一次，而且不会失去任何分数。
+总得分为：2 + 4 + 6 + 8 = 20 。
+
+
+提示：
+
+2 <= nums.length <= 105
+1 <= nums[i], x <= 106
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    long long maxScore(vector<int>& nums, int x) {
+        long long score = 0, odd = 0, even = 0;
+
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            int num = nums[i];
+            if ((num & 1) == 0) {
+                score = max(num + even, num + odd - x);
+                even = score;
+            }
+            else {
+                score = max(num + even - x, num + odd);
+                odd = score;
+            }
+        }
+
+        return score;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 2,3,6,1,9,2 };
+    check.checkLongLong(13, o.maxScore(nums, 5));
+
+    nums = { 2,4,6,8 };
+    check.checkLongLong(20, o.maxScore(nums, 3));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
