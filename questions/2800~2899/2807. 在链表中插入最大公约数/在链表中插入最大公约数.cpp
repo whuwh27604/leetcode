816a1ef5adccc884f3f81d4c0b7f2d8a7c5cb9c0
whuwh27604 <5991277@qq.com>
2023-08-13 20:26:41 +0800

    add

---
+++ questions/2800~2899/2807. 在链表中插入最大公约数/在链表中插入最大公约数.cpp
@@ -0,0 +1,91 @@
+﻿/* 在链表中插入最大公约数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个链表的头 head ，每个结点包含一个整数值。
+
+在相邻结点之间，请你插入一个新的结点，结点值为这两个相邻结点值的 最大公约数 。
+
+请你返回插入之后的链表。
+
+两个数的 最大公约数 是可以被两个数字整除的最大正整数。
+
+
+
+示例 1：
+
+
+
+输入：head = [18,6,10,3]
+输出：[18,6,6,2,10,1,3]
+解释：第一幅图是一开始的链表，第二幅图是插入新结点后的图（蓝色结点为新插入结点）。
+- 18 和 6 的最大公约数为 6 ，插入第一和第二个结点之间。
+- 6 和 10 的最大公约数为 2 ，插入第二和第三个结点之间。
+- 10 和 3 的最大公约数为 1 ，插入第三和第四个结点之间。
+所有相邻结点之间都插入完毕，返回链表。
+示例 2：
+
+
+
+输入：head = [7]
+输出：[7]
+解释：第一幅图是一开始的链表，第二幅图是插入新结点后的图（蓝色结点为新插入结点）。
+没有相邻结点，所以返回初始链表。
+
+
+提示：
+
+链表中结点数目在 [1, 5000] 之间。
+1 <= Node.val <= 1000
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* next = cur->next;
+
+        while (next != NULL) {
+            ListNode* node = new ListNode(gcd(cur->val, next->val));
+            cur->next = node;
+            node->next = next;
+            cur = next;
+            next = cur->next;
+        }
+
+        return head;
+    }
+
+    int gcd(int x, int y) {
+        return y == 0 ? x : gcd(y, x % y);
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> values = { 18,6,10,3 };
+    ListNode* actual = o.insertGreatestCommonDivisors(createList(values));
+    values = { 18,6,6,2,10,1,3 };
+    check.checkSingleList(createList(values), actual);
+
+    values = { 7 };
+    actual = o.insertGreatestCommonDivisors(createList(values));
+    values = { 7 };
+    check.checkSingleList(createList(values), actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
