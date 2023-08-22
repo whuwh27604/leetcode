45ccdbe8cdb5c131a680ebbddbe0d07a868d953a
whuwh27604 <5991277@qq.com>
2023-08-22 20:51:52 +0800

    add

--- questions/2800~2899/2824. 统计和小于目标的下标对数目/统计和小于目标的下标对数目.cpp
+++ questions/2800~2899/2824. 统计和小于目标的下标对数目/统计和小于目标的下标对数目.cpp
@@ -1,42 +1,37 @@
 ﻿/* 统计和小于目标的下标对数目.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-给你一个下标从 0 开始的字符串 str1 和 str2 。
-
-一次操作中，你选择 str1 中的若干下标。对于选中的每一个下标 i ，你将 str1[i] 循环 递增，变成下一个字符。也就是说 'a' 变成 'b' ，'b' 变成 'c' ，以此类推，'z' 变成 'a' 。
-
-如果执行以上操作 至多一次 ，可以让 str2 成为 str1 的子序列，请你返回 true ，否则返回 false 。
-
-注意：一个字符串的子序列指的是从原字符串中删除一些（可以一个字符也不删）字符后，剩下字符按照原本先后顺序组成的新字符串。
-
+给你一个下标从 0 开始长度为 n 的整数数组 nums 和一个整数 target ，请你返回满足 0 <= i < j < n 且 nums[i] + nums[j] < target 的下标对 (i, j) 的数目。
 
 
 示例 1：
 
-输入：str1 = "abc", str2 = "ad"
-输出：true
-解释：选择 str1 中的下标 2 。
-将 str1[2] 循环递增，得到 'd' 。
-因此，str1 变成 "abd" 且 str2 现在是一个子序列。所以返回 true 。
+输入：nums = [-1,1,2,3,1], target = 2
+输出：3
+解释：总共有 3 个下标对满足题目描述：
+- (0, 1) ，0 < 1 且 nums[0] + nums[1] = 0 < target
+- (0, 2) ，0 < 2 且 nums[0] + nums[2] = 1 < target
+- (0, 4) ，0 < 4 且 nums[0] + nums[4] = 0 < target
+注意 (0, 3) 不计入答案因为 nums[0] + nums[3] 不是严格小于 target 。
 示例 2：
 
-输入：str1 = "zc", str2 = "ad"
-输出：true
-解释：选择 str1 中的下标 0 和 1 。
-将 str1[0] 循环递增得到 'a' 。
-将 str1[1] 循环递增得到 'd' 。
-因此，str1 变成 "ad" 且 str2 现在是一个子序列。所以返回 true 。
-示例 3：
-
-输入：str1 = "ab", str2 = "d"
-输出：false
-解释：这个例子中，没法在执行一次操作的前提下，将 str2 变为 str1 的子序列。
-所以返回 false 。
+输入：nums = [-6,2,5,-2,-7,-1,3], target = -2
+输出：10
+解释：总共有 10 个下标对满足题目描述：
+- (0, 1) ，0 < 1 且 nums[0] + nums[1] = -4 < target
+- (0, 3) ，0 < 3 且 nums[0] + nums[3] = -8 < target
+- (0, 4) ，0 < 4 且 nums[0] + nums[4] = -13 < target
+- (0, 5) ，0 < 5 且 nums[0] + nums[5] = -7 < target
+- (0, 6) ，0 < 6 且 nums[0] + nums[6] = -3 < target
+- (1, 4) ，1 < 4 且 nums[1] + nums[4] = -5 < target
+- (3, 4) ，3 < 4 且 nums[3] + nums[4] = -9 < target
+- (3, 5) ，3 < 5 且 nums[3] + nums[5] = -3 < target
+- (4, 5) ，4 < 5 且 nums[4] + nums[5] = -8 < target
+- (4, 6) ，4 < 6 且 nums[4] + nums[6] = -4 < target
 
 
 提示：
 
-1 <= str1.length <= 105
-1 <= str2.length <= 105
-str1 和 str2 只包含小写英文字母。
+1 <= nums.length == n <= 50
+-50 <= nums[i], target <= 50
 */
 
 #include <iostream>
