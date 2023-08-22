9c2f92bb89293d8d214a2466c6f17ec19f4770bb
whuwh27604 <5991277@qq.com>
2023-08-23 00:36:57 +0800

    add

---
+++ questions/2800~2899/2828. 判别首字母缩略词/判别首字母缩略词.cpp
@@ -0,0 +1,86 @@
+﻿/* 判别首字母缩略词.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个字符串数组 words 和一个字符串 s ，请你判断 s 是不是 words 的 首字母缩略词 。
+
+如果可以按顺序串联 words 中每个字符串的第一个字符形成字符串 s ，则认为 s 是 words 的首字母缩略词。例如，"ab" 可以由 ["apple", "banana"] 形成，但是无法从 ["bear", "aardvark"] 形成。
+
+如果 s 是 words 的首字母缩略词，返回 true ；否则，返回 false 。
+
+
+
+示例 1：
+
+输入：words = ["alice","bob","charlie"], s = "abc"
+输出：true
+解释：words 中 "alice"、"bob" 和 "charlie" 的第一个字符分别是 'a'、'b' 和 'c'。因此，s = "abc" 是首字母缩略词。
+示例 2：
+
+输入：words = ["an","apple"], s = "a"
+输出：false
+解释：words 中 "an" 和 "apple" 的第一个字符分别是 'a' 和 'a'。
+串联这些字符形成的首字母缩略词是 "aa" 。
+因此，s = "a" 不是首字母缩略词。
+示例 3：
+
+输入：words = ["never","gonna","give","up","on","you"], s = "ngguoy"
+输出：true
+解释：串联数组 words 中每个字符串的第一个字符，得到字符串 "ngguoy" 。
+因此，s = "ngguoy" 是首字母缩略词。
+
+
+提示：
+
+1 <= words.length <= 100
+1 <= words[i].length <= 10
+1 <= s.length <= 100
+words[i] 和 s 由小写英文字母组成
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        if (words.size() != s.size()) {
+            return false;
+        }
+
+        int index = 0;
+
+        for (string& word : words) {
+            if (word[0] != s[index++]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<string> words = { "alice","bob","charlie" };
+    check.checkBool(true, o.isAcronym(words, "abc"));
+
+    words = { "never","gonna","give","up","on","you" };
+    check.checkBool(true, o.isAcronym(words, "ngguoy"));
+
+    words = { "an","apple" };
+    check.checkBool(false, o.isAcronym(words, "a"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
