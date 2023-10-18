cab55cf2522c8f3cd3f0d605a8e8a66ac346ca9f
whuwh27604 <5991277@qq.com>
2023-10-18 11:10:44 +0800

    add

---
+++ questions/2900~2999/2900. 最长相邻不相等子序列 I/最长相邻不相等子序列 I.cpp
@@ -0,0 +1,94 @@
+﻿/* 最长相邻不相等子序列 I.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数 n 和一个下标从 0 开始的字符串数组 words ，和一个下标从 0 开始的 二进制 数组 groups ，两个数组长度都是 n 。
+
+你需要从下标 [0, 1, ..., n - 1] 中选出一个 最长子序列 ，将这个子序列记作长度为 k 的 [i0, i1, ..., ik - 1] ，对于所有满足 0 < j + 1 < k 的 j 都有 groups[ij] != groups[ij + 1] 。
+
+请你返回一个字符串数组，它是下标子序列 依次 对应 words 数组中的字符串连接形成的字符串数组。如果有多个答案，返回任意一个。
+
+子序列 指的是从原数组中删掉一些（也可能一个也不删掉）元素，剩余元素不改变相对位置得到的新的数组。
+
+注意：words 中的字符串长度可能 不相等 。
+
+
+
+示例 1：
+
+输入：n = 3, words = ["e","a","b"], groups = [0,0,1]
+输出：["e","b"]
+解释：一个可行的子序列是 [0,2] ，因为 groups[0] != groups[2] 。
+所以一个可行的答案是 [words[0],words[2]] = ["e","b"] 。
+另一个可行的子序列是 [1,2] ，因为 groups[1] != groups[2] 。
+得到答案为 [words[1],words[2]] = ["a","b"] 。
+这也是一个可行的答案。
+符合题意的最长子序列的长度为 2 。
+示例 2：
+
+输入：n = 4, words = ["a","b","c","d"], groups = [1,0,1,1]
+输出：["a","b","c"]
+解释：一个可行的子序列为 [0,1,2] 因为 groups[0] != groups[1] 且 groups[1] != groups[2] 。
+所以一个可行的答案是 [words[0],words[1],words[2]] = ["a","b","c"] 。
+另一个可行的子序列为 [0,1,3] 因为 groups[0] != groups[1] 且 groups[1] != groups[3] 。
+得到答案为 [words[0],words[1],words[3]] = ["a","b","d"] 。
+这也是一个可行的答案。
+符合题意的最长子序列的长度为 3 。
+
+
+提示：
+
+1 <= n == words.length == groups.length <= 100
+1 <= words[i].length <= 10
+0 <= groups[i] < 2
+words 中的字符串 互不相同 。
+words[i] 只包含小写英文字母。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> getWordsInLongestSubsequence(int n, vector<string>& words, vector<int>& groups) {
+        vector<string> ans;
+        int want = groups[0];
+
+        for (int i = 0; i < n; ++i) {
+            if (groups[i] == want) {
+                ans.push_back(words[i]);
+                want ^= 1;
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<string> words = { "e","a","b" };
+    vector<int> groups = { 0,0,1 };
+    vector<string> actual = o.getWordsInLongestSubsequence(3, words, groups);
+    vector<string> expected = { "e","b" };
+    check.checkStringVector(expected, actual);
+
+    words = { "a","b","c","d" };
+    groups = { 1,0,1,1 };
+    actual = o.getWordsInLongestSubsequence(3, words, groups);
+    expected = { "a","b","c" };
+    check.checkStringVector(expected, actual);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
