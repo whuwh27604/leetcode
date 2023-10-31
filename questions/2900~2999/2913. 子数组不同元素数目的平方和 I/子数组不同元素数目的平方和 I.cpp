93601357679abc3c5c4f87f76ae7a9f1ee39e3b4
whuwh27604 <5991277@qq.com>
2023-10-31 10:43:58 +0800

    add

---
+++ questions/2900~2999/2913. 子数组不同元素数目的平方和 I/子数组不同元素数目的平方和 I.cpp
@@ -0,0 +1,89 @@
+﻿/* 子数组不同元素数目的平方和 I.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums 。
+
+定义 nums 一个子数组的 不同计数 值如下：
+
+令 nums[i..j] 表示 nums 中所有下标在 i 到 j 范围内的元素构成的子数组（满足 0 <= i <= j < nums.length ），那么我们称子数组 nums[i..j] 中不同值的数目为 nums[i..j] 的不同计数。
+请你返回 nums 中所有子数组的 不同计数 的 平方 和。
+
+由于答案可能会很大，请你将它对 109 + 7 取余 后返回。
+
+子数组指的是一个数组里面一段连续 非空 的元素序列。
+
+
+
+示例 1：
+
+输入：nums = [1,2,1]
+输出：15
+解释：六个子数组分别为：
+[1]: 1 个互不相同的元素。
+[2]: 1 个互不相同的元素。
+[1]: 1 个互不相同的元素。
+[1,2]: 2 个互不相同的元素。
+[2,1]: 2 个互不相同的元素。
+[1,2,1]: 2 个互不相同的元素。
+所有不同计数的平方和为 12 + 12 + 12 + 22 + 22 + 22 = 15 。
+示例 2：
+
+输入：nums = [2,2]
+输出：3
+解释：三个子数组分别为：
+[2]: 1 个互不相同的元素。
+[2]: 1 个互不相同的元素。
+[2,2]: 1 个互不相同的元素。
+所有不同计数的平方和为 12 + 12 + 12 = 3 。
+
+
+提示：
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+*/
+
+#include <iostream>
+#include <unordered_set>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int sumCounts(vector<int>& nums) {
+        int size = nums.size(), sum = 0, mod = 1000000007;
+
+        for (int i = 0; i < size; ++i) {
+            unordered_set<int> numbers;
+
+            for (int j = i; j < size; ++j) {
+                numbers.insert(nums[j]);
+                sum = (sum + numbers.size() * numbers.size()) % mod;
+            }
+        }
+
+        return sum;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,2,1 };
+    check.checkInt(15, o.sumCounts(nums));
+
+    nums = { 2,2 };
+    check.checkInt(3, o.sumCounts(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
