66c23bbdd83ddbb7cbc026d04f0a15cc8ef96b13
whuwh27604 <5991277@qq.com>
2023-11-13 17:38:36 +0800

    add

---
+++ questions/2900~2999/2928. 给小朋友们分糖果 I/给小朋友们分糖果 I.cpp
@@ -0,0 +1,66 @@
+﻿/* 给小朋友们分糖果 I.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你两个正整数 n 和 limit 。
+
+请你将 n 颗糖果分给 3 位小朋友，确保没有任何小朋友得到超过 limit 颗糖果，请你返回满足此条件下的 总方案数 。
+
+
+
+示例 1：
+
+输入：n = 5, limit = 2
+输出：3
+解释：总共有 3 种方法分配 5 颗糖果，且每位小朋友的糖果数不超过 2 ：(1, 2, 2) ，(2, 1, 2) 和 (2, 2, 1) 。
+示例 2：
+
+输入：n = 3, limit = 3
+输出：10
+解释：总共有 10 种方法分配 3 颗糖果，且每位小朋友的糖果数不超过 3 ：(0, 0, 3) ，(0, 1, 2) ，(0, 2, 1) ，(0, 3, 0) ，(1, 0, 2) ，(1, 1, 1) ，(1, 2, 0) ，(2, 0, 1) ，(2, 1, 0) 和 (3, 0, 0) 。
+
+
+提示：
+
+1 <= n <= 50
+1 <= limit <= 50
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int distributeCandies(int n, int limit) {
+        int count = 0;
+
+        for (int a = 0; a <= n && a <= limit; ++a) {
+            for (int b = 0; b <= n - a && b <= limit; ++b) {
+                if (n - a - b <= limit) {
+                    ++count;
+                }
+            }
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    check.checkInt(3, o.distributeCandies(5, 2));
+    check.checkInt(10, o.distributeCandies(3, 3));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
