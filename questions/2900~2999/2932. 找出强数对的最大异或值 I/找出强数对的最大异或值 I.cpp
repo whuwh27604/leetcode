c90be32f299bd8db4d99d7cc85a6f09ad15bd922
whuwh27604 <5991277@qq.com>
2023-11-14 10:46:36 +0800

    add

---
+++ questions/2900~2999/2932. 找出强数对的最大异或值 I/找出强数对的最大异或值 I.cpp
@@ -0,0 +1,90 @@
+﻿/* 找出强数对的最大异或值 I.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的整数数组 nums 。如果一对整数 x 和 y 满足以下条件，则称其为 强数对 ：
+
+|x - y| <= min(x, y)
+你需要从 nums 中选出两个整数，且满足：这两个整数可以形成一个强数对，并且它们的按位异或（XOR）值是在该数组所有强数对中的 最大值 。
+
+返回数组 nums 所有可能的强数对中的 最大 异或值。
+
+注意，你可以选择同一个整数两次来形成一个强数对。
+
+
+
+示例 1：
+
+输入：nums = [1,2,3,4,5]
+输出：7
+解释：数组 nums 中有 11 个强数对：(1, 1), (1, 2), (2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (3, 5), (4, 4), (4, 5) 和 (5, 5) 。
+这些强数对中的最大异或值是 3 XOR 4 = 7 。
+示例 2：
+
+输入：nums = [10,100]
+输出：0
+解释：数组 nums 中有 2 个强数对：(10, 10) 和 (100, 100) 。
+这些强数对中的最大异或值是 10 XOR 10 = 0 ，数对 (100, 100) 的异或值也是 100 XOR 100 = 0 。
+示例 3：
+
+输入：nums = [5,6,25,30]
+输出：7
+解释：数组 nums 中有 6 个强数对：(5, 5), (5, 6), (6, 6), (25, 25), (25, 30) 和 (30, 30) 。
+这些强数对中的最大异或值是 25 XOR 30 = 7 ；另一个异或值非零的数对是 (5, 6) ，其异或值是 5 XOR 6 = 3 。
+
+
+提示：
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 100
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumStrongPairXor(vector<int>& nums) {
+        int size = nums.size(), maxXor = 0;
+
+        for (int i = 0; i < size; ++i) {
+            int x = nums[i];
+
+            for (int j = 0; j < size; ++j) {
+                int y = nums[j];
+
+                if (abs(x - y) <= min(x, y)) {
+                    maxXor = max(maxXor, x ^ y);
+                }
+            }
+        }
+
+        return maxXor;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,2,3,4,5 };
+    check.checkInt(7, o.maximumStrongPairXor(nums));
+
+    nums = { 10,100 };
+    check.checkInt(0, o.maximumStrongPairXor(nums));
+
+    nums = { 5,6,25,30 };
+    check.checkInt(7, o.maximumStrongPairXor(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
