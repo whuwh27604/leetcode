8391abcc66e332f9dff0e6e6886f123526003bc4
whuwh27604 <5991277@qq.com>
2023-12-14 16:52:02 +0800

    add

---
+++ questions/2900~2999/2960. 统计已测试设备/统计已测试设备.cpp
@@ -0,0 +1,84 @@
+﻿/* 统计已测试设备.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个长度为 n 、下标从 0 开始的整数数组 batteryPercentages ，表示 n 个设备的电池百分比。
+
+你的任务是按照顺序测试每个设备 i，执行以下测试操作：
+
+如果 batteryPercentages[i] 大于 0：
+增加 已测试设备的计数。
+将下标在 [i + 1, n - 1] 的所有设备的电池百分比减少 1，确保它们的电池百分比 不会低于 0 ，即 batteryPercentages[j] = max(0, batteryPercentages[j] - 1)。
+移动到下一个设备。
+否则，移动到下一个设备而不执行任何测试。
+返回一个整数，表示按顺序执行测试操作后 已测试设备 的数量。
+
+
+
+示例 1：
+
+输入：batteryPercentages = [1,1,2,1,3]
+输出：3
+解释：按顺序从设备 0 开始执行测试操作：
+在设备 0 上，batteryPercentages[0] > 0 ，现在有 1 个已测试设备，batteryPercentages 变为 [1,0,1,0,2] 。
+在设备 1 上，batteryPercentages[1] == 0 ，移动到下一个设备而不进行测试。
+在设备 2 上，batteryPercentages[2] > 0 ，现在有 2 个已测试设备，batteryPercentages 变为 [1,0,1,0,1] 。
+在设备 3 上，batteryPercentages[3] == 0 ，移动到下一个设备而不进行测试。
+在设备 4 上，batteryPercentages[4] > 0 ，现在有 3 个已测试设备，batteryPercentages 保持不变。
+因此，答案是 3 。
+示例 2：
+
+输入：batteryPercentages = [0,1,2]
+输出：2
+解释：按顺序从设备 0 开始执行测试操作：
+在设备 0 上，batteryPercentages[0] == 0 ，移动到下一个设备而不进行测试。
+在设备 1 上，batteryPercentages[1] > 0 ，现在有 1 个已测试设备，batteryPercentages 变为 [0,1,1] 。
+在设备 2 上，batteryPercentages[2] > 0 ，现在有 2 个已测试设备，batteryPercentages 保持不变。
+因此，答案是 2 。
+
+
+提示：
+
+1 <= n == batteryPercentages.length <= 100
+0 <= batteryPercentages[i] <= 100
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int countTestedDevices(vector<int>& batteryPercentages) {
+        int size = batteryPercentages.size(), count = 0;
+
+        for (int i = 0; i < size; ++i) {
+            if (batteryPercentages[i] > count) {
+                ++count;
+            }
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> batteryPercentages = { 1,1,2,1,3 };
+    check.checkInt(3, o.countTestedDevices(batteryPercentages));
+
+    batteryPercentages = { 0,1,2 };
+    check.checkInt(2, o.countTestedDevices(batteryPercentages));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
