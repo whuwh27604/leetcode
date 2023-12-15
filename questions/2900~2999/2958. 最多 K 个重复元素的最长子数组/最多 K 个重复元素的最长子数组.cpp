3f0fad8066f65c246a817c865c28ff409fd39320
whuwh27604 <5991277@qq.com>
2023-12-15 12:00:50 +0800

    add

---
+++ questions/2900~2999/2958. 最多 K 个重复元素的最长子数组/最多 K 个重复元素的最长子数组.cpp
@@ -0,0 +1,96 @@
+﻿/* 最多 K 个重复元素的最长子数组.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个整数数组 nums 和一个整数 k 。
+
+一个元素 x 在数组中的 频率 指的是它在数组中的出现次数。
+
+如果一个数组中所有元素的频率都 小于等于 k ，那么我们称这个数组是 好 数组。
+
+请你返回 nums 中 最长好 子数组的长度。
+
+子数组 指的是一个数组中一段连续非空的元素序列。
+
+
+
+示例 1：
+
+输入：nums = [1,2,3,1,2,3,1,2], k = 2
+输出：6
+解释：最长好子数组是 [1,2,3,1,2,3] ，值 1 ，2 和 3 在子数组中的频率都没有超过 k = 2 。[2,3,1,2,3,1] 和 [3,1,2,3,1,2] 也是好子数组。
+最长好子数组的长度为 6 。
+示例 2：
+
+输入：nums = [1,2,1,2,1,2,1,2], k = 1
+输出：2
+解释：最长好子数组是 [1,2] ，值 1 和 2 在子数组中的频率都没有超过 k = 1 。[2,1] 也是好子数组。
+最长好子数组的长度为 2 。
+示例 3：
+
+输入：nums = [5,5,5,5,5,5,5], k = 4
+输出：4
+解释：最长好子数组是 [5,5,5,5] ，值 5 在子数组中的频率没有超过 k = 4 。
+最长好子数组的长度为 4 。
+
+
+提示：
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+1 <= k <= nums.length
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int, int> freqs;
+        int size = nums.size(), left = 0, right = 0, maxLen = 0;
+
+        for (; right < size; ++right) {
+            int num = nums[right];
+            ++freqs[num];
+
+            while (freqs[num] > k) {
+                --freqs[nums[left++]];
+            }
+
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,2,3,1,2,3,1,2 };
+    check.checkInt(6, o.maxSubarrayLength(nums, 2));
+
+    nums = { 1,2,1,2,1,2,1,2 };
+    check.checkInt(2, o.maxSubarrayLength(nums, 1));
+
+    nums = { 5,5,5,5,5,5,5 };
+    check.checkInt(4, o.maxSubarrayLength(nums, 4));
+
+    nums = { 2,1,1,3 };
+    check.checkInt(4, o.maxSubarrayLength(nums, 2));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
