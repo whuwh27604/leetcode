26305eb477c0a3aad9c8a6b38857dad16cb93353
whuwh27604 <5991277@qq.com>
2024-01-01 13:21:03 +0800

    add

---
+++ questions/2900~2999/2970. 统计移除递增子数组的数目 I/统计移除递增子数组的数目 I.cpp
@@ -0,0 +1,104 @@
+﻿/* 统计移除递增子数组的数目 I.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的 正 整数数组 nums 。
+
+如果 nums 的一个子数组满足：移除这个子数组后剩余元素 严格递增 ，那么我们称这个子数组为 移除递增 子数组。比方说，[5, 3, 4, 6, 7] 中的 [3, 4] 是一个移除递增子数组，因为移除该子数组后，[5, 3, 4, 6, 7] 变为 [5, 6, 7] ，是严格递增的。
+
+请你返回 nums 中 移除递增 子数组的总数目。
+
+注意 ，剩余元素为空的数组也视为是递增的。
+
+子数组 指的是一个数组中一段连续的元素序列。
+
+
+
+示例 1：
+
+输入：nums = [1,2,3,4]
+输出：10
+解释：10 个移除递增子数组分别为：[1], [2], [3], [4], [1,2], [2,3], [3,4], [1,2,3], [2,3,4] 和 [1,2,3,4]。移除任意一个子数组后，剩余元素都是递增的。注意，空数组不是移除递增子数组。
+示例 2：
+
+输入：nums = [6,5,7,8]
+输出：7
+解释：7 个移除递增子数组分别为：[5], [6], [5,7], [6,5], [5,7,8], [6,5,7] 和 [6,5,7,8] 。
+nums 中只有这 7 个移除递增子数组。
+示例 3：
+
+输入：nums = [8,7,6,6]
+输出：3
+解释：3 个移除递增子数组分别为：[8,7,6], [7,6,6] 和 [8,7,6,6] 。注意 [8,7] 不是移除递增子数组因为移除 [8,7] 后 nums 变为 [6,6] ，它不是严格递增的。
+
+
+提示：
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int incremovableSubarrayCount(vector<int>& nums) {
+        int size = nums.size(), count = 0;
+
+        for (int i = 0; i < size; ++i) {
+            for (int j = i; j < size; ++j) {
+                if (check(nums, size, i, j)) {
+                    ++count;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    bool check(vector<int>& nums, int size, int start, int end) {
+        for (int i = 1; i < start; ++i) {
+            if (nums[i] <= nums[i - 1]) {
+                return false;
+            }
+        }
+
+        if (start != 0 && end != size - 1 && nums[end + 1] <= nums[start - 1]) {
+            return false;
+        }
+
+        for (int i = end + 2; i < size; ++i) {
+            if (nums[i] <= nums[i - 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    CheckResult check;
+    Solution o;
+
+    vector<int> nums = { 1,2,3,4 };
+    check.checkInt(10, o.incremovableSubarrayCount(nums));
+
+    nums = { 6,5,7,8 };
+    check.checkInt(7, o.incremovableSubarrayCount(nums));
+
+    nums = { 8,7,6,6 };
+    check.checkInt(3, o.incremovableSubarrayCount(nums));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
