c0c0fd34dbe16bc315de8f05447860d9a443c6a4
whuwh27604 <5991277@qq.com>
2024-04-10 08:34:45 +0800

    add

---
+++ questions/3000~3099/3031. 将单词恢复初始状态所需的最短时间 II/将单词恢复初始状态所需的最短时间 II.cpp
@@ -0,0 +1,109 @@
+﻿/* 将单词恢复初始状态所需的最短时间 II.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个下标从 0 开始的字符串 word 和一个整数 k 。
+
+在每一秒，你必须执行以下操作：
+
+移除 word 的前 k 个字符。
+在 word 的末尾添加 k 个任意字符。
+注意 添加的字符不必和移除的字符相同。但是，必须在每一秒钟都执行 两种 操作。
+
+返回将 word 恢复到其 初始 状态所需的 最短 时间（该时间必须大于零）。
+
+
+
+示例 1：
+
+输入：word = "abacaba", k = 3
+输出：2
+解释：
+第 1 秒，移除 word 的前缀 "aba"，并在末尾添加 "bac" 。因此，word 变为 "cababac"。
+第 2 秒，移除 word 的前缀 "cab"，并在末尾添加 "aba" 。因此，word 变为 "abacaba" 并恢复到始状态。
+可以证明，2 秒是 word 恢复到其初始状态所需的最短时间。
+示例 2：
+
+输入：word = "abacaba", k = 4
+输出：1
+解释：
+第 1 秒，移除 word 的前缀 "abac"，并在末尾添加 "caba" 。因此，word 变为 "abacaba" 并恢复到初始状态。
+可以证明，1 秒是 word 恢复到其初始状态所需的最短时间。
+示例 3：
+
+输入：word = "abcbabcd", k = 2
+输出：4
+解释：
+每一秒，我们都移除 word 的前 2 个字符，并在 word 末尾添加相同的字符。
+4 秒后，word 变为 "abcbabcd" 并恢复到初始状态。
+可以证明，4 秒是 word 恢复到其初始状态所需的最短时间。
+
+
+提示：
+
+1 <= word.length <= 105
+1 <= k <= word.length
+word仅由小写英文字母组成。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumTimeToInitialState(string word, int k) {
+		int n = (int)word.size(), time = 1;
+		vector<int> z = functionZ(word);
+
+		for (int i = k; i < n; i += k) {
+			if (z[i] == n - i) {
+				break;
+			}
+
+			++time;
+		}
+
+		return time;
+    }
+
+	vector<int> functionZ(string& s) {
+		int n = (int)s.size(), left = 0, right = 0;
+		vector<int> z(n, 0);  // z是s的后缀和s的LCP，即 z[i] = LCP(s[i, n), s)，特别的，z[0] = 0
+
+		for (int i = 1; i < n; ++i) {
+			if (i <= right) {
+				z[i] = min(z[i - left], right - i + 1);
+			}
+			
+			while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+				left = i;
+				right = i + z[i];
+				++z[i];
+			}
+		}
+
+		return z;
+	}
+};
+
+int main()
+{
+	Solution o;
+	CheckResult check;
+
+	check.checkInt(2, o.minimumTimeToInitialState("abacaba", 3));
+	check.checkInt(1, o.minimumTimeToInitialState("abacaba", 4));
+	check.checkInt(4, o.minimumTimeToInitialState("abcbabcd", 2));
+	check.checkInt(1, o.minimumTimeToInitialState("z", 1));
+	check.checkInt(2, o.minimumTimeToInitialState("baba", 3));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
