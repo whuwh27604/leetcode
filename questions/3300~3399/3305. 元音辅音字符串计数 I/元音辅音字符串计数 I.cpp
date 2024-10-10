04ad34dcae13872f131e5c0fe1bd6be56deadae0
whuwh27604 <5991277@qq.com>
2024-10-10 10:40:57 +0800

    add

---
+++ questions/3300~3399/3305. 元音辅音字符串计数 I/元音辅音字符串计数 I.cpp
@@ -0,0 +1,137 @@
+﻿/* 元音辅音字符串计数 I.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个字符串 word 和一个 非负 整数 k。
+
+返回 word 的
+子字符串
+ 中，每个元音字母（'a'、'e'、'i'、'o'、'u'）至少 出现一次，并且 恰好 包含 k 个辅音字母的子字符串的总数。
+
+
+
+示例 1：
+
+输入：word = "aeioqq", k = 1
+
+输出：0
+
+解释：
+
+不存在包含所有元音字母的子字符串。
+
+示例 2：
+
+输入：word = "aeiou", k = 0
+
+输出：1
+
+解释：
+
+唯一一个包含所有元音字母且不含辅音字母的子字符串是 word[0..4]，即 "aeiou"。
+
+示例 3：
+
+输入：word = "ieaouqqieaouqq", k = 1
+
+输出：3
+
+解释：
+
+包含所有元音字母并且恰好含有一个辅音字母的子字符串有：
+
+word[0..5]，即 "ieaouq"。
+word[6..11]，即 "qieaou"。
+word[7..12]，即 "ieaouq"。
+
+
+提示：
+
+5 <= word.length <= 250
+word 仅由小写英文字母组成。
+0 <= k <= word.length - 5
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int countOfSubstrings(string word, int k) {
+        int size = (int)word.size(), subs = 0, R1 = 0, consonants = 0;
+        vector<int> vowels(127, 0);
+        vector<int> R2(size, size);
+
+        getR2(word, size, R2);
+
+        for (int L = 0; L < size; ++L) {
+            while (R1 < size && !(isAllVowel(vowels) && consonants == k)) {
+                if (isVowel(word[R1])) {
+                    ++vowels[word[R1++]];
+                }
+                else {
+                    if (consonants < k) {
+                        ++consonants;
+                        ++R1;
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+
+            if (isAllVowel(vowels) && consonants == k) {
+                subs += (R2[R1 - 1] - R1 + 1);
+            }
+
+            if (isVowel(word[L])) {
+                --vowels[word[L]];
+            }
+            else {
+                --consonants;
+            }
+        }
+
+        return subs;
+    }
+
+    void getR2(string& word, int size, vector<int>& R2) {
+        int r2 = size;
+
+        for (int i = size - 1; i >= 0; --i) {
+            R2[i] = r2;
+            if (!isVowel(word[i])) {
+                r2 = i;
+            }
+        }
+    }
+
+    bool isAllVowel(vector<int>& vowels) {
+        return (vowels['a'] > 0) && (vowels['e'] > 0) && (vowels['i'] > 0) && (vowels['o'] > 0) && (vowels['u'] > 0);
+    }
+
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    check.checkInt(0, o.countOfSubstrings("aeioqq", 1));
+    check.checkInt(1, o.countOfSubstrings("aeiou", 0));
+    check.checkInt(3, o.countOfSubstrings("ieaouqqieaouqq", 1));
+    check.checkInt(3, o.countOfSubstrings("iqeaouqi", 2));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
