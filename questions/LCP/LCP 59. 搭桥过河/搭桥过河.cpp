7249cdd2255d6cdd6f53b7f4c8e41d647decc9d3
whuwh27604 <5991277@qq.com>
2024-11-21 08:13:11 +0800

    add

---
+++ questions/LCP/LCP 59. 搭桥过河/搭桥过河.cpp
@@ -0,0 +1,103 @@
+﻿/* 搭桥过河.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+欢迎各位勇者来到力扣城，本次试炼主题为「搭桥过河」。
+
+勇者面前有一段长度为 num 的河流，河流可以划分为若干河道。每条河道上恰有一块浮木，wood[i] 记录了第 i 条河道上的浮木初始的覆盖范围。
+
+当且仅当浮木与相邻河道的浮木覆盖范围有重叠时，勇者才可以在两条浮木间移动
+勇者 仅能在岸上 通过花费一点「自然之力」，使任意一条浮木沿着河流移动一个单位距离
+请问勇者跨越这条河流，最少需要花费多少「自然之力」。
+
+示例 1：
+
+输入： num = 10, wood = [[1,2],[4,7],[8,9]] 输出： 3 解释：如下图所示， 将 [1,2] 浮木移动至 [3,4]，花费 2「自然之力」， 将 [8,9] 浮木移动至 [7,8]，花费 1「自然之力」， 此时勇者可以顺着 [3,4]->[4,7]->[7,8] 跨越河流， 因此，勇者最少需要花费 3 点「自然之力」跨越这条河流wood (2).gif
+
+示例 2：
+
+输入： num = 10, wood = [[1,5],[1,1],[10,10],[6,7],[7,8]] 输出： 10 解释： 将 [1,5] 浮木移动至 [2,6]，花费 1「自然之力」， 将 [1,1] 浮木移动至 [6,6]，花费 5「自然之力」， 将 [10,10] 浮木移动至 [6,6]，花费 4「自然之力」， 此时勇者可以顺着 [2,6]->[6,6]->[6,6]->[6,7]->[7,8] 跨越河流， 因此，勇者最少需要花费 10 点「自然之力」跨越这条河流
+
+示例 3：
+
+输入： num = 5, wood = [[1,2],[2,4]] 输出： 0 解释：勇者不需要移动浮木，仍可以跨越这条河流
+
+提示:
+
+1 <= num <= 10^9
+1 <= wood.length <= 10^5
+wood[i].length == 2
+1 <= wood[i][0] <= wood[i][1] <= num
+*/
+
+#include <iostream>
+#include <queue>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    typedef long long LL;
+
+    long long buildBridge(int num, vector<vector<int>>& wood) {
+        priority_queue<LL> L;
+        priority_queue<LL, vector<LL>, greater<LL>> R;
+        LL biasL = 0, biasR = 0, ans = 0;
+
+        L.push(wood[0][0]);
+        R.push(wood[0][0]);
+
+        for (int i = 1; i < (int)wood.size(); ++i) {
+            biasL -= (wood[i][1] - wood[i][0]);
+            biasR += (wood[i - 1][1] - wood[i - 1][0]);
+            LL left = L.top() + biasL;
+            LL right = R.top() + biasR;
+            int x = wood[i][0];
+
+            if (x < left) {
+                ans += (left - x);
+                L.pop();
+                L.push(x - biasL);
+                L.push(x - biasL);
+                R.push(left - biasR);
+            }
+            else if (x > right) {
+                ans += (x - right);
+                R.pop();
+                R.push(x - biasR);
+                R.push(x - biasR);
+                L.push(right - biasL);
+            }
+            else {
+                L.push(x - biasL);
+                R.push(x - biasR);
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<vector<int>> wood = { {1,2},{4,7},{8,9} };
+    check.checkLongLong(3, o.buildBridge(10, wood));
+
+    wood = { {1,5},{1,1},{10,10},{6,7},{7,8} };
+    check.checkLongLong(10, o.buildBridge(10, wood));
+
+    wood = { {1,2},{2,4} };
+    check.checkLongLong(0, o.buildBridge(5, wood));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ questions/LCP/LCP 74. 最强祝福力场/最强祝福力场.cpp
@@ -0,0 +1,134 @@
+﻿/* 最强祝福力场.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+小扣在探索丛林的过程中，无意间发现了传说中“落寞的黄金之都”。而在这片建筑废墟的地带中，小扣使用探测仪监测到了存在某种带有「祝福」效果的力场。 经过不断的勘测记录，小扣将所有力场的分布都记录了下来。forceField[i] = [x,y,side] 表示第 i 片力场将覆盖以坐标 (x,y) 为中心，边长为 side 的正方形区域。
+
+若任意一点的 力场强度 等于覆盖该点的力场数量，请求出在这片地带中 力场强度 最强处的 力场强度。
+
+注意：
+
+力场范围的边缘同样被力场覆盖。
+示例 1：
+
+输入： forceField = [[0,0,1],[1,0,1]]
+
+输出：2
+
+解释：如图所示，（0.5, 0) 处力场强度最强为 2， （0.5，-0.5）处力场强度同样是 2。image.png
+
+示例 2：
+
+输入： forceField = [[4,4,6],[7,5,3],[1,6,2],[5,6,3]]
+
+输出：3
+
+解释：如下图所示， forceField[0]、forceField[1]、forceField[3] 重叠的区域力场强度最大，返回 3image.png
+
+提示：
+
+1 <= forceField.length <= 100
+forceField[i].length == 3
+0 <= forceField[i][0], forceField[i][1] <= 10^9
+1 <= forceField[i][2] <= 10^9
+*/
+
+#include <iostream>
+#include <set>
+#include <unordered_map>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+typedef long long LL;
+
+class Square {
+public:
+    LL x1;
+    LL x2;
+    LL y1;
+    LL y2;
+
+    Square() : x1(0), x2(0), y1(0), y2(0) {}
+    Square(LL a, LL b, LL c, LL d) : x1(a), x2(b), y1(c), y2(d) {}
+};
+
+class Solution {
+public:
+    int fieldOfGreatestBlessing(vector<vector<int>>& forceField) {
+        vector<Square> squares;
+        set<LL> xs, ys;
+        unordered_map<LL, int> xIndices, yIndices;
+
+        getSquare(forceField, squares, xs, ys);
+        getIndex(xs, xIndices);
+        getIndex(ys, yIndices);
+
+        int n = (int)xs.size(), m = (int)ys.size(), maxBlessing = 0;
+        vector<vector<int>> diff(n + 2, vector<int>(m + 2, 0));
+
+        for (Square& square : squares) {
+            int x1 = xIndices[square.x1], x2 = xIndices[square.x2], y1 = yIndices[square.y1], y2 = yIndices[square.y2];
+            ++diff[x1 + 1][y1 + 1];
+            ++diff[x2 + 2][y2 + 2];
+            --diff[x2 + 2][y1 + 1];
+            --diff[x1 + 1][y2 + 2];
+        }
+
+        for (int x = 1; x <= n; ++x) {
+            for (int y = 1; y <= m; ++y) {
+                diff[x][y] += diff[x - 1][y] + diff[x][y - 1] - diff[x - 1][y - 1];
+                maxBlessing = max(maxBlessing, diff[x][y]);
+            }
+        }
+
+        return maxBlessing;
+    }
+
+    void getSquare(vector<vector<int>>& forceField, vector<Square>& squares, set<LL>& xs, set<LL>& ys) {
+        for (auto& field : forceField) {
+            LL x = field[0], y = field[1], side = field[2];
+            LL x1 = x * 2 - side, x2 = x * 2 + side, y1 = y * 2 - side, y2 = y * 2 + side;
+            squares.push_back(Square(x1, x2, y1, y2));
+            xs.insert(x1);
+            xs.insert(x2);
+            ys.insert(y1);
+            ys.insert(y2);
+        }
+    }
+
+    void getIndex(set<LL>& xs, unordered_map<LL, int>& indices) {
+        int index = 0;
+
+        for (LL x : xs) {
+            indices[x] = index++;
+        }
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    vector<vector<int>> forceField = { {0,0,1},{1,0,1} };
+    check.checkInt(2, o.fieldOfGreatestBlessing(forceField));
+
+    forceField = { {4,4,6},{7,5,3},{1,6,2},{5,6,3} };
+    check.checkInt(3, o.fieldOfGreatestBlessing(forceField));
+
+    int n = 100;
+    forceField = vector<vector<int>>(n, { 0,0,0 });
+    for (int i = 0; i < n; ++i) {
+        forceField[i][2] = n - i;
+    }
+    check.checkInt(n, o.fieldOfGreatestBlessing(forceField));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
