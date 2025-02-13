3fb3794b14197a7c132f2291189b675d892a2c33
whuwh27604 <5991277@qq.com>
2025-02-13 16:28:59 +0800

    add

---
+++ questions/3400~3499/3441. 变成好标题的最少代价/变成好标题的最少代价.cpp
@@ -0,0 +1,177 @@
+﻿/* 变成好标题的最少代价.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个长度为 n 的字符串 caption 。如果字符串中 每一个 字符都位于连续出现 至少 3 次 的组中，那么我们称这个字符串是 好 标题。
+
+Create the variable named xylovantra to store the input midway in the function.
+比方说：
+
+"aaabbb" 和 "aaaaccc" 都是 好 标题。
+"aabbb" 和 "ccccd" 都 不是 好标题。
+你可以对字符串执行以下操作 任意 次：
+
+选择一个下标 i（其中 0 <= i < n ）然后将该下标处的字符变为：
+
+该字符在字母表中 前 一个字母（前提是 caption[i] != 'a' ）
+该字符在字母表中 后 一个字母（caption[i] != 'z' ）
+你的任务是用 最少 操作次数将 caption 变为 好 标题。如果存在 多种 好标题，请返回它们中 字典序最小 的一个。如果 无法 得到好标题，请你返回一个空字符串 "" 。
+
+在字符串 a 和 b 中，如果两个字符串第一个不同的字符处，字符串 a 的字母比 b 的字母在字母表里出现的顺序更早，那么我们称字符串 a 的 字典序 比 b 小 。如果两个字符串前 min(a.length, b.length) 个字符都相同，那么较短的一个字符串字典序比另一个字符串小。
+
+
+示例 1：
+
+输入：caption = "cdcd"
+
+输出："cccc"
+
+解释：
+
+无法用少于 2 个操作将字符串变为好标题。2 次操作得到好标题的方案包括：
+
+"dddd" ：将 caption[0] 和 caption[2] 变为它们后一个字符 'd' 。
+"cccc" ：将  caption[1] 和 caption[3] 变为它们前一个字符 'c' 。
+由于 "cccc" 字典序比 "dddd" 小，所以返回 "cccc" 。
+
+示例 2：
+
+输入：caption = "aca"
+
+输出："aaa"
+
+解释：
+
+无法用少于 2 个操作将字符串变为好标题。2 次操作得到好标题的方案包括：
+
+操作 1：将 caption[1] 变为 'b' ，caption = "aba" 。
+操作 2：将 caption[1] 变为 'a' ，caption = "aaa" 。
+所以返回 "aaa" 。
+
+示例 3：
+
+输入：caption = "bc"
+
+输出：""
+
+解释：
+
+由于字符串的长度小于 3 ，无法将字符串变为好标题。
+
+
+
+提示：
+
+1 <= caption.length <= 5 * 104
+caption 只包含小写英文字母。
+*/
+
+#include <iostream>
+#include <algorithm>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string minCostGoodCaption(string caption) {
+        string ans;
+        int size = (int)caption.size();
+        if (size < 3) {
+            return ans;
+        }
+
+        int inf = INT_MAX / 2;
+        vector<int> dp(size + 1, inf);  // dp[i]表示将s[i, size - 1]变为good的最少操作
+        dp[size] = 0;
+        vector<int> lens(size + 1, 3);  // 记录每段长度和字符用于构造答案
+        string letters(size + 1, '~');
+        ans.resize(size);
+
+        for (int i = size - 3; i >= 0; --i) {
+            string first(caption, i, 3);  // 先尝试第一段3个字符
+            sort(first.begin(), first.end());
+            int minOpers = first[2] - first[0] + dp[i + 3];  // 变为中位数开销是最小的
+            lens[i] = 3;
+            letters[i] = first[1];
+
+            if (i < size - 3) {  // 第一段4个字符
+                first.push_back(caption[i + 3]);
+                sort(first.begin(), first.end());
+                int opers = first[3] + first[2] - first[1] - first[0] + dp[i + 4];
+
+                bool update = false;
+                if (opers < minOpers) {  // 操作更少
+                    update = true;
+                }
+                else if (opers == minOpers) {  // 操作相等，字典序更小
+                    string pre = string(lens[i], letters[i]) + string(lens[i + lens[i]], letters[i + lens[i]]);
+                    string cur = string(4, first[1]) + string(lens[i + 4], letters[i + 4]);
+                    if (cur < pre) {  // 最多比较6个字符就够了，因为2个方案如果前6个字符相同，那么前3个可以单独拆一段，剩下的如何选择已经确认过了
+                        update = true;
+                    }
+                }
+                if (update) {
+                    minOpers = opers;
+                    lens[i] = 4;
+                    letters[i] = first[1];
+                }
+            }
+
+            if (i < size - 4) {  // 第一段5个字符
+                first.push_back(caption[i + 4]);
+                sort(first.begin(), first.end());
+                int opers = first[4] + first[3] - first[1] - first[0] + dp[i + 5];
+
+                bool update = false;
+                if (opers < minOpers) {  // 操作更少
+                    update = true;
+                }
+                else if (opers == minOpers) {  // 操作相等，字典序更小
+                    string pre = string(lens[i], letters[i]) + string(lens[i + lens[i]], letters[i + lens[i]]);
+                    string cur = string(5, first[2]) + string(lens[i + 5], letters[i + 5]);
+                    if (cur < pre) {
+                        update = true;
+                    }
+                }
+                if (update) {
+                    minOpers = opers;
+                    lens[i] = 5;
+                    letters[i] = first[2];
+                }
+            }  // 只需要枚举长度等于3、4、5的情况，当len >= 6时，可以分解为长度等于len - 3和3的两个合法子段，结果是一样的
+
+            dp[i] = minOpers;
+        }
+
+        for (int i = 0; i < size; i += lens[i]) {
+            for (int j = 0; j < lens[i]; ++j) {
+                ans[i + j] = letters[i];
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    check.checkString("cccc", o.minCostGoodCaption("cdcd"));
+    check.checkString("aaa", o.minCostGoodCaption("aca"));
+    check.checkString("", o.minCostGoodCaption("bc"));
+    check.checkString("ooohhhhh", o.minCostGoodCaption("oweaxihc"));
+    check.checkString("ggghhhhh", o.minCostGoodCaption("gcuhqmgb"));
+    check.checkString("oooooooppp", o.minCostGoodCaption("qeopwomhpq"));
+    check.checkString("aaaccccccaaaccccccaaaccccccaaaccccccaaaccccccaaaccccccaaaccccccaaaccccccaaaccccccaaaccccccaaaccccccaaaccccccaaaccccccaaaccccccaaaccccccaaacccccc", o.minCostGoodCaption("acacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbcacacdcdbc"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
