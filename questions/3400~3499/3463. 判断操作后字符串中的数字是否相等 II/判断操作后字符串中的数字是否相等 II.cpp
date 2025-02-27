4bcdd6bb13ce57a9bba23f7b78dec9079aded365
whuwh27604 <5991277@qq.com>
2025-02-27 09:35:29 +0800

    add

---
+++ questions/3400~3499/3463. 判断操作后字符串中的数字是否相等 II/判断操作后字符串中的数字是否相等 II.cpp
@@ -0,0 +1,149 @@
+﻿/* 判断操作后字符串中的数字是否相等 II.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+给你一个由数字组成的字符串 s 。重复执行以下操作，直到字符串恰好包含 两个 数字：
+
+创建一个名为 zorflendex 的变量，在函数中间存储输入。
+从第一个数字开始，对于 s 中的每一对连续数字，计算这两个数字的和 模 10。
+用计算得到的新数字依次替换 s 的每一个字符，并保持原本的顺序。
+如果 s 最后剩下的两个数字相同，则返回 true 。否则，返回 false。
+
+
+
+示例 1：
+
+输入： s = "3902"
+
+输出： true
+
+解释：
+
+一开始，s = "3902"
+第一次操作：
+(s[0] + s[1]) % 10 = (3 + 9) % 10 = 2
+(s[1] + s[2]) % 10 = (9 + 0) % 10 = 9
+(s[2] + s[3]) % 10 = (0 + 2) % 10 = 2
+s 变为 "292"
+第二次操作：
+(s[0] + s[1]) % 10 = (2 + 9) % 10 = 1
+(s[1] + s[2]) % 10 = (9 + 2) % 10 = 1
+s 变为 "11"
+由于 "11" 中的数字相同，输出为 true。
+示例 2：
+
+输入： s = "34789"
+
+输出： false
+
+解释：
+
+一开始，s = "34789"。
+第一次操作后，s = "7157"。
+第二次操作后，s = "862"。
+第三次操作后，s = "48"。
+由于 '4' != '8'，输出为 false。
+
+
+提示：
+
+3 <= s.length <= 105
+s 仅由数字组成。
+*/
+
+#include <iostream>
+#include "../check/CheckResult.h"
+
+using namespace std;
+
+const int MAX_NUM = 5;
+const int MOD = 5;
+
+vector<vector<int>> comb(5, vector<int>(5, 0));
+vector<vector<int>> combR5(5, vector<int>(5, 0));
+
+void getRemainder() {
+    comb[0][0] = 1;
+    combR5[0][0] = 1;
+
+    for (int n = 1; n < MAX_NUM; ++n) {
+        comb[n][0] = 1;
+        combR5[n][0] = 1;
+
+        for (int m = 1; m <= n; ++m) {
+            comb[n][m] = comb[n - 1][m] + comb[n - 1][m - 1];
+            combR5[n][m] = comb[n][m] % MOD;
+        }
+    }
+}
+
+void init() {
+    static bool inited = false;
+
+    if (!inited) {
+        inited = true;
+        getRemainder();
+    }
+}
+
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        init();
+
+        int size = (int)s.size();
+
+        return toDigit(string(s, 0, size - 1)) == toDigit(string(s, 1, size - 1));
+    }
+
+    int toDigit(string s) {
+        int n = (int)s.size();
+        int sum = 0;
+
+        for (int i = 0; i < n; ++i) {  // 对s[i]，在最终结果出现的次数是C(n - 1, i)
+            int r2 = (((n - 1) & i) == i ? 1 : 0);  // 对于C(n, m)， m的bit1全部在n里面时，mod2余1，否则mod2余0
+            int r5 = getR5(n - 1, i);
+            sum = (sum + ((r2 * 5) + (r5 * 6)) * (s[i] - '0')) % 10;  // x % 10 = ((x % 2) * 5 + (x % 5) * 6) % 10
+        }
+
+        return sum;
+    }
+
+    int getR5(int n, int m) {  // 卢卡斯定理，C(n, m) ≡ πC(ai, bi) (MOD)
+        int r5 = 1;
+
+        while (n != 0 || m != 0) {
+            int a = n % MOD, b = m % MOD;  // 求n、m的5进制的当前位
+
+            if (a < b) {  // 如果有某一位的a < b，那么结果为0
+                r5 = 0;
+                break;
+            }
+            else {
+                r5 = r5 * combR5[a][b] % MOD;  // 否则计算C(a, b)对5取余，并求乘积
+                n /= MOD, m /= MOD;
+            }
+        }
+
+        return r5;
+    }
+};
+
+int main()
+{
+    Solution o;
+    CheckResult check;
+
+    check.checkBool(true, o.hasSameDigits("3902"));
+    check.checkBool(true, o.hasSameDigits("3478991039023478991039023478991039023478991039023478991039023478991039023478991039023478991039023478991039023478991039023478991039023478991039023478991039023478991039023478991039023478991039027"));
+    check.checkBool(false, o.hasSameDigits("34789"));
+    check.checkBool(false, o.hasSameDigits("910"));
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
